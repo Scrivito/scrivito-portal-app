da202e5b76ffcdd392367614b8a0d2261e7e726a
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-04 13:34:25 +0200

    [app] Use correct index.scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { configure } from './config'
-import './index.scss'
+import './assets/stylesheets/index.scss'
 
 configure()
 
