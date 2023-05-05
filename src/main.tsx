141dad5342bdf624c26e2ce0fc62972ecf94d092
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-05 10:24:55 +0200

    Load css directly from index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { configure } from './config'
-import './assets/stylesheets/index.scss'
 
 configure()
 
