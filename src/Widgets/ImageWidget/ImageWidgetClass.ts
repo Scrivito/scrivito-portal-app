598805f921cd5d4b4897f73e0f3415042f6ec597
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-16 13:56:26 +0200

    Improved ImageWidget

--- src/Widgets/ImageWidget/ImageWidgetClass.ts
+++ src/Widgets/ImageWidget/ImageWidgetClass.ts
@@ -2,6 +2,7 @@ import * as Scrivito from 'scrivito'
 
 export const ImageWidget = Scrivito.provideWidgetClass('ImageWidget', {
   attributes: {
+    alignment: ['enum', { values: ['left', 'center', 'right'] }],
     alternativeText: 'string',
     image: ['reference', { only: 'Image' }],
     link: 'link',
