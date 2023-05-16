c4d06d2e67860ccc83467d85d1aafdc0cec5ea89
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-16 11:05:30 +0200

    Introducing "Dropdown" obj to group subchildren together.

---
+++ src/Objs/Dropdown/DropdownObjClass.ts
@@ -0,0 +1,8 @@
+import * as Scrivito from 'scrivito'
+
+export const Dropdown = Scrivito.provideObjClass('Dropdown', {
+  attributes: {
+    childOrder: 'referencelist',
+    title: 'string',
+  },
+})
