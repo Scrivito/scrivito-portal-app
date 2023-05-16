0fb7690812f545669829eaaa32ed8853c670b179
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-16 11:05:24 +0200

    Allow to sort children of Homepage and Page objs.

--- src/Objs/Homepage/HomepageObjClass.ts
+++ src/Objs/Homepage/HomepageObjClass.ts
@@ -3,6 +3,7 @@ import * as Scrivito from 'scrivito'
 export const Homepage = Scrivito.provideObjClass('Homepage', {
   attributes: {
     body: 'widgetlist',
+    childOrder: 'referencelist',
     title: 'string',
   },
   extractTextAttributes: ['body'],
--- src/Objs/Page/PageObjClass.ts
+++ src/Objs/Page/PageObjClass.ts
@@ -3,6 +3,7 @@ import * as Scrivito from 'scrivito'
 export const Page = Scrivito.provideObjClass('Page', {
   attributes: {
     body: 'widgetlist',
+    childOrder: 'referencelist',
     title: 'string',
   },
   extractTextAttributes: ['body'],
