f5b2eb42fbaee12b0eb7a87aa2fecd278b289777
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-16 13:43:58 +0200

    Improved TextWidget

--- src/Widgets/TextWidget/TextWidgetClass.ts
+++ src/Widgets/TextWidget/TextWidgetClass.ts
@@ -2,6 +2,7 @@ import * as Scrivito from 'scrivito'
 
 export const TextWidget = Scrivito.provideWidgetClass('TextWidget', {
   attributes: {
+    alignment: ['enum', { values: ['left', 'center', 'right'] }],
     text: 'html',
   },
   extractTextAttributes: ['text'],
--- src/Widgets/TextWidget/TextWidgetComponent.tsx
+++ src/Widgets/TextWidget/TextWidgetComponent.tsx
@@ -2,5 +2,17 @@ import * as Scrivito from 'scrivito'
 import { TextWidget } from './TextWidgetClass'
 
 Scrivito.provideComponent(TextWidget, ({ widget }) => (
-  <Scrivito.ContentTag tag="div" content={widget} attribute="text" />
+  <Scrivito.ContentTag
+    attribute="text"
+    className={alignmentClassName(widget.get('alignment'))}
+    content={widget}
+    tag="div"
+  />
 ))
+
+function alignmentClassName(
+  widgetAlignment: string | null
+): string | undefined {
+  if (widgetAlignment === 'center') return 'text-center'
+  if (widgetAlignment === 'right') return 'text-end'
+}
