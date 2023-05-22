b3142a2f98a876eedf40c59afb41b53b875d15cf
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-22 13:37:18 +0200

    Added "Download" obj

---
+++ src/Objs/Download/DownloadObjClass.ts
@@ -0,0 +1,10 @@
+import * as Scrivito from 'scrivito'
+
+export const Download = Scrivito.provideObjClass('Download', {
+  attributes: {
+    blob: 'binary',
+    tags: 'stringlist',
+    title: 'string',
+  },
+  extractTextAttributes: ['blob:text'],
+})
