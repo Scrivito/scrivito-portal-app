99a94c4d26365f9f941c37687bcd6b0055c4964b
Alexander Gessler <alexander.gessler@infopark.de>
2023-05-25 11:59:48 +0200

    button widget improvements

--- src/Widgets/ButtonWidget/ButtonWidgetClass.ts
+++ src/Widgets/ButtonWidget/ButtonWidgetClass.ts
@@ -3,7 +3,7 @@ import * as Scrivito from 'scrivito'
 export const ButtonWidget = Scrivito.provideWidgetClass('ButtonWidget', {
   attributes: {
     alignment: ['enum', { values: ['left', 'center', 'right', 'block'] }],
-    buttonColor: ['enum', { values: ['btn-primary', 'btn-secondary'] }],
+    buttonColor: ['enum', { values: ['btn-primary', 'btn-secondary','btn-outline-primary', 'btn-outline-secondary'] }],
     buttonSize: ['enum', { values: ['small', 'medium', 'large'] }],
     target: 'link',
   },
