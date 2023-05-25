8e3ac1bc2e3fd4a63fe0ab2fc72d8fce5f7c8183
Alexander Pepper <alexander.pepper@justrelate.com>
2023-05-25 13:55:45 +0200

    Prettier ButtonWidgetClass

--- src/Widgets/ButtonWidget/ButtonWidgetClass.ts
+++ src/Widgets/ButtonWidget/ButtonWidgetClass.ts
@@ -3,7 +3,17 @@ import * as Scrivito from 'scrivito'
 export const ButtonWidget = Scrivito.provideWidgetClass('ButtonWidget', {
   attributes: {
     alignment: ['enum', { values: ['left', 'center', 'right', 'block'] }],
-    buttonColor: ['enum', { values: ['btn-primary', 'btn-secondary','btn-outline-primary', 'btn-outline-secondary'] }],
+    buttonColor: [
+      'enum',
+      {
+        values: [
+          'btn-primary',
+          'btn-secondary',
+          'btn-outline-primary',
+          'btn-outline-secondary',
+        ],
+      },
+    ],
     buttonSize: ['enum', { values: ['small', 'medium', 'large'] }],
     target: 'link',
   },
