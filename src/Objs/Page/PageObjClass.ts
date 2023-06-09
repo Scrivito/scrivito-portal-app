905ccc88531366e96e6967e1863fe97c3e599148
Alexander Pepper <alexander.pepper@justrelate.com>
2023-06-09 11:25:01 +0200

    body widgetlists are no longer restricted to SectionWidgets
    This avoids the situation, that a datalayout renders a section by itself.

--- src/Objs/Page/PageObjClass.ts
+++ src/Objs/Page/PageObjClass.ts
@@ -2,7 +2,7 @@ import * as Scrivito from 'scrivito'
 
 export const Page = Scrivito.provideObjClass('Page', {
   attributes: {
-    body: ['widgetlist', { only: 'SectionWidget' }],
+    body: 'widgetlist',
     childOrder: 'referencelist',
     hideInNavigation: 'boolean',
     title: 'string',
