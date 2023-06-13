d563c55f16e871dc118ad4cc77e3b3f82a7f5cb3
Alexander Pepper <alexander.pepper@justrelate.com>
2023-06-13 10:25:29 +0200

    Add mutliline editor for orders + better editing

--- src/Objs/Order/OrderEditingConfig.tsx
+++ src/Objs/Order/OrderEditingConfig.tsx
@@ -1,28 +1,54 @@
 import * as Scrivito from 'scrivito'
 import { Order } from './OrderObjClass'
+import { MultilineStringEditor } from '../../Components/MultilineStringEditor'
 
 Scrivito.provideEditingConfig(Order, {
   title: 'Order',
   attributes: {
     billingAddress: { title: 'Billing address' },
     items: { title: 'Items' },
-    orderDate: { title: 'Order date' },
+    orderDate: { title: 'Order date', description: 'Format: YYYY-MM-DD' },
     orderId: { title: 'Order ID' },
-    orderTotal: { title: 'Order total' },
-    payment: { title: 'Payment' },
+    orderTotal: { title: 'Order total', description: 'Example: €123.00' },
+    payment: {
+      title: 'Payment',
+      description: '"invoice" or "advance payment"',
+    },
     salesRepresentative: { title: 'Sales representative' },
     shippingId: { title: 'Shipping ID' },
   },
   properties: [
-    'billingAddress',
-    'items',
-    'orderDate',
     'orderId',
+    'orderDate',
     'orderTotal',
     'payment',
-    'salesRepresentative',
     'shippingId',
+    'salesRepresentative',
   ],
+  propertiesGroups: [
+    {
+      title: 'Multiline attributes',
+      key: 'multiline-attributes',
+
+      // Cast is a working around for issue #9925
+      // TODO: remove work around
+      component: (({ obj }: { obj: InstanceType<typeof Order> }) => (
+        <>
+          <MultilineStringEditor
+            content={obj}
+            attribute="items"
+            title="Items"
+          />
+          <MultilineStringEditor
+            content={obj}
+            attribute="billingAddress"
+            title="Billing address"
+          />
+        </>
+      )) as unknown as null,
+    },
+  ],
+
   titleForContent: (obj) => `#${obj.get('orderId')}: ${obj.get('items')}`,
   hideInSelectionDialogs: true,
 })
