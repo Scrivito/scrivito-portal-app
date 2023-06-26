7b186b417f89a42962bfddfea0fbec1731e9bb90
Alexander Pepper <alexander.pepper@justrelate.com>
2023-06-26 10:11:49 +0200

    Order: orderDate => createdAt & orderTotal => total

--- src/Objs/Order/OrderObjClass.ts
+++ src/Objs/Order/OrderObjClass.ts
@@ -3,12 +3,12 @@ import * as Scrivito from 'scrivito'
 export const Order = Scrivito.provideObjClass('Order', {
   attributes: {
     billingAddress: 'string',
+    createdAt: 'string',
     items: 'string',
-    orderDate: 'string',
     orderId: 'string',
-    orderTotal: 'string',
     payment: 'string',
     pdfDownloadUrl: 'string',
     shippingId: 'string',
+    total: 'string',
   },
 })
