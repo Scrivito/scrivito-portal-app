560f677ba5c5d091a9297be6085c3251adb27aba
Alexander Pepper <alexander.pepper@justrelate.com>
2023-06-26 13:49:51 +0200

    Added customerID to currentUser as well.

--- src/Data/CurrentUser/CurrentUserDataItem.ts
+++ src/Data/CurrentUser/CurrentUserDataItem.ts
@@ -3,12 +3,13 @@ import * as Scrivito from 'scrivito'
 export const CurrentUserDataItem = Scrivito.provideDataItem(
   'CurrentUser',
   async () => ({
-    name: 'Jane Smith',
-    email: 'jane.smith@example.com',
     address: `Gartenstraße 10
 39104 Magdeburg
 Germany`,
-    telephone: '+49 391 5976405',
+    customerId: '6504',
+    email: 'jane.smith@example.com',
     jobTitle: 'CNC Machinist',
+    name: 'Jane Smith',
+    telephone: '+49 391 5976405',
   })
 )
