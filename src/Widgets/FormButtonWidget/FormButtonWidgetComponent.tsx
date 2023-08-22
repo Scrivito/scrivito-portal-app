d251a2a5807a8b85df31390169539443602d3bbe
stansilav <stanislav.opeida@nure.ua>
2023-08-22 13:21:22 +0200

    Inline an interface and extract a function

--- src/Widgets/FormButtonWidget/FormButtonWidgetComponent.tsx
+++ src/Widgets/FormButtonWidget/FormButtonWidgetComponent.tsx
@@ -1,12 +1,7 @@
-import * as React from 'react'
 import { provideComponent } from 'scrivito'
 import { FormButtonWidget } from './FormButtonWidgetClass'
-
-interface WrapIfClassNameProps {
-  className?: string
-  children: React.ReactNode
-}
 import { alignmentClassName } from '../FormContainerWidget/utils/alignmentClassName'
+import { WrapIfClassName } from '../FormContainerWidget/utils/wrapIfClassName'
 
 provideComponent(FormButtonWidget, ({ widget }) => {
   const alignment = widget.get('alignment')
@@ -25,14 +20,3 @@ provideComponent(FormButtonWidget, ({ widget }) => {
     </WrapIfClassName>
   )
 })
-
-function WrapIfClassName({
-  className,
-  children,
-}: WrapIfClassNameProps): JSX.Element {
-  return className ? (
-    <div className={className}>{children}</div>
-  ) : (
-    <>{children}</>
-  )
-}
