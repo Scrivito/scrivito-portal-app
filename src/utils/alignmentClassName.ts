9686785cc699d1044c4d962fd9516bf42f987c30
Alexander Pepper <alexander.pepper@justrelate.com>
2023-08-24 10:28:42 +0200

    Improved alignmentClassName
    Explicit return type and reuse of code.

--- src/utils/alignmentClassName.ts
+++ src/utils/alignmentClassName.ts
@@ -1,14 +1,13 @@
-export function alignmentClassName(widgetAlignment: string | null) {
+export function alignmentClassName(
+  widgetAlignment: string | null
+): string | undefined {
   if (widgetAlignment === 'center') return 'text-center'
   if (widgetAlignment === 'right') return 'text-end'
-
-  return undefined
 }
 
 export function alignmentClassNameWithBlock(
   widgetAlignment: string | null
 ): string | undefined {
-  if (widgetAlignment === 'center') return 'text-center'
-  if (widgetAlignment === 'right') return 'text-end'
   if (widgetAlignment === 'block') return 'btn-block'
+  return alignmentClassName(widgetAlignment)
 }
