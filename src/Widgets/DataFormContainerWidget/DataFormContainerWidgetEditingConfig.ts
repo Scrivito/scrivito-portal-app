c57c21ac0765af71bf43e91ac9a5ae0ede910c79
Alexander Pepper <alexander.pepper@justrelate.com>
2023-09-06 14:28:32 +0200

    DataFormContainer: Add some useful initial content.

--- src/Widgets/DataFormContainerWidget/DataFormContainerWidgetEditingConfig.ts
+++ src/Widgets/DataFormContainerWidget/DataFormContainerWidgetEditingConfig.ts
@@ -1,6 +1,14 @@
 import { provideEditingConfig } from 'scrivito'
 import { DataFormContainerWidget } from './DataFormContainerWidgetClass'
+import { DataFormInputFieldWidget } from '../DataFormInputFieldWidget/DataFormInputFieldWidgetClass'
+import { DataFormSubmitButtonWidget } from '../DataFormSubmitButtonWidget/DataFormSubmitButtonWidgetClass'
 
 provideEditingConfig(DataFormContainerWidget, {
   title: 'Data Form',
+  initialContent: {
+    content: [
+      new DataFormInputFieldWidget({}),
+      new DataFormSubmitButtonWidget({}),
+    ],
+  },
 })
