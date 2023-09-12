3a6f32863c3c19d53817032ca78a8aa30f1879d1
Alexander Pepper <alexander.pepper@justrelate.com>
2023-09-12 09:01:25 +0200

    Read current user from Scrivito

--- src/Data/CurrentUser/CurrentUserDataEditingConfig.ts
+++ src/Data/CurrentUser/CurrentUserDataEditingConfig.ts
@@ -6,9 +6,6 @@ provideEditingConfig(CurrentUserDataItem, {
   attributes: {
     name: { title: 'Name' },
     email: { title: 'Email' },
-    customerId: { title: 'Customer ID' },
-    address: { title: 'Address' },
-    telephone: { title: 'Telephone' },
-    jobTitle: { title: 'Job title' },
+    jrUserId: { title: 'JustRelate user ID' },
   },
 })
