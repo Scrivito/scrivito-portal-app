9ba2a5b491ecd1de05133bd6ff5f323c70b4ed0e
stansilav <stanislav.opeida@nure.ua>
2023-09-12 11:25:06 +0200

    Add the 'InPlaceEditingPlaceholder'

---
+++ src/Components/InPlaceEditingPlaceholder.tsx
@@ -0,0 +1,22 @@
+import { isInPlaceEditingActive } from 'scrivito'
+import { placeholderCss } from '../utils/placeholderCss'
+
+export const InPlaceEditingPlaceholder = ({
+  children,
+  center,
+  block,
+}: {
+  children: string
+  center?: boolean
+  block?: boolean
+}) => {
+  if (!isInPlaceEditingActive()) return null
+
+  const innerSpan = <span style={placeholderCss}>{children}</span>
+
+  if (center) return <div className="text-center">{innerSpan}</div>
+
+  if (block) return <div>{innerSpan}</div>
+
+  return innerSpan
+}
