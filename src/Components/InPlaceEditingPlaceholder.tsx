0831397c710de492b9a9f781673bd9a0b8b7fb8a
stansilav <stanislav.opeida@nure.ua>
2023-09-14 12:59:27 +0200

    Connect the component

--- src/Components/InPlaceEditingPlaceholder.tsx
+++ src/Components/InPlaceEditingPlaceholder.tsx
@@ -1,22 +1,24 @@
-import { isInPlaceEditingActive } from 'scrivito'
+import { connect, isInPlaceEditingActive } from 'scrivito'
 import { placeholderCss } from '../utils/placeholderCss'
 
-export const InPlaceEditingPlaceholder = ({
-  children,
-  center,
-  block,
-}: {
-  children: string
-  center?: boolean
-  block?: boolean
-}) => {
-  if (!isInPlaceEditingActive()) return null
+export const InPlaceEditingPlaceholder = connect(
+  ({
+    children,
+    center,
+    block,
+  }: {
+    children: string
+    center?: boolean
+    block?: boolean
+  }) => {
+    if (!isInPlaceEditingActive()) return null
 
-  const innerSpan = <span style={placeholderCss}>{children}</span>
+    const innerSpan = <span style={placeholderCss}>{children}</span>
 
-  if (center) return <div className="text-center">{innerSpan}</div>
+    if (center) return <div className="text-center">{innerSpan}</div>
 
-  if (block) return <div>{innerSpan}</div>
+    if (block) return <div>{innerSpan}</div>
 
-  return innerSpan
-}
+    return innerSpan
+  },
+)
