73053d1212b68159c877c117e2c9a325ce6c875b
stansilav <stanislav.opeida@nure.ua>
2023-09-27 12:50:07 +0200

    Remove the character and length limit

--- src/Widgets/DataFormContainerWidget/utils/validations/attributeNameValidation.ts
+++ src/Widgets/DataFormContainerWidget/utils/validations/attributeNameValidation.ts
@@ -7,14 +7,6 @@ export const attributeNameValidation = [
   (attributeName: unknown, { widget }: { widget: Widget }) => {
     if (typeof attributeName !== 'string') return
 
-    if (attributeName.match(/^[A-Za-z_][A-Za-z0-9_]*$/) === null) {
-      return 'Attribute names may consist of the following characters: "a-z", "A-Z", "0-9", "_".'
-    }
-
-    if (attributeName.length > 50) {
-      return `Attribute names may be up to 50 characters long. This name is ${attributeName.length} characters long.`
-    }
-
     if (!isAttributeNameUnique(widget)) {
       return 'Specify a unique attribute name. There is at least one other element with the same attribute name.'
     }
