ba9c5a260596d7fd11e7617f5e5321e2fc1f6d46
stansilav <stanislav.opeida@nure.ua>
2023-09-28 23:19:50 +0200

    Simplify 'name' check

--- src/Widgets/DataFormHiddenFieldWidget/DataFormHiddenFieldWidgetComponent.tsx
+++ src/Widgets/DataFormHiddenFieldWidget/DataFormHiddenFieldWidgetComponent.tsx
@@ -2,10 +2,8 @@ import { provideComponent } from 'scrivito'
 import { DataFormHiddenFieldWidget } from './DataFormHiddenFieldWidgetClass'
 
 provideComponent(DataFormHiddenFieldWidget, ({ widget }) => {
-  const name =
-    typeof widget.get('attributeName') === 'string'
-      ? widget.get('attributeName')
-      : ''
+  const name = widget.get('attributeName')
+  if (typeof widget.get('attributeName') !== 'string') return null
   if (!name) return null
 
   return <input type="hidden" name={name} value={widget.get('hiddenValue')} />
