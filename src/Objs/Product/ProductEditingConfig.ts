9d572a183f4b49ccc0f388be548783cffa4a12f5
Alexander Pepper <alexander.pepper@justrelate.com>
2023-10-06 08:37:56 +0200

    Product: Add attributes description
    This is needed for placeholders to appear.

--- src/Objs/Product/ProductEditingConfig.ts
+++ src/Objs/Product/ProductEditingConfig.ts
@@ -5,6 +5,11 @@ import { classNameToThumbnail } from '../../utils/classNameToThumbnail'
 provideEditingConfig(Product, {
   title: 'Product',
   thumbnail: classNameToThumbnail('Product'),
+  attributes: {
+    image: { title: 'Image' },
+    subtitle: { title: 'Subtitle' },
+    title: { title: 'Title' },
+  },
   properties: [
     'title',
     'subtitle',
