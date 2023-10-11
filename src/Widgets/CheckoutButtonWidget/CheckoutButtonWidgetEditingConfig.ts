4a7afd33956189fbf0addaf62ab4a4908bc50dcb
Alexander Pepper <alexander.pepper@justrelate.com>
2023-10-11 15:26:20 +0200

    Added CheckoutButtonWidget

---
+++ src/Widgets/CheckoutButtonWidget/CheckoutButtonWidgetEditingConfig.ts
@@ -0,0 +1,51 @@
+import { provideEditingConfig } from 'scrivito'
+import { CheckoutButtonWidget } from './CheckoutButtonWidgetClass'
+import { classNameToThumbnail } from '../../utils/classNameToThumbnail'
+
+provideEditingConfig(CheckoutButtonWidget, {
+  title: 'Checkout Button',
+  attributes: {
+    buttonText: { title: 'Button text' },
+    successMessage: { title: 'Success Message' },
+    alignment: {
+      title: 'Alignment',
+      description: 'Default: Left',
+      values: [
+        { value: 'left', title: 'Left' },
+        { value: 'center', title: 'Center' },
+        { value: 'right', title: 'Right' },
+        { value: 'block', title: 'Full width' },
+      ],
+    },
+    buttonColor: {
+      title: 'Background color',
+      description: 'Default: Primary color',
+      values: [
+        { value: 'btn-primary', title: 'Primary color' },
+        { value: 'btn-secondary', title: 'Secondary color' },
+        { value: 'btn-outline-primary', title: 'Primary outline color' },
+        { value: 'btn-outline-secondary', title: 'Secondary outline color' },
+      ],
+    },
+    buttonSize: {
+      title: 'Button size',
+      description: 'Default: medium',
+    },
+  },
+  thumbnail: classNameToThumbnail('CheckoutButtonWidget'),
+  properties: [
+    'buttonText',
+    'successMessage',
+    'alignment',
+    'buttonColor',
+    'buttonSize',
+  ],
+  initialContent: {
+    alignment: 'center',
+    buttonColor: 'btn-primary',
+    buttonSize: 'medium',
+    buttonText: 'Request quote',
+    successMessage:
+      "Quote sent! Thank you for your interest. We'll get back to you soon.",
+  },
+})
