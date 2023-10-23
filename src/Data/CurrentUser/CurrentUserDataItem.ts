49f507068dce4b02097e38a6d92bdd82719b8390
Alexander Pepper <alexander.pepper@justrelate.com>
2023-10-23 11:42:40 +0200

    Refactored CurrentUserDataItem

--- src/Data/CurrentUser/CurrentUserDataItem.ts
+++ src/Data/CurrentUser/CurrentUserDataItem.ts
@@ -1,12 +1,14 @@
 import { currentUser, load, provideDataItem } from 'scrivito'
 
-export const CurrentUserDataItem = provideDataItem('CurrentUser', async () => {
-  const user = await load(currentUser)
-  if (!user) return {}
+export const CurrentUserDataItem = provideDataItem('CurrentUser', {
+  async get() {
+    const user = await load(currentUser)
+    if (!user) return {}
 
-  return {
-    jrUserId: user.id(),
-    name: user.name(),
-    email: user.email(),
-  }
+    return {
+      jrUserId: user.id(),
+      name: user.name(),
+      email: user.email(),
+    }
+  },
 })
