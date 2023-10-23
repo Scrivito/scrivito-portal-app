05aed578c4e5f3c7b940b4ec3e600704916b08dd
Alexander Pepper <alexander.pepper@justrelate.com>
2023-10-23 14:16:57 +0200

    Refactored jr-api proxy on Cloudflare

--- functions/jr-api/[[catchall]].ts
+++ functions/jr-api/[[catchall]].ts
@@ -1,10 +1,11 @@
 export const onRequest: PagesFunction = async function onRequest({ request }) {
-  const { origin, pathname, search } = new URL(request.url)
+  const proxyLocationPrefix = `${new URL(request.url).origin}/jr-api`
 
-  request.headers.set('X-JR-API-Location', `${origin}/jr-api`)
-
-  return fetch(
-    `https://api.justrelate.com${pathname.replace(/^\/jr-api/, '')}${search}`,
-    new Request(request),
+  const actualRequestUrl = request.url.replace(
+    proxyLocationPrefix,
+    'https://api.justrelate.com',
   )
+  request.headers.set('X-JR-API-Location', proxyLocationPrefix)
+
+  return fetch(actualRequestUrl, new Request(request))
 }
