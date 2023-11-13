e64210bc5e161f9f0b906b175f18db396674d34f
Alexander Pepper <alexander.pepper@justrelate.com>
2023-11-13 12:08:20 +0100

    DataFormContainerWidget: Extracted attributesFromForm

--- src/Widgets/DataFormContainerWidget/DataFormContainerWidgetComponent.tsx
+++ src/Widgets/DataFormContainerWidget/DataFormContainerWidgetComponent.tsx
@@ -52,17 +52,7 @@ provideComponent(DataFormContainerWidget, ({ widget }) => {
 
     setIsSubmitting(true)
 
-    const attributes: { [key: string]: string | File | boolean } =
-      Object.fromEntries(new FormData(formRef.current).entries())
-
-    formRef.current
-      .querySelectorAll<HTMLInputElement>('input[type="checkbox"]')
-      .forEach((checkboxInput) => {
-        const key = checkboxInput.getAttribute('name')
-        if (typeof key !== 'string') return
-
-        attributes[key] = checkboxInput.checked
-      })
+    const attributes = attributesFromForm(formRef.current)
 
     try {
       if (dataItem) {
@@ -106,3 +96,20 @@ provideComponent(DataFormContainerWidget, ({ widget }) => {
       })
   }
 })
+
+function attributesFromForm(formElement: HTMLFormElement) {
+  const attributes: {
+    [key: string]: string | File | boolean
+  } = Object.fromEntries(new FormData(formElement).entries())
+
+  formElement
+    .querySelectorAll<HTMLInputElement>('input[type="checkbox"]')
+    .forEach((checkboxInput) => {
+      const key = checkboxInput.getAttribute('name')
+      if (typeof key !== 'string') return
+
+      attributes[key] = checkboxInput.checked
+    })
+
+  return attributes
+}
