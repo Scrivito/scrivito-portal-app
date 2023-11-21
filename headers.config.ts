8b875af1d4ad8a2db167013450450e8556b4980d
Alexander Pepper <alexander.pepper@justrelate.com>
2023-11-21 13:10:30 +0100

    Rewrite how headers are generated
    Now there is one central module to handle this.

---
+++ headers.config.ts
@@ -0,0 +1,51 @@
+import cspBuilder from 'content-security-policy-builder'
+
+function headers(environment: string) {
+  return {
+    'Content-Security-Policy': cspBuilder({
+      directives: {
+        'base-uri': "'none'",
+        'default-src': ["'self'", 'data:', 'https:', 'wss:'],
+        'style-src': ["'self'", 'data:', 'https:', 'wss:', "'unsafe-inline'"],
+        'script-src': [
+          "'self'",
+          'https://api.scrivito.com',
+          'https://assets.scrivito.com',
+        ].concat(
+          // The package `@vitejs/plugin-react-swc` include an inline script into index.html (see [1]).
+          // [1] https://github.com/vitejs/vite-plugin-react-swc/blob/17bb3ab6f0223f2c19d5cb3b9097457418188da5/src/index.ts#L17C7-L20
+          // In case it breaks please look into the JS console and search for "CSP".
+          // There you can find the current "sha256-x" value, which is to be copied over here.
+          environment === 'development'
+            ? ["'sha256-Z2/iFzh9VMlVkEOar1f/oSHWwQk3ve1qk/C2WdsC4Xk='"]
+            : [],
+        ),
+        'object-src': "'none'",
+        'block-all-mixed-content': true,
+        'frame-ancestors':
+          environment === 'development'
+            ? ['*']
+            : ["'self'", 'https://*.scrivito.com'],
+      },
+    }),
+    'X-Frame-Options': 'sameorigin',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Content-Type-Options': 'nosniff',
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+  }
+}
+
+// Netlify or Cloudflare Pages headers format. For details:
+// * https://www.netlify.com/docs/headers-and-basic-auth/
+// * https://developers.cloudflare.com/pages/platform/headers/
+export function productionHeaders(): string {
+  return `/*
+${Object.entries(headers('production'))
+  .map(([key, value]) => `  ${key}: ${value}`)
+  .join('\n')}
+`
+}
+
+export function developmentHeaders() {
+  return headers('development')
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import dns from 'dns'
 import fs from 'fs'
-import cspBuilder from 'content-security-policy-builder'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
-import headersCsp from './src/_headersCsp.json'
+import { productionHeaders, developmentHeaders } from './headers.config'
 
 // Ensure, that vite prints "localhost" instead of 127.0.0.1
 // See https://vitejs.dev/config/server-options.html#server-host
@@ -44,9 +43,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 8080,
       strictPort: true,
-      headers: {
-        'Content-Security-Policy': devServerCspHeader(),
-      },
+      headers: developmentHeaders(),
       proxy: {
         '/jr-api': {
           target: 'https://api.justrelate.com',
@@ -88,35 +85,10 @@ function writeProductionHeaders(outDir: string) {
     name: 'write-production-headers',
     apply: 'build' as const,
     async writeBundle() {
-      const unprocessedHeaders = await fs.promises.readFile(
-        resolve(__dirname, 'src', '_headers'),
-        'utf-8',
-      )
-
-      const headers = unprocessedHeaders.replace(
-        /CSP-DIRECTIVES-PLACEHOLDER/g,
-        cspBuilder({ directives: headersCsp }),
-      )
-
       await fs.promises.writeFile(
         resolve(__dirname, outDir, '_headers'),
-        headers,
+        productionHeaders(),
       )
     },
   }
-}
-
-function devServerCspHeader() {
-  const directives = JSON.parse(JSON.stringify(headersCsp))
-
-  // This snipped is included by `@vitejs/plugin-react-swc` (see [1]).
-  // See [2] how to generate a sha256 hash.
-  //
-  // [1] https://github.com/vitejs/vite-plugin-react-swc/blob/17bb3ab6f0223f2c19d5cb3b9097457418188da5/src/index.ts#L17C7-L20
-  // [2] https://content-security-policy.com/hash/
-  directives['script-src'].push(
-    "'sha256-Z2/iFzh9VMlVkEOar1f/oSHWwQk3ve1qk/C2WdsC4Xk='",
-  )
-
-  return cspBuilder({ directives })
 }
