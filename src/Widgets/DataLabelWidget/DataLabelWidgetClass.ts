25128d5447ae6c803085fd2bc5e6ab32855d0d8c
Alexander Pepper <alexander.pepper@justrelate.com>
2023-12-21 10:45:39 +0100

    Added DataLabelWidget
    Currently it supports text and currencies.

---
+++ src/Widgets/DataLabelWidget/DataLabelWidgetClass.ts
@@ -0,0 +1,15 @@
+import { provideWidgetClass } from 'scrivito'
+
+export const DataLabelWidget = provideWidgetClass('DataLabelWidget', {
+  attributes: {
+    label: 'string',
+    attributeName: 'string',
+    details: 'string',
+    showAs: ['enum', { values: ['text', 'currency'] }],
+    currency: ['enum', { values: ['EUR', 'USD', 'PLN'] }], // ISO 4217 codes
+    valueSize: [
+      'enum',
+      { values: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'body-font-size'] },
+    ],
+  },
+})
---
+++ src/Widgets/DataLabelWidget/DataLabelWidgetEditingConfig.ts
@@ -0,0 +1,40 @@
+import { provideEditingConfig } from 'scrivito'
+import { DataLabelWidget } from './DataLabelWidgetClass'
+import Thumbnail from './thumbnail.svg'
+
+provideEditingConfig(DataLabelWidget, {
+  title: 'Data Label',
+  thumbnail: Thumbnail,
+  attributes: {
+    attributeName: { title: 'Data item attribute name' },
+    showAs: {
+      title: 'Show as',
+      description: 'Default: Text',
+      values: [
+        { value: 'text', title: 'Text' },
+        { value: 'currency', title: 'Currency' },
+      ],
+    },
+    currency: {
+      title: 'Currency format',
+      description: 'Default: Euro (€)',
+      values: [
+        { value: 'EUR', title: 'Euro (€)' },
+        { value: 'USD', title: 'U.S. dollar ($)' },
+        { value: 'PLN', title: 'Polish złoty (zł)' },
+      ],
+    },
+  },
+  properties: (widget) => [
+    'attributeName',
+    'showAs',
+    ['currency', { enabled: widget.get('showAs') === 'currency' }],
+    'valueSize',
+  ],
+  initialContent: {
+    label: 'Label',
+    showAs: 'text',
+    currency: 'EUR',
+    valueSize: 'body-font-size',
+  },
+})
