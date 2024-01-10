6ccead5fd8a02c24141617f6ca00c8e8fcdac666
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-10 13:21:13 +0100

    Fake User from localStorage

---
+++ src/Data/User/UserDataClass.ts
@@ -0,0 +1,62 @@
+import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDataClass'
+
+export const User = provideLocalStorageDataClass('User', {
+  initialContent: [
+    {
+      _id: '052601BEBCEC39C8E040A8C00D0107AC',
+      name: 'Thomas Richter',
+      salutation: 'Herr',
+      givenName: 'Thomas',
+      familyName: 'Richter',
+    },
+    {
+      _id: '18EEEAEC56D37397E040A8C00F012319',
+      name: 'Adrian Schärli',
+      salutation: 'Herr',
+      givenName: 'Adrian',
+      familyName: 'Schärli',
+    },
+    {
+      _id: '228C5C6067EF486FB72F8D4BAAE6AB08',
+      name: 'Olaf Behrends',
+      salutation: 'Herr',
+      givenName: 'Olaf',
+      familyName: 'Behrends',
+    },
+    {
+      _id: 'D38D7FBFA277856DE030A8C02A010460',
+      name: 'Frank Goosens',
+      salutation: 'Herr',
+      givenName: 'Frank',
+      familyName: 'Goosens',
+    },
+    {
+      _id: 'D456ACF6FF405922E030A8C02A010C68',
+      name: 'Benjamin Braschau',
+      salutation: 'Herr',
+      givenName: 'Benjamin',
+      familyName: 'Braschau',
+    },
+    {
+      _id: 'F87BDC400E41D630E030A8C00D01158A',
+      name: 'Markus Neumann',
+      salutation: 'Herr',
+      givenName: 'Markus',
+      familyName: 'Neumann',
+    },
+    {
+      _id: 'F9E1B255870E5EEEE030A8C00D012D39',
+      name: 'Heinrich Siebert',
+      salutation: 'Herr',
+      givenName: 'Heinrich',
+      familyName: 'Siebert',
+    },
+    {
+      _id: '4668C6ADEF0443BE80FB4049097A901A',
+      name: 'Administrator',
+      salutation: '',
+      givenName: '',
+      familyName: 'Administrator',
+    },
+  ],
+})
