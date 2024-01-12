94df11274b016e62fc20735df6376f54fbc0708d
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-12 11:46:55 +0100

    More ticket data

--- src/Data/TicketMessage/TicketMessageDataClass.ts
+++ src/Data/TicketMessage/TicketMessageDataClass.ts
@@ -3,18 +3,25 @@ import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDat
 export const TicketMessage = provideLocalStorageDataClass('TicketMessage', {
   initialContent: [
     {
-      _id: '8F3799FADAA4437ABB83942F4B251BDE1',
-      ticketId: '8F3799FADAA4437ABB83942F4B251BDE',
-      text: 'Sure - did you try turning it off and back on again?',
-      createdBy: '052601BEBCEC39C8E040A8C00D0107AC',
-      createdAt: '2024-01-12T08:37:56Z',
+      _id: '5095866DEFF74DCDBE56B016898137491',
+      ticketId: '5095866DEFF74DCDBE56B01689813749',
+      text: 'Sorry to hear. Did you try turning it off and back on again?',
+      createdBy: 'D456ACF6FF405922E030A8C02A010C68',
+      createdAt: '2024-01-12T09:39:04Z',
     },
     {
-      _id: '8F3799FADAA4437ABB83942F4B251BDE2',
-      ticketId: '8F3799FADAA4437ABB83942F4B251BDE',
-      text: 'Great, this solved my problem!',
+      _id: '5095866DEFF74DCDBE56B016898137492',
+      ticketId: '5095866DEFF74DCDBE56B01689813749',
+      text: "I just tried it. Now it's working again. Thanks!",
       createdBy: 'F87BDC400E41D630E030A8C00D01158A',
-      createdAt: '2024-01-12T08:38:17Z',
+      createdAt: '2024-01-12T09:39:29Z',
+    },
+    {
+      _id: '5095866DEFF74DCDBE56B016898137493',
+      ticketId: '5095866DEFF74DCDBE56B01689813749',
+      text: 'Great to hear',
+      createdBy: 'D456ACF6FF405922E030A8C02A010C68',
+      createdAt: '2024-01-12T09:39:49Z',
     },
   ],
 })
