afaf822889c75cf6919573b993ab2c1c6f210f90
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-12 10:04:22 +0100

    Connect "User" to PisaSales

--- src/Data/User/UserDataClass.ts
+++ src/Data/User/UserDataClass.ts
@@ -1,62 +1,24 @@
-import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDataClass'
+import { provideDataClass, unstable_JrRestApi } from 'scrivito'
 
-export const User = provideLocalStorageDataClass('User', {
-  initialContent: [
-    {
-      _id: '052601BEBCEC39C8E040A8C00D0107AC',
-      name: 'Thomas Richter',
-      salutation: 'Herr',
-      givenName: 'Thomas',
-      familyName: 'Richter',
-    },
-    {
-      _id: '18EEEAEC56D37397E040A8C00F012319',
-      name: 'Adrian Schärli',
-      salutation: 'Herr',
-      givenName: 'Adrian',
-      familyName: 'Schärli',
-    },
-    {
-      _id: '228C5C6067EF486FB72F8D4BAAE6AB08',
-      name: 'Olaf Behrends',
-      salutation: 'Herr',
-      givenName: 'Olaf',
-      familyName: 'Behrends',
-    },
-    {
-      _id: 'D38D7FBFA277856DE030A8C02A010460',
-      name: 'Frank Goosens',
-      salutation: 'Herr',
-      givenName: 'Frank',
-      familyName: 'Goosens',
-    },
-    {
-      _id: 'D456ACF6FF405922E030A8C02A010C68',
-      name: 'Benjamin Braschau',
-      salutation: 'Herr',
-      givenName: 'Benjamin',
-      familyName: 'Braschau',
-    },
-    {
-      _id: 'F87BDC400E41D630E030A8C00D01158A',
-      name: 'Markus Neumann',
-      salutation: 'Herr',
-      givenName: 'Markus',
-      familyName: 'Neumann',
-    },
-    {
-      _id: 'F9E1B255870E5EEEE030A8C00D012D39',
-      name: 'Heinrich Siebert',
-      salutation: 'Herr',
-      givenName: 'Heinrich',
-      familyName: 'Siebert',
-    },
-    {
-      _id: '4668C6ADEF0443BE80FB4049097A901A',
-      name: 'Administrator',
-      salutation: '',
-      givenName: '',
-      familyName: 'Administrator',
-    },
-  ],
+const apiPath = '../pisa-api/user'
+
+// TODO: use `provideDataClass('User', { apiPath })` once available (with 1.39.0?)
+export const User = provideDataClass('User', {
+  connection: {
+    index: (params) =>
+      unstable_JrRestApi.fetch(apiPath, {
+        params: {
+          ...params.filters(),
+          _continuation: params.continuation(),
+          _order: params.order().length
+            ? params
+                .order()
+                .map((o) => o.join('.'))
+                .join(',')
+            : undefined,
+          _search: params.search() || undefined,
+        },
+      }) as Promise<{ results: Array<{ _id: string }>; continuation?: string }>,
+    get: (id) => unstable_JrRestApi.fetch(`${apiPath}/${id}`),
+  },
 })
