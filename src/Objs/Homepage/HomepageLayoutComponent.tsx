fdfa06bc109ee7c13a397d14a043b02c4d88d35a
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-15 12:34:09 +0100

    Redirect non portal pages to portal overview page
    When in edit mode a note for the editor is shown and a redirect does not take place.

--- src/Objs/Homepage/HomepageLayoutComponent.tsx
+++ src/Objs/Homepage/HomepageLayoutComponent.tsx
@@ -1,7 +1,19 @@
-import { provideLayoutComponent, ContentTag, CurrentPage } from 'scrivito'
-import { Homepage } from './HomepageObjClass'
+import {
+  provideLayoutComponent,
+  ContentTag,
+  CurrentPage,
+  currentPage,
+  isInPlaceEditingActive,
+  load,
+  urlFor,
+  connect,
+} from 'scrivito'
+import { Homepage, HomepageInstance } from './HomepageObjClass'
+import { useEffect } from 'react'
 
 provideLayoutComponent(Homepage, ({ page }) => {
+  const portalOnlyMode = page.get('sitePortalOnlyMode')
+
   return (
     <>
       <a href="#main" className="btn skip-to-content">
@@ -11,8 +23,9 @@ provideLayoutComponent(Homepage, ({ page }) => {
         tag="header"
         content={page}
         attribute="siteHeader"
-        className={page.get('sitePortalOnlyMode') ? 'portal-only' : null}
+        className={portalOnlyMode ? 'portal-only' : null}
       />
+      {portalOnlyMode ? <EditorNoteOrRedirectAwayIfNeeded root={page} /> : null}
       <main id="main">
         <CurrentPage />
       </main>
@@ -25,3 +38,50 @@ provideLayoutComponent(Homepage, ({ page }) => {
     </>
   )
 })
+
+const EditorNoteOrRedirectAwayIfNeeded = connect(
+  function EditorNoteOrRedirectAwayIfNeeded({
+    root,
+  }: {
+    root: HomepageInstance
+  }) {
+    const portalOverviewPage = root.get('sitePortalOverviewPage')
+    const portalOverviewPagePath = portalOverviewPage?.path()
+    const currentPageObjPath = currentPage()?.path()
+
+    const redirectNeeded =
+      !!portalOverviewPage &&
+      !!portalOverviewPagePath &&
+      !!currentPageObjPath &&
+      !currentPageObjPath.startsWith(portalOverviewPagePath)
+
+    useEffect(() => {
+      redirectIfNeeded()
+
+      async function redirectIfNeeded() {
+        if (!redirectNeeded) return
+        if (isInPlaceEditingActive()) return
+
+        const url = await load(() => urlFor(portalOverviewPage))
+        window.location.replace(url)
+      }
+    }, [
+      redirectNeeded,
+      portalOverviewPage,
+      currentPageObjPath, // added, so that useEffect is triggered when currentPageObjPath changes
+    ])
+
+    if (!redirectNeeded) return
+
+    return (
+      <div className="alert alert-warning d-flex m-auto">
+        <i className="bi bi-exclamation-circle bi-2x" aria-hidden="true"></i>
+        <div className="my-auto mx-2">
+          <b>Editor note:</b> This page is not part of the portal section.
+          Regular visitors will be automatically redirected to the portal
+          section.
+        </div>
+      </div>
+    )
+  },
+)
