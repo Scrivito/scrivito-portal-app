21dd0a98bb495ca0f0fad2d77477d9a2f0b5f543
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-29 14:50:46 +0100

    PersonCard renders current user
    Please be aware, that the user is taken from the context!

--- src/Widgets/PersonCardWidget/PersonCardWidgetComponent.tsx
+++ src/Widgets/PersonCardWidget/PersonCardWidgetComponent.tsx
@@ -1,30 +1,22 @@
 import {
   provideComponent,
-  isInPlaceEditingActive,
   WidgetTag,
   InPlaceEditingOff,
-  ImageTag,
-  ContentTag,
+  useDataItem,
 } from 'scrivito'
 import { PersonCardWidget } from './PersonCardWidgetClass'
-import { isPerson } from '../../Objs/Person/PersonObjClass'
+import { DataBinaryImage } from '../../Components/DataBinaryImage'
+import { ensureString } from '../../utils/ensureString'
+import { isDataBinary } from '../../utils/dataBinaryToUrl'
 
-provideComponent(PersonCardWidget, ({ widget }) => {
-  const person = widget.get('person')
-  if (!isPerson(person)) {
-    if (isInPlaceEditingActive()) {
-      return (
-        <div className="alert alert-warning d-flex m-auto">
-          <i className="bi bi-exclamation-circle bi-2x" aria-hidden="true"></i>
-          <div className="my-auto mx-2">
-            Please select a person in the widget properties.
-          </div>
-        </div>
-      )
-    }
+provideComponent(PersonCardWidget, () => {
+  const userItem = useDataItem()
+  if (!userItem) return null
 
-    return null
-  }
+  const name = ensureString(userItem.get('name'))
+  const position = ensureString(userItem.get('position'))
+  const email = ensureString(userItem.get('email'))
+  const image = userItem.get('image')
 
   return (
     <WidgetTag className="card mb-2 bg-secondary max-width-350">
@@ -32,67 +24,27 @@ provideComponent(PersonCardWidget, ({ widget }) => {
         <div className="card-body p-3">
           <div className="row">
             <div className="col-3 d-none d-xl-block">
-              <ImageTag
-                content={person}
-                attribute="image"
-                className="editor-img"
-              />
+              {isDataBinary(image) ? (
+                <DataBinaryImage dataBinary={image} className="editor-img" />
+              ) : null}
             </div>
             <div className="col">
-              <ContentTag
-                content={person}
-                attribute="name"
-                className="h5 text-break"
-              />
-              <ContentTag
-                content={person}
-                attribute="jobTitle"
-                className="text-bold opacity-60 text-extra-small text-uppercase"
-              />
+              <div className="h5 text-break">{name}</div>
+              <div className="text-bold opacity-60 text-extra-small text-uppercase">
+                {position}
+              </div>
               <table className="table-extra-small">
                 <tbody>
-                  <tr>
-                    <th className="align-top">
-                      <i className="bi bi-telephone"></i>
-                    </th>
-                    <td>
-                      <a href={`tel:${person.get('telephone')}`}>
-                        <ContentTag
-                          content={person}
-                          attribute="telephone"
-                          className="text-break"
-                        />
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="align-top">
-                      <i className="bi bi-printer"></i>
-                    </th>
-                    <td>
-                      <a href={`tel:${person.get('fax')}`}>
-                        <ContentTag
-                          content={person}
-                          attribute="fax"
-                          className="text-break"
-                        />
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th className="align-top">
-                      <i className="bi bi-envelope"></i>
-                    </th>
-                    <td>
-                      <a href={`mailto:${person.get('email')}`}>
-                        <ContentTag
-                          content={person}
-                          attribute="email"
-                          className="text-break"
-                        />
-                      </a>
-                    </td>
-                  </tr>
+                  {email ? (
+                    <tr>
+                      <th className="align-top">
+                        <i className="bi bi-envelope"></i>
+                      </th>
+                      <td>
+                        <a href={`mailto:${email}`}>{email}</a>
+                      </td>
+                    </tr>
+                  ) : null}
                 </tbody>
               </table>
             </div>
--- src/Widgets/PersonCardWidget/PersonCardWidgetEditingConfig.ts
+++ src/Widgets/PersonCardWidget/PersonCardWidgetEditingConfig.ts
@@ -5,5 +5,4 @@ import Thumbnail from './thumbnail.svg'
 provideEditingConfig(PersonCardWidget, {
   title: 'Person Card',
   thumbnail: Thumbnail,
-  properties: ['person'],
 })
