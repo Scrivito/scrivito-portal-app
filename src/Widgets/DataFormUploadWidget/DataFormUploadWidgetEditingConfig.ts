e9ffa84f8e188934e2e96b375195d29d165d2ae3
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-29 10:28:57 +0100

    Introducing DataFormUploadWidget
    In combination with the DataFormContainerWidget it converts the files into "File" objects and passes them on to the "dataItem". The "dataItem" then needs to handle the uploading of the file (see next commit)

---
+++ src/Widgets/DataFormUploadWidget/DataFormUploadWidgetEditingConfig.ts
@@ -0,0 +1,20 @@
+import { provideEditingConfig } from 'scrivito'
+import { DataFormUploadWidget } from './DataFormUploadWidgetClass'
+import Thumbnail from './thumbnail.svg'
+
+provideEditingConfig(DataFormUploadWidget, {
+  title: 'Data Form Upload',
+  thumbnail: Thumbnail,
+  attributes: {
+    attributeName: {
+      title: 'Name of the data attribute in question',
+    },
+    required: { title: 'Mandatory?' },
+    multiple: { title: 'Allow multiple files?' },
+    helpText: { title: 'Help text' },
+  },
+  properties: ['attributeName', 'label', 'required', 'helpText', 'multiple'],
+  initialContent: {
+    label: 'File',
+  },
+})
