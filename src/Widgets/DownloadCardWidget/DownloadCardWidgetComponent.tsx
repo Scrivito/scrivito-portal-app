2970fda2826fd2970cb4017877895a28687d45e0
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-30 08:48:44 +0100

    Nicer DownloadCardWidgetComponent

--- src/Widgets/DownloadCardWidget/DownloadCardWidgetComponent.tsx
+++ src/Widgets/DownloadCardWidget/DownloadCardWidgetComponent.tsx
@@ -15,35 +15,31 @@ provideComponent(DownloadCardWidget, ({ widget }) => {
         attribute="label"
         className="text-bold opacity-60 text-extra-small text-uppercase"
       />
-      <div className="card mb-4 bg-white max-width-350">
+      <div className="card mb-4 bg-white">
         <LinkTag to={widget.get('link')} draggable={!isInPlaceEditingActive()}>
           <div className="card-body p-2">
-            <div className="row">
-              <div className="col-3">
+            <div className="d-flex">
+              <div className="px-2">
                 <div className="d-flex h-100">
                   <i
                     className={`bi ${
                       widget.get('icon') || 'bi-filetype-pdf'
-                    } bi-2x m-auto`}
+                    } bi-4x m-auto`}
                     aria-hidden="true"
                   ></i>
                 </div>
               </div>
-              <div className="col-9">
-                <ContentTag
-                  content={widget}
-                  attribute="title"
-                  className="text-bold h6"
-                />
+              <div className="flex-grow-1">
+                <ContentTag content={widget} attribute="title" className="h6" />
                 <ContentTag
                   content={widget}
                   attribute="subtitle"
-                  className="text-bold opacity-60 text-small text-uppercase"
+                  className="text-bold text-small text-uppercase"
                 />
                 <ContentTag
                   content={widget}
                   attribute="details"
-                  className="text-bold opacity-40 text-extra-small text-uppercase"
+                  className="text-bold opacity-60 text-extra-small text-uppercase"
                 />
               </div>
             </div>
