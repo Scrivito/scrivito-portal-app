e0e0866b882ac5d641142f38cae2fca07157f012
Alexander Pepper <alexander.pepper@justrelate.com>
2024-01-31 11:16:50 +0100

    Connect "OrderDocument" and "QuoteDocument" to PisaSales

--- src/Data/OrderDocument/OrderDocumentDataClass.ts
+++ src/Data/OrderDocument/OrderDocumentDataClass.ts
@@ -1,26 +1,6 @@
-import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDataClass'
+import { provideDataClass } from 'scrivito'
 
-export const OrderDocument = provideLocalStorageDataClass('OrderDocument', {
-  initialContent: [
-    {
-      _id: '123',
-      orderId: 'EEDF141E8169451884B23C979526C51A',
-      documentId: '217C72335FC70467E040A8C00F013355',
-    },
-    {
-      _id: '456',
-      orderId: '3DDF9E11BF6E1F8DE040007F01005605',
-      documentId: '130C29DABAB24FE99836DD62450283FA',
-    },
-    {
-      _id: '789',
-      orderId: '4076A80ED49B4D85E040007F01001A11',
-      documentId: '217C72335FC70467E040A8C00F013355',
-    },
-    {
-      _id: '1011',
-      orderId: '4076A80ED49B4D85E040007F01001A11',
-      documentId: '130C29DABAB24FE99836DD62450283FA',
-    },
-  ],
-})
+const apiPath = '../pisa-api/order-document'
+
+// @ts-expect-error until out of private beta
+export const OrderDocument = provideDataClass('OrderDocument', { apiPath })
--- src/Data/QuoteDocument/QuoteDocumentDataClass.ts
+++ src/Data/QuoteDocument/QuoteDocumentDataClass.ts
@@ -1,26 +1,6 @@
-import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDataClass'
+import { provideDataClass } from 'scrivito'
 
-export const QuoteDocument = provideLocalStorageDataClass('QuoteDocument', {
-  initialContent: [
-    {
-      _id: '123',
-      quoteId: 'C6D30A8964AF42C795529F1CCEB3484E',
-      documentId: '217C72335FC70467E040A8C00F013355',
-    },
-    {
-      _id: '456',
-      quoteId: '18EEDF590CBA51BFE040A8C00F012317',
-      documentId: '130C29DABAB24FE99836DD62450283FA',
-    },
-    {
-      _id: '789',
-      quoteId: '2969FBD752154BC399ED8CC624BF0345',
-      documentId: '217C72335FC70467E040A8C00F013355',
-    },
-    {
-      _id: '1011',
-      quoteId: '2969FBD752154BC399ED8CC624BF0345',
-      documentId: '130C29DABAB24FE99836DD62450283FA',
-    },
-  ],
-})
+const apiPath = '../pisa-api/quote-document'
+
+// @ts-expect-error until out of private beta
+export const QuoteDocument = provideDataClass('QuoteDocument', { apiPath })
