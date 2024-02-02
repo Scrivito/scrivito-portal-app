f4646e6bddb828b5c3e88699b9ff73f622013017
David Császár <dcsaszar@users.noreply.github.com>
2024-02-02 14:51:50 +0100

    Add icon to "Restore initial content" menu item

--- src/Extensions/RestoreContent/RestoreContentEditingConfig.ts
+++ src/Extensions/RestoreContent/RestoreContentEditingConfig.ts
@@ -1,10 +1,12 @@
 import { currentPageParams, extendMenu, canWrite, openDialog } from 'scrivito'
+import icon from 'bootstrap-icons/icons/stars.svg'
 
 extendMenu((menu) => {
   if (currentPageParams().restoreContent !== null) return
 
   menu.insert({
     enabled: canWrite(),
+    icon,
     id: 'restoreContent',
     onClick: () => openDialog('RestoreContent'),
     position: { before: 'system.create' },
