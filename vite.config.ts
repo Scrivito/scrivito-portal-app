652d9e85b789f07364bedb3f3e1a1b845eb960ba
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2024-02-02 15:17:20 +0100

    Restart Vite server whenever SDK build changes
    [skip ci]
    Unfortunately, Vite is not able to reload cached deps when they change. However,
    Vite restarts the server whenever special files are changed, among others
    `vite.config.ts`. See https://vitejs.dev/guide/dep-pre-bundling.html#caching.
    
    So, we monitor SDK's build directory and touch the config in
    the app in order to force a server-restart.

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig(({ mode }) => {
     optimizeDeps: {
       include: ['scrivito'],
       force: true,
+      disabled: 'development',
     },
     plugins: [react(), writeProductionHeaders(outDir)],
     preview: {
