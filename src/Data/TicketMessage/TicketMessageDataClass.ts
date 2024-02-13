d37bf95b4a9d3a8b57393372bd54210b8ce49a72
Alexander Pepper <alexander.pepper@justrelate.com>
2024-02-13 11:54:29 +0100

    Updated attributes of ticket and ticket-message api

--- src/Data/TicketMessage/TicketMessageDataClass.ts
+++ src/Data/TicketMessage/TicketMessageDataClass.ts
@@ -18,6 +18,7 @@ export const TicketMessage = provideLocalStorageDataClass('TicketMessage', {
       text: 'Sorry to hear. Did you try turning it off and back on again?',
       createdBy: 'D456ACF6FF405922E030A8C02A010C68',
       createdAt: '2024-01-12T09:39:04Z',
+      attachments: [],
     },
     {
       _id: '5095866DEFF74DCDBE56B016898137492',
@@ -25,6 +26,7 @@ export const TicketMessage = provideLocalStorageDataClass('TicketMessage', {
       text: "I just tried it. Now it's working again. Thanks!",
       createdBy: 'F87BDC400E41D630E030A8C00D01158A',
       createdAt: '2024-01-12T09:39:29Z',
+      attachments: [],
     },
     {
       _id: '5095866DEFF74DCDBE56B016898137493',
@@ -32,6 +34,7 @@ export const TicketMessage = provideLocalStorageDataClass('TicketMessage', {
       text: 'Great to hear',
       createdBy: 'D456ACF6FF405922E030A8C02A010C68',
       createdAt: '2024-01-12T09:39:49Z',
+      attachments: [],
     },
   ],
 })
