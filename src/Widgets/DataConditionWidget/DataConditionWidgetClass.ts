51e5090a1cb11ed7132e789f826f23ac821d434e
David Császár <dcsaszar@users.noreply.github.com>
2024-02-16 11:50:40 +0100

    Revert "Introduce conditional group"
    This reverts commit 07623d20ec4b8f714c433f300ffc991e47026ce9.

--- src/Widgets/DataConditionWidget/DataConditionWidgetClass.ts
+++
@@ -1,9 +0,0 @@
-import { provideWidgetClass } from 'scrivito'
-
-export const DataConditionWidget = provideWidgetClass('DataConditionWidget', {
-  attributes: {
-    attributeName: 'string',
-    attributeValue: 'string',
-    content: 'widgetlist',
-  },
-})
