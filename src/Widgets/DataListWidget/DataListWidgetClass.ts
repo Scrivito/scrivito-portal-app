ae36495fbab81f0e46636eb2646d3addfc4a757d
Alexander Pepper <alexander.pepper@justrelate.com>
2024-03-04 09:21:29 +0100

    Removed "showFooter" option from DataListWidget
    DataGroupWidget now takes care of this.

--- src/Widgets/DataListWidget/DataListWidgetClass.ts
+++ src/Widgets/DataListWidget/DataListWidgetClass.ts
@@ -7,6 +7,5 @@ export const DataListWidget = provideWidgetClass('DataListWidget', {
     footer: 'widgetlist',
     nothingFound: 'widgetlist',
     nrOfColumns: ['enum', { values: ['1', '2', '3', '4', '5', '6'] }],
-    showFooter: 'boolean',
   },
 })
