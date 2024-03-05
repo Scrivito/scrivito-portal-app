bd24caee9c2f7d03560e730a0b451a05de91e0cd
Alexander Pepper <alexander.pepper@justrelate.com>
2024-03-05 08:54:42 +0100

    DataListWidget: Remove obsolete Fragment

--- src/Widgets/DataListWidget/DataListWidgetComponent.tsx
+++ src/Widgets/DataListWidget/DataListWidgetComponent.tsx
@@ -8,18 +8,16 @@ provideComponent(DataListWidget, ({ widget }) => {
   const nrOfColumns = widget.get('nrOfColumns') || '1'
 
   return (
-    <>
-      <div className={`row row-cols-1 row-cols-md-${nrOfColumns}`}>
-        {dataScope.take().map((dataItem) => (
-          <ContentTag
-            content={widget}
-            attribute="content"
-            className="col"
-            dataContext={dataItem}
-            key={dataItem.id()}
-          />
-        ))}
-      </div>
-    </>
+    <div className={`row row-cols-1 row-cols-md-${nrOfColumns}`}>
+      {dataScope.take().map((dataItem) => (
+        <ContentTag
+          content={widget}
+          attribute="content"
+          className="col"
+          dataContext={dataItem}
+          key={dataItem.id()}
+        />
+      ))}
+    </div>
   )
 })
