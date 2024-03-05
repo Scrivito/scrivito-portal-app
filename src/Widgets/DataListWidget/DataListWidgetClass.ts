31be0de30d028772dc6d66e5ff9655935e888f7b
Alexander Pepper <alexander.pepper@justrelate.com>
2024-03-05 08:49:43 +0100

    DataListWidget: Remove obsolete footer attribute
    The rendering code was already removed in #321.

--- src/Widgets/DataListWidget/DataListWidgetClass.ts
+++ src/Widgets/DataListWidget/DataListWidgetClass.ts
@@ -4,7 +4,6 @@ export const DataListWidget = provideWidgetClass('DataListWidget', {
   attributes: {
     content: 'widgetlist',
     data: 'datalocator',
-    footer: 'widgetlist',
     nrOfColumns: ['enum', { values: ['1', '2', '3', '4', '5', '6'] }],
   },
 })
