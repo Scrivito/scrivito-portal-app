c7a9c09deefe44a80f6013fc27238a0c0851549d
Alexander Pepper <alexander.pepper@justrelate.com>
2024-03-07 17:09:46 +0100

    Connect "Gdpr" to Pisa

--- src/Data/Gdpr/GdprDataClass.ts
+++ src/Data/Gdpr/GdprDataClass.ts
@@ -1,18 +1,9 @@
-import { provideLocalStorageDataClass } from '../../utils/provideLocalStorageDataClass'
+import { provideDataClass } from 'scrivito'
+import { pisaClient } from '../pisaClient'
 
-export const Gdpr = provideLocalStorageDataClass('Gdpr', {
-  prepareData: async (data) => ({
-    active: data.active,
-    _id: 'F9BB0AAA22C947BAE030A8C00C015B91',
-    name: 'Contact by telephone',
-    description: 'I agree to be contacted by telephone.',
-  }),
-  initialContent: [
-    {
-      _id: 'F9BB0AAA22C947BAE030A8C00C015B91',
-      name: 'Contact by telephone',
-      description: 'I agree to be contacted by telephone.',
-      active: true,
-    },
-  ],
+const gdprClient = pisaClient('gdpr')
+
+export const Gdpr = provideDataClass('Gdpr', {
+  // @ts-expect-error until out of private beta
+  restApi: gdprClient,
 })
