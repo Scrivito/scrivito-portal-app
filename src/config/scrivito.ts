a8f1b80185ac8d4d7f2ea43f8c823ae993eccd57
David Császár <dcsaszar@users.noreply.github.com>
2024-04-04 14:46:18 +0200

    Move tenant discovery to config module

--- src/config/scrivito.ts
+++ src/config/scrivito.ts
@@ -1,13 +1,17 @@
 import { configure } from 'scrivito'
+import { getTenantFromEnv } from './scrivitoSites'
 
 export function configureScrivito() {
+  const tenant = getTenantFromEnv()
+  if (!tenant) return
+
   const config: Parameters<typeof configure>[0] = {
     adoptUi: true,
     autoConvertAttributes: true,
     optimizedWidgetLoading: true,
     strictSearchOperators: true,
     contentTagsForEmptyAttributes: false,
-    tenant: import.meta.env.SCRIVITO_TENANT || '',
+    tenant,
     // @ts-expect-error // TODO: Remove later on
     unstable: {
       trustedUiOrigins: [
@@ -20,35 +24,6 @@ export function configureScrivito() {
 
   if (!import.meta.env.SCRIVITO_TENANT) {
     // Multitenancy mode
-    const tenantFromUrl =
-      window.location.pathname.match(/^\/([0-9a-f]{32})\b/)?.[1]
-    const tenantFromQuery = new URLSearchParams(window.location.search).get(
-      'tenantId',
-    )
-    const tenant = tenantFromUrl || tenantFromQuery
-
-    if (!tenant) {
-      if (
-        import.meta.env.VITE_MULTITENANCY_FALLBACK_SCRIVITO_TENANT &&
-        !tenantFromQuery
-      ) {
-        const fallbackScrivitoTenant = import.meta.env
-          .VITE_MULTITENANCY_FALLBACK_SCRIVITO_TENANT
-        if (
-          typeof fallbackScrivitoTenant === 'string' &&
-          fallbackScrivitoTenant.match(/^[0-9a-f]{32}$/)
-        ) {
-          window.location.replace(
-            `${window.location.origin}/${fallbackScrivitoTenant}`,
-          )
-          return
-        }
-      }
-
-      throw new Error('Could not determine tenant!')
-    }
-
-    config.tenant = tenant
     config.routingBasePath = `/${tenant}`
     config.extensionsUrl = `/_scrivito_extensions.html?tenantId=${tenant}`
   }
---
+++ src/config/scrivitoSites.ts
@@ -0,0 +1,33 @@
+const location = typeof window !== 'undefined' ? window.location : undefined
+
+export function getTenantFromEnv(): string | undefined {
+  if (import.meta.env.SCRIVITO_TENANT) return import.meta.env.SCRIVITO_TENANT
+
+  if (!location) throw new Error('Could not determine tenant!')
+
+  // Multitenancy mode
+  const tenantFromUrl = location.pathname.match(/^\/([0-9a-f]{32})\b/)?.[1]
+  const tenantFromQuery = new URLSearchParams(location.search).get('tenantId')
+  const tenant = tenantFromUrl || tenantFromQuery
+
+  if (!tenant) {
+    if (
+      import.meta.env.VITE_MULTITENANCY_FALLBACK_SCRIVITO_TENANT &&
+      !tenantFromQuery
+    ) {
+      const fallbackScrivitoTenant = import.meta.env
+        .VITE_MULTITENANCY_FALLBACK_SCRIVITO_TENANT
+      if (
+        typeof fallbackScrivitoTenant === 'string' &&
+        fallbackScrivitoTenant.match(/^[0-9a-f]{32}$/)
+      ) {
+        location.replace(`${location.origin}/${fallbackScrivitoTenant}`)
+        return
+      }
+    }
+
+    throw new Error('Could not determine tenant!')
+  }
+
+  return tenant
+}
