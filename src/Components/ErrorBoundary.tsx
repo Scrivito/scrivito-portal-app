cf50ff00154d2093d05e4f6754d62f4cbda09ded
Alexander Pepper <alexander.pepper@justrelate.com>
2024-04-10 13:39:41 +0200

    ErrorBoundary: Fix image import

--- src/Components/ErrorBoundary.tsx
+++ src/Components/ErrorBoundary.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import errorBackground from '../assets/images/error-background.jpg'
+import spaceman from '../assets/images/spaceman.svg'
 
 interface ErrorBoundaryProps {
   children: React.ReactNode
@@ -28,14 +30,14 @@ export class ErrorBoundary extends React.Component<
     return (
       <section className="bg-danger vh-100 py-4">
         <img
-          src="src/assets/images/error-background.jpg"
+          src={errorBackground}
           alt="factory"
           className="img-background img-zoom"
         />
         <div className="container h-100">
           <div className="row h-100 align-items-center">
             <div className="col-sm-6">
-              <img src="src/assets/images/spaceman.svg" alt="spaceman" />
+              <img src={spaceman} alt="spaceman" />
             </div>
             <div className="col-sm-6">
               <h1 className="display-1">Error</h1>
