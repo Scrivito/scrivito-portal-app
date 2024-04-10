41ec57f7cb4fae0b444d8388dfd076e13e1b269a
Markus Hartig <markus.hartig@justrelate.com>
2024-04-10 10:18:29 +0200

    move `link` and `url` up

--- src/Objs/Redirect/RedirectComponent.tsx
+++ src/Objs/Redirect/RedirectComponent.tsx
@@ -1,29 +1,23 @@
 import * as React from 'react'
-import { ensureUserIsLoggedIn, load, provideComponent, urlFor } from 'scrivito'
+import { ensureUserIsLoggedIn, provideComponent, urlFor } from 'scrivito'
 import { InPlaceEditingPlaceholder } from '../../Components/InPlaceEditingPlaceholder'
 import { Redirect } from './RedirectObjClass'
 
 provideComponent(Redirect, ({ page }) => {
+  const link = page.get('link')
+  const url = link && urlFor(link)
+
   React.useEffect(() => {
-    load(() => {
-      const link = page.get('link')
-      const url = link && urlFor(link)
+    if (!link || !url) return
 
-      return { link, url }
-    }).then(({ link, url }) => {
-      if (!link || !url) return
-
-      if (link.isExternal()) {
-        window.top?.location.replace(url)
-      } else {
-        window.location.replace(url)
-      }
-    })
-  }, [page])
+    if (link.isExternal()) {
+      window.top?.location.replace(url)
+    } else {
+      window.location.replace(url)
+    }
+  }, [link, url])
 
   if (page.get('ensureUserIsLoggedIn')) ensureUserIsLoggedIn()
-
-  const link = page.get('link')
 
   if (!link) {
     return (
