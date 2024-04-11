fa3346fedd2a8b87cca873dc95ca503580361753
Markus Hartig <markus.hartig@justrelate.com>
2024-04-11 17:07:16 +0200

    avoid redirect in UI but always show editor note

--- src/Objs/Redirect/RedirectComponent.tsx
+++ src/Objs/Redirect/RedirectComponent.tsx
@@ -14,6 +14,8 @@ provideComponent(Redirect, ({ page }) => {
   const link = page.get('link')
 
   React.useEffect(() => {
+    if (isEditorLoggedIn()) return
+
     if (requireLogin && !isUserLoggedIn()) {
       ensureUserIsLoggedIn()
       return
@@ -30,16 +32,14 @@ provideComponent(Redirect, ({ page }) => {
     })
   }, [requireLogin, link])
 
-  if (!link && isEditorLoggedIn()) {
+  if (isEditorLoggedIn()) {
     return (
       <div className="container d-flex h-100">
         <div className="alert alert-warning d-flex m-auto">
           <i className="bi bi-exclamation-circle bi-2x" aria-hidden="true"></i>
           <div className="my-auto mx-2">
             This obj never renders any content but triggers a redirect to the
-            specified target.
-            <br />
-            Select a redirect target in the page properties.
+            target specified in the page properties.
           </div>
         </div>
       </div>
