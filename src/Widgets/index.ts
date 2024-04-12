d72dbfb2cfb8cd3215227fe856b91afc8cd7bd73
Amier El Ahmad <amier.elahmad@justrelate.com>
2024-04-12 11:53:01 +0200

    Add TODO

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -2,6 +2,6 @@ import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets';
 import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
 })
-
+//TODO: remove instanceId
 initNeoletterFormWidgets("your instanceId goes here");
 export { }
