516dbbd3723b04e227221b52338b6321d5d6f447
Alexander Pepper <alexander.pepper@justrelate.com>
2024-04-22 14:16:05 +0200

    Hide pisaUrl - it will be needed later on.

--- src/Data/pisaClient.ts
+++ src/Data/pisaClient.ts
@@ -1,7 +1,7 @@
 import { createApiClient } from 'scrivito'
 import { ensureString } from '../utils/ensureString'
 
-export function pisaUrl(): string {
+function pisaUrl(): string {
   const url = ensureString(import.meta.env.PISA_URL)
   if (!url) throw new Error('PISA_URL is not set!')
 
