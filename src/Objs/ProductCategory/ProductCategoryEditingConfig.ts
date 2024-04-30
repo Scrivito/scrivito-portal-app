7acbddf613b5f6b6ba237a676f36c6ad936aa48f
David Császár <dcsaszar@users.noreply.github.com>
2024-04-30 16:22:25 +0200

    Hardcode product category localizers

--- src/Objs/ProductCategory/ProductCategoryEditingConfig.ts
+++ src/Objs/ProductCategory/ProductCategoryEditingConfig.ts
@@ -5,29 +5,7 @@ import Thumbnail from './thumbnail.svg'
 provideEditingConfig(ProductCategory, {
   title: 'Product Category',
   thumbnail: Thumbnail,
-  attributes: {
-    resultsHeadline0: { title: 'Headline for 0 results' },
-    resultsHeadline1: { title: 'Headline for 1 result' },
-    resultsHeadline: {
-      title: 'Headline for multiple results',
-      description:
-        'The placeholder __count__ represents the total number of results.',
-    },
-    title: { title: 'Title' },
-  },
-  properties: [
-    'title',
-    'description',
-    'image',
-    'resultsHeadline0',
-    'resultsHeadline1',
-    'resultsHeadline',
-  ],
+  attributes: { title: { title: 'Title' } },
+  properties: ['title', 'description', 'image'],
   thumbnailForContent: (obj) => obj.get('image'),
-  initialContent: {
-    headline: 'Product category',
-    resultsHeadline: '__count__ items',
-    resultsHeadline0: 'No items',
-    resultsHeadline1: '1 item',
-  },
 })
