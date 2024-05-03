b1554eddab06833a8a5912cf92c3ea45807cec31
Alexander Pepper <alexander.pepper@justrelate.com>
2024-05-03 13:14:01 +0200

    Pisa Service Object is language aware

--- src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
+++ src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
@@ -2,6 +2,7 @@ import { provideDataClass } from 'scrivito'
 import { pisaClient } from '../../pisaClient'
 import { toClientParams } from '../../toClientParams'
 import { DataIndexResponse } from '../../types'
+import { acceptLanguageHeader } from '../../../utils/currentLanguage'
 
 export function pisaServiceObjectDataClass() {
   const serviceObjectClient = pisaClient('service-object')
@@ -11,8 +12,10 @@ export function pisaServiceObjectDataClass() {
       index: (params) =>
         serviceObjectClient.get('', {
           params: toClientParams(params),
+          headers: acceptLanguageHeader(),
         }) as Promise<DataIndexResponse>,
-      get: (id) => serviceObjectClient.get(id),
+      get: (id) =>
+        serviceObjectClient.get(id, { headers: acceptLanguageHeader() }),
     },
   })
 }
