dcc8fd87a3348c6dc54258dabb126ee23941b02f
David Császár <dcsaszar@users.noreply.github.com>
2024-05-06 18:43:34 +0200

    Data widgets show spinner

--- src/Widgets/DataColumnListWidget/DataColumnListWidgetComponent.tsx
+++ src/Widgets/DataColumnListWidget/DataColumnListWidgetComponent.tsx
@@ -3,31 +3,36 @@ import { DataColumnListWidget } from './DataColumnListWidgetClass'
 import { EditorNote } from '../../Components/EditorNote'
 import { useContext } from 'react'
 import { DataBatchContext } from '../../Components/DataBatchContext'
+import { Loading } from '../../Components/Loading'
 
-provideComponent(DataColumnListWidget, ({ widget }) => {
-  const dataScope = useData()
-  const { limit } = useContext(DataBatchContext)
+provideComponent(
+  DataColumnListWidget,
+  ({ widget }) => {
+    const dataScope = useData()
+    const { limit } = useContext(DataBatchContext)
 
-  if (dataScope.isEmpty()) {
-    return <EditorNote>The data column list is empty.</EditorNote>
-  }
+    if (dataScope.isEmpty()) {
+      return <EditorNote>The data column list is empty.</EditorNote>
+    }
 
-  const columnsCount = widget.get('columnsCount') || '2'
+    const columnsCount = widget.get('columnsCount') || '2'
 
-  return (
-    <div className={`row row-cols-1 row-cols-md-${columnsCount}`}>
-      {dataScope
-        .transform({ limit })
-        .take()
-        .map((dataItem) => (
-          <ContentTag
-            content={widget}
-            attribute="content"
-            className="col"
-            dataContext={dataItem}
-            key={dataItem.id()}
-          />
-        ))}
-    </div>
-  )
-})
+    return (
+      <div className={`row row-cols-1 row-cols-md-${columnsCount}`}>
+        {dataScope
+          .transform({ limit })
+          .take()
+          .map((dataItem) => (
+            <ContentTag
+              content={widget}
+              attribute="content"
+              className="col"
+              dataContext={dataItem}
+              key={dataItem.id()}
+            />
+          ))}
+      </div>
+    )
+  },
+  { loading: Loading },
+)
