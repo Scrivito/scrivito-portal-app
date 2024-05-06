caa91cae897e70292686d6142e119d65171ebe88
David Császár <dcsaszar@users.noreply.github.com>
2024-05-06 18:55:33 +0200

    Hide data-empty widget while loading data

--- src/Widgets/DataEmptyWidget/DataEmptyWidgetComponent.tsx
+++ src/Widgets/DataEmptyWidget/DataEmptyWidgetComponent.tsx
@@ -7,22 +7,26 @@ import {
 } from 'scrivito'
 import { DataEmptyWidget } from './DataEmptyWidgetClass'
 
-provideComponent(DataEmptyWidget, ({ widget }) => {
-  const dataScope = useData()
+provideComponent(
+  DataEmptyWidget,
+  ({ widget }) => {
+    const dataScope = useData()
 
-  if (
-    dataScope.containsData() &&
-    !isInPlaceEditingActive() &&
-    !isComparisonActive()
-  ) {
-    return null
-  }
+    if (
+      dataScope.containsData() &&
+      !isInPlaceEditingActive() &&
+      !isComparisonActive()
+    ) {
+      return null
+    }
 
-  return (
-    <ContentTag
-      content={widget}
-      attribute="content"
-      className={dataScope.containsData() ? 'opacity-60' : null}
-    />
-  )
-})
+    return (
+      <ContentTag
+        content={widget}
+        attribute="content"
+        className={dataScope.containsData() ? 'opacity-60' : null}
+      />
+    )
+  },
+  { loading: () => null },
+)
