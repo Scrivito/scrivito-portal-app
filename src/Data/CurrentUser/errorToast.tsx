928967995b00257bbee3e7dd9857695122981580
Alexander Pepper <alexander.pepper@justrelate.com>
2024-05-13 09:35:53 +0200

    Improve "error.message" handling

--- src/Data/CurrentUser/errorToast.tsx
+++ src/Data/CurrentUser/errorToast.tsx
@@ -5,16 +5,16 @@ export function errorToast(title: string, error: unknown): void {
   toast.error(
     <>
       <div>{title}</div>
+      <small>
+        {error instanceof Error
+          ? error.message
+          : JSON.stringify(error, null, 1)}
+      </small>
       {error instanceof ClientError ? (
-        <>
-          <div className="text-small">{error.message}</div>
-          <pre className="text-small text-muted">
-            {JSON.stringify(error.details, null, 1)}
-          </pre>
-        </>
-      ) : (
-        <div className="text-small">{JSON.stringify(error, null, 1)}</div>
-      )}
+        <pre className="text-small text-muted">
+          {JSON.stringify(error.details, null, 1)}
+        </pre>
+      ) : null}
     </>,
     {
       autoClose: false,
