bff4e590a71106abb8e886a3cfda0d7cfbdd1294
Alexander Pepper <alexander.pepper@justrelate.com>
2024-05-29 15:36:09 +0200

    Removed "imageFromDataItem" option from ImageWidget

--- src/Widgets/ImageWidget/ImageWidgetClass.ts
+++ src/Widgets/ImageWidget/ImageWidgetClass.ts
@@ -4,10 +4,8 @@ export const ImageWidget = provideWidgetClass('ImageWidget', {
   attributes: {
     alignment: ['enum', { values: ['left', 'center', 'right'] }],
     alternativeText: 'string',
-    attributeName: 'string',
     roundCorners: 'boolean',
     image: ['reference', { only: 'Image' }],
-    imageFromDataItem: 'boolean',
     link: 'link',
   },
 })
--- src/Widgets/ImageWidget/ImageWidgetComponent.tsx
+++ src/Widgets/ImageWidget/ImageWidgetComponent.tsx
@@ -6,19 +6,13 @@ import {
   Obj,
   connect,
   Link,
-  useDataItem,
   WidgetTag,
-  DataItem,
 } from 'scrivito'
 import { alignmentClassName } from '../../utils/alignmentClassName'
-import { ImageWidget, ImageWidgetInstance } from './ImageWidgetClass'
+import { ImageWidget } from './ImageWidgetClass'
 import { ensureString } from '../../utils/ensureString'
-import { isDataBinary } from '../../utils/dataBinaryToUrl'
-import { DataBinaryImage } from '../../Components/DataBinaryImage'
 
 provideComponent(ImageWidget, ({ widget }) => {
-  const dataItem = useDataItem()
-
   const classNames = ['image-widget']
   const alignment = alignmentClassName(widget.get('alignment'))
   if (alignment) classNames.push(alignment)
@@ -26,7 +20,15 @@ provideComponent(ImageWidget, ({ widget }) => {
   return (
     <WidgetTag className={classNames.join(' ')}>
       <LinkWrapper link={widget.get('link')}>
-        <ImageComponent widget={widget} dataItem={dataItem} />
+        <ImageTag
+          alt={
+            widget.get('alternativeText') ||
+            alternativeTextFromObj(widget.get('image'))
+          }
+          className={widget.get('roundCorners') ? 'rounded' : undefined}
+          attribute="image"
+          content={widget}
+        />
       </LinkWrapper>
     </WidgetTag>
   )
@@ -43,66 +45,6 @@ const LinkWrapper = connect(function LinkWrapper({
   if (!link) return children
 
   return <LinkTag to={link}>{children}</LinkTag>
-})
-
-const ImageComponent = connect(function ImageComponent({
-  dataItem,
-  widget,
-}: {
-  dataItem?: DataItem
-  widget: ImageWidgetInstance
-}) {
-  const widgetAlternativeText = widget.get('alternativeText')
-  const className = widget.get('roundCorners') ? 'rounded' : undefined
-
-  if (!widget.get('imageFromDataItem')) {
-    return (
-      <ImageTag
-        alt={
-          widgetAlternativeText || alternativeTextFromObj(widget.get('image'))
-        }
-        className={className}
-        attribute="image"
-        content={widget}
-      />
-    )
-  }
-
-  if (!dataItem) return null
-  const attributeName = widget.get('attributeName')
-  if (!attributeName) return null
-
-  const objValue = dataItem.obj()?.get(attributeName)
-  if (objValue instanceof Obj) {
-    return (
-      <ImageTag
-        content={objValue}
-        alt={widgetAlternativeText || alternativeTextFromObj(objValue)}
-        className={className}
-      />
-    )
-  }
-
-  const attributeValue = dataItem.get(attributeName)
-  if (isDataBinary(attributeValue)) {
-    return (
-      <DataBinaryImage
-        dataBinary={attributeValue}
-        alt={widgetAlternativeText}
-        className={className}
-      />
-    )
-  }
-
-  if (typeof attributeValue !== 'string') return null
-
-  return (
-    <img
-      src={attributeValue}
-      alt={widgetAlternativeText}
-      className={className}
-    />
-  )
 })
 
 function alternativeTextFromObj(image: Obj | null): string {
