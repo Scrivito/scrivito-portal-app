d4821be265d43114c19648b42fab8e7ba59ab0a7
Alexander Pepper <alexander.pepper@justrelate.com>
2024-06-07 13:50:08 +0200

    DataAttachmentsWIdget: Remove now obsolete "attributeName"

--- src/Widgets/DataAttachmentsWidget/DataAttachmentsWidgetClass.ts
+++ src/Widgets/DataAttachmentsWidget/DataAttachmentsWidgetClass.ts
@@ -4,7 +4,6 @@ export const DataAttachmentsWidget = provideWidgetClass(
   'DataAttachmentsWidget',
   {
     attributes: {
-      attributeName: 'string',
       data: 'datalocator',
       label: 'string',
     },
