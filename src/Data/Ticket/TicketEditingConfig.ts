df1014592a5cccc2e64ef6c85632df2af1a5ecfa
Alexander Pepper <alexander.pepper@justrelate.com>
2024-06-11 14:46:17 +0200

    Convert Ticket to TicketPromise
    Diff best viewed without whitespace changes

--- src/Data/Ticket/TicketEditingConfig.ts
+++ src/Data/Ticket/TicketEditingConfig.ts
@@ -1,21 +1,23 @@
 import { provideEditingConfig } from 'scrivito'
-import { Ticket } from './TicketDataClass'
+import { TicketPromise } from './TicketDataClass'
 
-provideEditingConfig(Ticket, {
-  title: 'Ticket',
-  attributes: {
-    _id: { title: 'Ticket ID' },
-    title: { title: 'Title' },
-    description: { title: 'Description' },
-    number: { title: 'Ticket number' },
-    referenceNumber: { title: 'Reference number' },
-    type: { title: 'Type' },
-    status: { title: 'Status' },
-    open: { title: 'Open?' },
-    responsibleAgent: { title: 'Responsible agent (ID)' },
-    createdBy: { title: 'Created by (ID)' },
-    createdAt: { title: 'Created at' },
-    updatedAt: { title: 'Updated at' },
-    attachments: { title: 'Attachments' },
-  },
+TicketPromise.then((Ticket) => {
+  provideEditingConfig(Ticket, {
+    title: 'Ticket',
+    attributes: {
+      _id: { title: 'Ticket ID' },
+      title: { title: 'Title' },
+      description: { title: 'Description' },
+      number: { title: 'Ticket number' },
+      referenceNumber: { title: 'Reference number' },
+      type: { title: 'Type' },
+      status: { title: 'Status' },
+      open: { title: 'Open?' },
+      responsibleAgent: { title: 'Responsible agent (ID)' },
+      createdBy: { title: 'Created by (ID)' },
+      createdAt: { title: 'Created at' },
+      updatedAt: { title: 'Updated at' },
+      attachments: { title: 'Attachments' },
+    },
+  })
 })
