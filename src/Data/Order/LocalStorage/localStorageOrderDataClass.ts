d98326d29cad3c900aceaa881e98400ed59ba2c3
Alexander Pepper <alexander.pepper@justrelate.com>
2024-06-11 14:46:11 +0200

    Convert Order to OrderPromise
    Diff best viewed without whitespace changes

--- src/Data/Order/LocalStorage/localStorageOrderDataClass.ts
+++ src/Data/Order/LocalStorage/localStorageOrderDataClass.ts
@@ -1,6 +1,6 @@
 import { provideLocalStorageDataClass } from '../../../utils/provideLocalStorageDataClass'
 
-export function localStorageOrderDataClass() {
+export async function localStorageOrderDataClass() {
   return provideLocalStorageDataClass('Order', {
     initialContent: [
       {
