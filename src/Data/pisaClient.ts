2a928de87904c4653d546a88f4a999036d91c659
Alexander Pepper <alexander.pepper@justrelate.com>
2024-06-12 12:02:15 +0200

    pisaUrl is now async

--- src/Data/pisaClient.ts
+++ src/Data/pisaClient.ts
@@ -1,7 +1,7 @@
 import { createRestApiClient, currentLanguage, load } from 'scrivito'
 import { ensureString } from '../utils/ensureString'
 
-export function pisaUrl(): string {
+export async function pisaUrl(): Promise<string> {
   const url = ensureString(import.meta.env.PISA_URL)
   if (!url) throw new Error('PISA_URL is not set!')
 
@@ -9,7 +9,7 @@ export function pisaUrl(): string {
 }
 
 export async function pisaClient(subPath: string) {
-  const url = `${pisaUrl()}/${subPath}`
+  const url = `${await pisaUrl()}/${subPath}`
 
   const language = await load(() => currentLanguage() ?? 'en')
   const headers = { 'Accept-Language': language }
