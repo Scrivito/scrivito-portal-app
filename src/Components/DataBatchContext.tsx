d8b15444a66c8caf602f8835b54ed9683c49d398
David Császár <dcsaszar@users.noreply.github.com>
2024-06-17 11:25:10 +0200

    Add search capability to data batch context

--- src/Components/DataBatchContext.tsx
+++ src/Components/DataBatchContext.tsx
@@ -4,10 +4,14 @@ import { connect, useData, Obj, Widget, ContentTag } from 'scrivito'
 export const DataBatchContext = createContext<{
   hasMore: () => boolean
   loadMore: () => void
+  setSearch: (query: string | undefined) => void
 }>({
   hasMore: () => true,
   loadMore: () => {
     throw new Error('loadMore is not provided!')
+  },
+  setSearch: () => {
+    throw new Error('setSearch is not provided!')
   },
 })
 
@@ -23,23 +27,27 @@ export const DataBatchContextProvider = connect(
     const configuredLimit = dataScope.limit() ?? 20
     const [limit, setLimit] = useState(configuredLimit)
     const [initialLimit, setInitialLimit] = useState(configuredLimit)
+    const [search, setSearch] = useState<string | undefined>()
 
     if (initialLimit !== configuredLimit) {
       setInitialLimit(configuredLimit)
       setLimit(configuredLimit)
     }
 
+    const transform = { limit, search }
+
     const hasMore = () =>
-      dataScope.transform({ limit: limit + 1 }).take().length > limit
+      dataScope.transform({ ...transform, limit: limit + 1 }).take().length >
+      limit
 
     const loadMore = () => setLimit((prevLimit) => prevLimit + configuredLimit)
 
     return (
-      <DataBatchContext.Provider value={{ hasMore, loadMore }}>
+      <DataBatchContext.Provider value={{ hasMore, loadMore, setSearch }}>
         <ContentTag
           content={content}
           attribute={attribute}
-          dataContext={dataScope.transform({ limit })}
+          dataContext={dataScope.transform(transform)}
         />
       </DataBatchContext.Provider>
     )
