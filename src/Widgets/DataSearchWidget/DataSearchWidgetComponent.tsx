f15d02b02d7d3c209514602cf8efd2cfbb792630
David Császár <dcsaszar@users.noreply.github.com>
2024-06-17 16:33:32 +0200

    Persist data search value in URL query

--- src/Widgets/DataSearchWidget/DataSearchWidgetComponent.tsx
+++ src/Widgets/DataSearchWidget/DataSearchWidgetComponent.tsx
@@ -1,7 +1,9 @@
-import { provideComponent } from 'scrivito'
+import { currentPageParams, provideComponent, useData } from 'scrivito'
 import { DataSearchWidget } from './DataSearchWidgetClass'
-import { useContext, useRef } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { DataBatchContext } from '../../Components/DataBatchContext'
+import { getHistory } from '../../config/history'
+import { ensureString } from '../../utils/ensureString'
 
 provideComponent(DataSearchWidget, ({ widget }) => {
   const { setSearch } = useContext(DataBatchContext)
@@ -9,22 +11,19 @@ provideComponent(DataSearchWidget, ({ widget }) => {
   const buttonColor = widget.get('buttonColor') || 'btn-primary'
   const placeholder = widget.get('placeholder')
   const inputRef = useRef<HTMLInputElement>(null)
+  const paramName = useData().dataClassName() || 'search'
+  const search = ensureString(currentPageParams()[paramName]) || undefined
+
+  useEffect(() => setSearch(search), [setSearch, search])
 
   return (
-    <form
-      className="input-group mb-3"
-      role="search"
-      onSubmit={(event) => {
-        event.preventDefault()
-        if (!inputRef.current) return
-        setSearch(inputRef.current.value || undefined)
-        inputRef.current.blur()
-      }}
-    >
+    <form className="input-group mb-3" role="search" onSubmit={onSubmit}>
       <input
+        key={search}
         className="form-control"
         placeholder={placeholder}
         ref={inputRef}
+        defaultValue={search}
       />
       <span className="input-group-btn">
         <button className={`btn ${buttonColor}`} type="submit">
@@ -33,4 +32,17 @@ provideComponent(DataSearchWidget, ({ widget }) => {
       </span>
     </form>
   )
+
+  function onSubmit(event: React.FormEvent) {
+    event.preventDefault()
+
+    const history = getHistory()
+    if (!inputRef.current || !history) return
+
+    const value = inputRef.current.value || undefined
+    const searchParams = new URLSearchParams(history.location.search)
+    searchParams.delete(paramName)
+    if (value) searchParams.set(paramName, value)
+    history.push(`?${searchParams.toString()}`)
+  }
 })
