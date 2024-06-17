0ce45c4c530aeb479c260acd7d6a43b7d1eae152
Alexander Pepper <alexander.pepper@justrelate.com>
2024-06-17 13:45:25 +0200

    Revert "Scrivito js-1.41.0-rc1-494-g77baba2159"
    This reverts commit 44737df9967c0d0eb49890c710e61f7257b47acf.

--- vendor/scrivito/src/scrivito_sdk/react/hooks/use_data_scope.ts
+++
@@ -1,8 +0,0 @@
-import { DataScope } from 'scrivito_sdk/data_integration';
-import { useData } from 'scrivito_sdk/react/hooks/use_data';
-
-/** @beta */
-export function useDataScope(): DataScope | undefined {
-  const data = useData();
-  return data.isDataItem() ? undefined : data;
-}
