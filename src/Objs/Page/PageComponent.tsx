60dad5f8bf6c5f6b118f0f9c3a2dedef6539972e
David Császár <dcsaszar@users.noreply.github.com>
2024-06-19 16:58:06 +0200

    Fix: don't share the data batch context across pages

--- src/Objs/Page/PageComponent.tsx
+++ src/Objs/Page/PageComponent.tsx
@@ -2,6 +2,8 @@ import { provideComponent } from 'scrivito'
 import { Page } from './PageObjClass'
 import { DataBatchContextProvider } from '../../Components/DataBatchContext'
 
-provideComponent(Page, ({ page }) => (
-  <DataBatchContextProvider content={page} attribute="body" />
-))
+provideComponent(Page, ({ page }) => {
+  return (
+    <DataBatchContextProvider key={page.id()} content={page} attribute="body" />
+  )
+})
