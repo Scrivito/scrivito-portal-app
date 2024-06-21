086a567a3b0725e67ea5e1dce47b3895ab657a75
David Császár <dcsaszar@users.noreply.github.com>
2024-06-21 13:34:50 +0200

    Prevent accidental context reuse in data group widgets

--- src/Widgets/DataGroupWidget/DataGroupWidgetComponent.tsx
+++ src/Widgets/DataGroupWidget/DataGroupWidgetComponent.tsx
@@ -1,7 +1,13 @@
-import { provideComponent } from 'scrivito'
+import { provideComponent, useData } from 'scrivito'
 import { DataGroupWidget } from './DataGroupWidgetClass'
 import { DataBatchContextProvider } from '../../Components/DataBatchContext'
 
-provideComponent(DataGroupWidget, ({ widget }) => (
-  <DataBatchContextProvider content={widget} attribute="content" />
-))
+provideComponent(DataGroupWidget, ({ widget }) => {
+  const id = ['DataGroupWidget', widget.id(), useData().dataItem()?.id()].join(
+    '-',
+  )
+
+  return (
+    <DataBatchContextProvider key={id} content={widget} attribute="content" />
+  )
+})
