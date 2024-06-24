beb34f1e4fc8f8cc8a5b9b3adc566b282e5c125f
Alexander Pepper <alexander.pepper@justrelate.com>
2024-06-24 12:24:07 +0200

    Homepage lets you define custom colors

--- src/Components/DesignAdjustments.tsx
+++ src/Components/DesignAdjustments.tsx
@@ -8,6 +8,26 @@ export const DesignAdjustments = connect(function DesignAdjustments() {
 
   const styles: string[] = []
 
+  const primary = root.get('siteColorPrimary')
+  if (primary) styles.push(`--bs-primary: ${primary};`)
+
+  const primaryLighten = root.get('siteColorPrimaryLighten')
+  if (primaryLighten) styles.push(`--bs-primary-lighten: ${primaryLighten};`)
+
+  const primaryDarken = root.get('siteColorPrimaryDarken')
+  if (primaryDarken) styles.push(`--bs-primary-darken: ${primaryDarken};`)
+
+  const secondary = root.get('siteColorSecondary')
+  if (secondary) styles.push(`--bs-secondary: ${secondary};`)
+
+  const secondaryLighten = root.get('siteColorSecondaryLighten')
+  if (secondaryLighten) {
+    styles.push(`--bs-secondary-lighten: ${secondaryLighten};`)
+  }
+
+  const secondaryDarken = root.get('siteColorSecondaryDarken')
+  if (secondaryDarken) styles.push(`--bs-secondary-darken: ${secondaryDarken};`)
+
   const dropShadow = root.get('siteDropShadow')
   if (!dropShadow) styles.push('--jr-card-box-shadow: none;')
 
