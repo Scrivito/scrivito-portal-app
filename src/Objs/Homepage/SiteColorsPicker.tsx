e3f04a84e36ad52ea7504406bfc669e01934b0e9
Alexander Pepper <alexander.pepper@justrelate.com>
2024-06-27 11:04:15 +0200

    SiteColorsPicker: Simplify color cache

--- src/Objs/Homepage/SiteColorsPicker.tsx
+++ src/Objs/Homepage/SiteColorsPicker.tsx
@@ -1,9 +1,9 @@
 import Color from 'color'
-import { ColorPicker, ColorService } from 'react-color-palette'
+import { ColorPicker, useColor } from 'react-color-palette'
 import { connect, uiContext } from 'scrivito'
 import { HomepageInstance } from './HomepageObjClass'
 import './SiteColorsPicker.scss'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export const SiteColorsPicker = connect(function SiteColorsPicker({
   page,
@@ -61,39 +61,18 @@ function AdvancedColorPicker({
   color: string
   setColor: (color: string) => void
 }) {
-  const [iColor, setIColor] = useState(ColorService.convert('hex', color))
-
-  const [hexHsvCache, setHexHsvCache] = useState<
-    Record<
-      string,
-      {
-        readonly h: number
-        readonly s: number
-        readonly v: number
-        readonly a: number
-      }
-    >
-  >({})
-
-  useEffect(() => {
-    const cachedHsv = hexHsvCache[color]
-    const newIColor = cachedHsv
-      ? ColorService.convert('hsv', cachedHsv)
-      : ColorService.convert('hex', color)
-
-    setIColor(newIColor)
-  }, [color, hexHsvCache])
+  const [iColor] = useColor(color)
+  const [highResIColor, setHighResIColor] = useState(iColor)
 
   return (
     <ColorPicker
       height={100}
-      color={iColor}
+      color={iColor.hex === highResIColor.hex ? highResIColor : iColor}
       hideInput={['hsv']}
       hideAlpha={true}
       onChange={(newIColor) => {
-        const newColor = newIColor.hex
-        setHexHsvCache((prev) => ({ ...prev, [newColor]: newIColor.hsv }))
-        setColor(newColor)
+        setHighResIColor(newIColor)
+        setColor(newIColor.hex)
       }}
     />
   )
