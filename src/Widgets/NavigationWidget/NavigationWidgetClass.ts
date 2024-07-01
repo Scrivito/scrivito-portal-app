eb9e585769af3a7017f48af0e1d5aee42cea6e21
Alexander Pepper <alexander.pepper@justrelate.com>
2024-07-01 11:54:49 +0200

    Navigation: logOutLabel is now translated code wise

--- src/Widgets/NavigationWidget/NavigationWidgetClass.ts
+++ src/Widgets/NavigationWidget/NavigationWidgetClass.ts
@@ -5,7 +5,6 @@ export const NavigationWidget = provideWidgetClass('NavigationWidget', {
     metaNavigationObjs: 'referencelist',
     metaNavigationUserDescription: 'string',
     metaNavigationUserTitle: 'string',
-    logOutLabel: 'string',
   },
 })
 
--- src/Widgets/NavigationWidget/NavigationWidgetEditingConfig.ts
+++ src/Widgets/NavigationWidget/NavigationWidgetEditingConfig.ts
@@ -3,9 +3,7 @@ import { NavigationWidget } from './NavigationWidgetClass'
 import Thumbnail from './thumbnail.svg'
 
 provideEditingConfig(NavigationWidget, {
-  attributes: { logOutLabel: { title: 'Log-out menu item label' } },
   title: 'Navigation',
   thumbnail: Thumbnail,
-  properties: ['metaNavigationObjs', 'logOutLabel'],
-  initialContent: { logOutLabel: 'Log out' },
+  properties: ['metaNavigationObjs'],
 })
