34be7dbc85a9660520122d78516389cf4e06c31c
David Császár <dcsaszar@users.noreply.github.com>
2024-07-08 17:50:14 +0200

    Make submit indicator block all input
    If the component was inside a link, clicking still followed the link.

--- src/Components/Submitting.tsx
+++ src/Components/Submitting.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useRef } from 'react'
 import { Modal } from 'react-bootstrap'
 import { connect } from 'scrivito'
 
 export const Submitting = connect(function Submitting() {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  useEffect(() => dialogRef.current?.showModal(), [])
+
   return (
-    <Modal
-      backdrop="static"
-      centered
-      dialogAs={() => <div className="loader" />}
-      fullscreen
-      show
-    />
+    <>
+      <dialog className="d-none" ref={dialogRef} />
+      <Modal
+        backdrop="static"
+        centered
+        dialogAs={() => <div className="loader" />}
+        fullscreen
+        keyboard={false}
+        show
+      />
+    </>
   )
 })
