2236d3a9777a66ee2d07275d6473157110e37bdf
agessler <alexander.gessler@infopark.de>
2024-07-15 11:17:21 +0200

    ModalSpinner: better style

--- src/Components/ModalSpinner.tsx
+++ src/Components/ModalSpinner.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react'
-import { Modal } from 'react-bootstrap'
 import { connect } from 'scrivito'
 
 export const ModalSpinner = connect(function ModalSpinner() {
@@ -7,16 +6,9 @@ export const ModalSpinner = connect(function ModalSpinner() {
   useEffect(() => dialogRef.current?.showModal(), [])
 
   return (
-    <>
-      <dialog className="d-none" ref={dialogRef} />
-      <Modal
-        backdrop="static"
-        centered
-        dialogAs={() => <div className="loader" />}
-        fullscreen
-        keyboard={false}
-        show
-      />
-    </>
+    <dialog className="loader-dailog" ref={dialogRef}>
+      <div className="fade modal-backdrop show"></div>
+      <div className="loader" />
+    </dialog>
   )
 })
