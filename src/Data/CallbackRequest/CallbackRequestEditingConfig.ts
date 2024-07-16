7c77ee72322cd1a3446be6dbe4f2b7f21b89d143
Alexander Pepper <alexander.pepper@justrelate.com>
2024-07-16 11:59:19 +0200

    CallbackRequest: Corrected attribute _id

--- src/Data/CallbackRequest/CallbackRequestEditingConfig.ts
+++ src/Data/CallbackRequest/CallbackRequestEditingConfig.ts
@@ -5,7 +5,7 @@ CallbackRequestPromise.then((CallbackRequest) => {
   provideEditingConfig(CallbackRequest, {
     title: 'Callback request',
     attributes: {
-      _id: { title: 'Contract ID' },
+      _id: { title: 'Callback request ID' },
       message: { title: 'Message' },
     },
   })
