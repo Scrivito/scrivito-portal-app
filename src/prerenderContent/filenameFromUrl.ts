90f29e0f4974f8fff948906b7de46aab715e845e
David Császár <dcsaszar@users.noreply.github.com>
2024-07-23 17:46:47 +0200

    Support Netlify filename scheme
    In contrast to the default implementation of the
    Example App we now require `NETLIFY=true` for local testing.

--- src/prerenderContent/filenameFromUrl.ts
+++ src/prerenderContent/filenameFromUrl.ts
@@ -1,6 +1,8 @@
+const preferExtension = process.env.NETLIFY === 'true'
+
 export function filenameFromUrl(url: string): string {
   const { pathname } = new URL(url)
   if (pathname === '/') return '/index.html'
 
-  return `${pathname}/index.html`
+  return preferExtension ? `${pathname}.html` : `${pathname}/index.html`
 }
