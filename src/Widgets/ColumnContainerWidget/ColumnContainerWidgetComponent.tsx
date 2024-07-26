c0aa2ae7b4b90e95ddecab81bc55fae7c28b4bef
David Császár <dcsaszar@users.noreply.github.com>
2024-07-26 18:28:08 +0200

    Flex column rendering basics

--- src/Widgets/ColumnContainerWidget/ColumnContainerWidgetComponent.tsx
+++ src/Widgets/ColumnContainerWidget/ColumnContainerWidgetComponent.tsx
@@ -6,18 +6,33 @@ provideComponent(ColumnContainerWidget, ({ widget }) => {
   if (!columns.length) return null
 
   const alignment = widget.get('alignment') || 'start'
+  const isFlex = widget.get('layoutMode') === 'flex'
 
-  const classNames = ['row', `align-items-${alignment}`]
+  const classNames = [`align-items-${alignment}`, isFlex ? 'd-flex' : 'row']
 
   return (
     <div className={classNames.join(' ')}>
       {columns.map((columnWidget) => {
+        const key = columnWidget.id()
+
+        if (isFlex) {
+          const className = 'flex-grow-0'
+          return (
+            <ContentTag
+              key={key}
+              className={className}
+              content={columnWidget}
+              attribute="content"
+            />
+          )
+        }
+
         const colSize = columnWidget.get('colSize') || 1
         const className = widget.get('disableResponsiveAdaption')
           ? `col-${colSize}`
           : `col-md-${colSize}`
         return (
-          <div key={columnWidget.id()} className={className}>
+          <div key={key} className={className}>
             <ContentTag
               content={columnWidget}
               attribute="content"
