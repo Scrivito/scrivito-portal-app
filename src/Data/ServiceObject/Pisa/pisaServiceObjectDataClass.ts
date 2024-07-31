9af07400689af84025df20a1c0030b89898528ba
Alexander Pepper <alexander.pepper@justrelate.com>
2024-07-31 10:26:57 +0200

    Pisa ServiceObject uses simple restApi

--- src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
+++ src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
@@ -1,18 +1,9 @@
 import { provideDataClass } from 'scrivito'
-import { pisaClient } from '../../pisaClient'
-import { toClientParams } from '../../toClientParams'
-import { DataConnection, DataIndexResponse } from '../../types'
+import { pisaConfig } from '../../pisaClient'
 
 export function pisaServiceObjectDataClass() {
   return provideDataClass('ServiceObject', {
-    connection: pisaClient('service-object').then(
-      (apiClient): DataConnection => ({
-        index: (params) =>
-          apiClient.get('', {
-            params: toClientParams(params),
-          }) as Promise<DataIndexResponse>,
-        get: (id) => apiClient.get(id),
-      }),
-    ),
+    restApi: pisaConfig('service-object'),
+    attributes: {},
   })
 }
