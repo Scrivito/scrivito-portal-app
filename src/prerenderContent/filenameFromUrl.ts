43ffe3a74e5d955542c4335853ceec07b2eadd43
David Császár <dcsaszar@users.noreply.github.com>
2024-07-31 17:23:54 +0200

    Toggle default prerender filename scheme
    This way, we get out of the box:
    * local and Netlify builds are identical
    * `npm run prerender && npm run prerender-preview` uses hydration

--- src/prerenderContent/filenameFromUrl.ts
+++ src/prerenderContent/filenameFromUrl.ts
@@ -1,8 +1,12 @@
-const preferExtension = process.env.NETLIFY === 'true'
+const preferIndex =
+  // https://developers.cloudflare.com/pages/configuration/build-configuration/
+  process.env.CF_PAGES === '1' ||
+  // https://docs.aws.amazon.com/codebuild/latest/userguide/use-codebuild-agent.html
+  !!process.env.CODEBUILD_BUILD_ID
 
 export function filenameFromUrl(url: string): string {
   const { pathname } = new URL(url)
   if (pathname === '/') return '/index.html'
 
-  return preferExtension ? `${pathname}.html` : `${pathname}/index.html`
+  return preferIndex ? `${pathname}/index.html` : `${pathname}.html`
 }
