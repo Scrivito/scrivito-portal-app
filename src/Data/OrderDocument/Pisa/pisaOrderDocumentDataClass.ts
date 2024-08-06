f01f11cc08aded80ccfbbe602215f7138edc6341
Alexander Pepper <alexander.pepper@justrelate.com>
2024-08-06 14:49:23 +0200

    OrderDocument: provide string attributes

--- src/Data/OrderDocument/Pisa/pisaOrderDocumentDataClass.ts
+++ src/Data/OrderDocument/Pisa/pisaOrderDocumentDataClass.ts
@@ -1,9 +1,10 @@
 import { provideDataClass } from 'scrivito'
 import { pisaConfig } from '../../pisaClient'
+import { DataClassAttributes } from '../../types'
 
-export function pisaOrderDocumentDataClass() {
+export function pisaOrderDocumentDataClass(attributes: DataClassAttributes) {
   return provideDataClass('OrderDocument', {
     restApi: pisaConfig('order-document'),
-    attributes: {},
+    attributes,
   })
 }
