3bff70c452e7725bed6aac2a608f8bb8841e3ee7
Alexander Pepper <alexander.pepper@justrelate.com>
2024-08-06 14:49:17 +0200

    EventDocument: provide string attributes

--- src/Data/EventDocument/Pisa/pisaEventDocumentDataClass.ts
+++ src/Data/EventDocument/Pisa/pisaEventDocumentDataClass.ts
@@ -1,9 +1,10 @@
 import { provideDataClass } from 'scrivito'
 import { pisaConfig } from '../../pisaClient'
+import { DataClassAttributes } from '../../types'
 
-export function pisaEventDocumentDataClass() {
+export function pisaEventDocumentDataClass(attributes: DataClassAttributes) {
   return provideDataClass('EventDocument', {
     restApi: pisaConfig('event-document'),
-    attributes: {},
+    attributes,
   })
 }
