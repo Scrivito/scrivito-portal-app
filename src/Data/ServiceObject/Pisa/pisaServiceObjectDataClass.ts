b1895d3346ae70834daa0ff11c2f79f471abe689
Alexander Pepper <alexander.pepper@justrelate.com>
2024-08-06 14:49:27 +0200

    ServiceObject: provide string attributes

--- src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
+++ src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
@@ -1,9 +1,10 @@
 import { provideDataClass } from 'scrivito'
 import { pisaConfig } from '../../pisaClient'
+import { DataClassAttributes } from '../../types'
 
-export function pisaServiceObjectDataClass() {
+export function pisaServiceObjectDataClass(attributes: DataClassAttributes) {
   return provideDataClass('ServiceObject', {
     restApi: pisaConfig('service-object'),
-    attributes: {},
+    attributes,
   })
 }
--- src/Data/ServiceObject/ServiceObjectDataClass.ts
+++ src/Data/ServiceObject/ServiceObjectDataClass.ts
@@ -1,6 +1,26 @@
+import { DataClassAttributes } from '../types'
 import { localStorageServiceObjectDataClass } from './LocalStorage/localStorageServiceObjectDataClass'
 import { pisaServiceObjectDataClass } from './Pisa/pisaServiceObjectDataClass'
 
+const attributes: DataClassAttributes = {
+  carrier: 'string',
+  customer: 'string',
+  information: 'string',
+  keyword: 'string',
+  locationCity: 'string',
+  locationCountry: 'string', // TODO: convert to enum, once fully available
+  locationPostalCode: 'string',
+  locationStreet: 'string',
+  modelNumber: 'string',
+  number: 'string',
+  parentId: 'string', // TODO: convert to reference, once fully available
+  product: 'string',
+  responsibleAgent: 'string', // TODO: convert to reference, once fully available
+  serialNumber: 'string',
+  status: 'string', // TODO: convert to enum, once fully available
+  supplier: 'string',
+}
+
 export const ServiceObject = import.meta.env.ENABLE_PISA
-  ? pisaServiceObjectDataClass()
-  : localStorageServiceObjectDataClass()
+  ? pisaServiceObjectDataClass(attributes)
+  : localStorageServiceObjectDataClass(attributes)
