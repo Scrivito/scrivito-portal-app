f8617dcdc93b6c500a9ca9dc794e0d29d0ee97c0
Alexander Pepper <alexander.pepper@justrelate.com>
2024-08-06 14:49:11 +0200

    CallbackRequest: provide string attributes

--- src/Data/CallbackRequest/CallbackRequestDataClass.ts
+++ src/Data/CallbackRequest/CallbackRequestDataClass.ts
@@ -1,6 +1,11 @@
+import { DataClassAttributes } from '../types'
 import { localStorageCallbackRequestDataClass } from './LocalStorage/localStorageCallbackRequestDataClass'
 import { pisaCallbackRequestDataClass } from './Pisa/pisaCallbackRequestDataClass'
 
+const attributes: DataClassAttributes = {
+  message: 'string',
+}
+
 export const CallbackRequest = import.meta.env.ENABLE_PISA
-  ? pisaCallbackRequestDataClass()
-  : localStorageCallbackRequestDataClass()
+  ? pisaCallbackRequestDataClass(attributes)
+  : localStorageCallbackRequestDataClass(attributes)
--- src/Data/CallbackRequest/LocalStorage/localStorageCallbackRequestDataClass.ts
+++ src/Data/CallbackRequest/LocalStorage/localStorageCallbackRequestDataClass.ts
@@ -1,5 +1,8 @@
 import { provideLocalStorageDataClass } from '../../../utils/provideLocalStorageDataClass'
+import { DataClassAttributes } from '../../types'
 
-export function localStorageCallbackRequestDataClass() {
-  return provideLocalStorageDataClass('CallbackRequest')
+export function localStorageCallbackRequestDataClass(
+  attributes: DataClassAttributes,
+) {
+  return provideLocalStorageDataClass('CallbackRequest', { attributes })
 }
