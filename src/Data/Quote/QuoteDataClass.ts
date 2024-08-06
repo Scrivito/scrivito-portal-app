9570fc44d9acad54471e80f53805d9cd5f0fb4ce
Alexander Pepper <alexander.pepper@justrelate.com>
2024-08-06 14:49:25 +0200

    Quote: provide string attributes

--- src/Data/Quote/QuoteDataClass.ts
+++ src/Data/Quote/QuoteDataClass.ts
@@ -1,6 +1,23 @@
+import { DataClassAttributes } from '../types'
 import { localQuoteDataClass } from './LocalStorage/localQuoteDataClass'
 import { pisaQuoteDataClass } from './Pisa/pisaQuoteDataClass'
 
+const attributes: DataClassAttributes = {
+  commercialAgent: 'string', // TODO: convert to reference, once fully available
+  customer: 'string',
+  description: 'string',
+  keyword: 'string',
+  mainStatus: 'string', // TODO: convert to enum, once fully available
+  number: 'string',
+  salesPartner: 'string',
+  status: 'string', // TODO: convert to enum, once fully available
+  technicalAgent: 'string', // TODO: convert to reference, once fully available
+  termsOfDelivery: 'string', // TODO: convert to enum, once fully available
+  termsOfPayment: 'string', // TODO: convert to enum, once fully available
+  totalPriceCurrency: 'string',
+  type: 'string', // TODO: convert to enum, once fully available
+}
+
 export const Quote = import.meta.env.ENABLE_PISA
-  ? pisaQuoteDataClass()
-  : localQuoteDataClass()
+  ? pisaQuoteDataClass(attributes)
+  : localQuoteDataClass(attributes)
