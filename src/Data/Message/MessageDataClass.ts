e9dd1c14ea31f5c138f20ecb9582d3be49266490
Alexander Pepper <alexander.pepper@justrelate.com>
2024-08-06 14:49:20 +0200

    Message: provide string attributes

--- src/Data/Message/MessageDataClass.ts
+++ src/Data/Message/MessageDataClass.ts
@@ -1,6 +1,13 @@
+import { DataClassAttributes } from '../types'
 import { localStorageMessageDataClass } from './LocalStorage/localStorageMessageDataClass'
 import { pisaMessageDataClass } from './Pisa/pisaMessageDataClass'
 
+const attributes: DataClassAttributes = {
+  createdBy: 'string', // TODO: convert to reference, once fully available
+  subjectId: 'string', // TODO: convert to reference, once fully available
+  text: 'string',
+}
+
 export const Message = import.meta.env.ENABLE_PISA
-  ? pisaMessageDataClass()
-  : localStorageMessageDataClass()
+  ? pisaMessageDataClass(attributes)
+  : localStorageMessageDataClass(attributes)
