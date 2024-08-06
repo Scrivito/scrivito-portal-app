34aff5eb00b7642212d4b592347156107a2baa13
Alexander Pepper <alexander.pepper@justrelate.com>
2024-08-06 14:49:18 +0200

    EventRegistration: provide string attributes

--- src/Data/EventRegistration/EventRegistrationDataClass.ts
+++ src/Data/EventRegistration/EventRegistrationDataClass.ts
@@ -1,6 +1,11 @@
+import { DataClassAttributes } from '../types'
 import { localStorageEventRegistrationDataClass } from './LocalStorage/localStorageEventRegistrationDataClass'
 import { pisaEventRegistrationDataClass } from './Pisa/pisaEventRegistrationDataClass'
 
+const attributes: DataClassAttributes = {
+  eventId: 'string', // TODO: convert to reference, once fully available
+}
+
 export const EventRegistration = import.meta.env.ENABLE_PISA
-  ? pisaEventRegistrationDataClass()
-  : localStorageEventRegistrationDataClass()
+  ? pisaEventRegistrationDataClass(attributes)
+  : localStorageEventRegistrationDataClass(attributes)
