92500a042b9f453bd98c7be0764766f5b421ae36
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-03 13:55:45 +0200

    DataFormUploadWidget: Allow to drag 'n' drop files
    Plus a nice preview of the selected files.
    
    See issue https://github.com/react-dropzone/react-dropzone/issues/131 for details on "useEffect" and "DataTransfer".

--- src/Widgets/DataFormUploadWidget/DataFormUploadWidgetComponent.tsx
+++ src/Widgets/DataFormUploadWidget/DataFormUploadWidgetComponent.tsx
@@ -1,11 +1,37 @@
-import { ContentTag, InPlaceEditingOff, provideComponent } from 'scrivito'
+import {
+  ContentTag,
+  currentLanguage,
+  InPlaceEditingOff,
+  provideComponent,
+} from 'scrivito'
 import { DataFormUploadWidget } from './DataFormUploadWidgetClass'
 import { OverlayTrigger, Popover } from 'react-bootstrap'
+import { useDropzone } from 'react-dropzone'
+import { Attachment } from '../../Components/Attachment'
+import { useEffect } from 'react'
 
 provideComponent(DataFormUploadWidget, ({ widget }) => {
   const id = ['DataFormUploadWidget', widget.id()].join('-')
-
   const attributeName = widget.get('attributeName')
+
+  const { acceptedFiles, getRootProps, getInputProps, inputRef } = useDropzone()
+
+  useEffect(() => {
+    if (!inputRef.current) return
+
+    const dataTransfer = new DataTransfer()
+    acceptedFiles.forEach((file) => dataTransfer.items.add(file))
+
+    inputRef.current.files = dataTransfer.files
+  }, [acceptedFiles, inputRef])
+
+  const attachments = acceptedFiles.map((file) => ({
+    _id: file.name,
+    contentLength: file.size,
+    contentType: file.type,
+    file,
+    filename: file.name,
+  }))
 
   return (
     <div className="mb-3" key={[id, attributeName].join('-')}>
@@ -50,14 +76,33 @@ provideComponent(DataFormUploadWidget, ({ widget }) => {
         </>
       ) : null}
 
-      <input
-        className="form-control"
-        type="file"
-        id={id}
-        multiple={widget.get('multiple')}
-        name={attributeName}
-        required={widget.get('required')}
-      />
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input
+          {...getInputProps({
+            id,
+            multiple: widget.get('multiple'),
+            name: attributeName,
+            required: widget.get('required'),
+          })}
+        />
+        {getDropMessage(widget.get('multiple'))}
+      </div>
+      <div>
+        <div className="d-flex flex-wrap mt-2 gap-2">
+          {attachments.map((attachment) => (
+            <Attachment attachment={attachment} key={attachment._id} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 })
+
+function getDropMessage(multiple: boolean) {
+  switch (currentLanguage()) {
+    case 'de':
+      return `Wählen oder legen Sie ${multiple ? 'Dateien' : 'eine Datei'} hier ab.`
+    default:
+      return `Choose or drop ${multiple ? 'files' : 'a file'} here.`
+  }
+}
