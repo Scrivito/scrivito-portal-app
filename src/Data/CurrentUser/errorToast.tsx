55ad21409cf8f1b5a3af9ca8c5cc7ea123c761bf
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-09 15:27:38 +0200

    errorToast: Bugfix - avoid "endless" render with too long messages

--- src/Data/CurrentUser/errorToast.tsx
+++ src/Data/CurrentUser/errorToast.tsx
@@ -1,15 +1,15 @@
+import { truncate } from 'lodash-es'
 import { toast } from 'react-toastify'
 import { ClientError } from 'scrivito'
 
 export function errorToast(title: string, error: unknown): void {
+  const message =
+    error instanceof Error ? error.message : JSON.stringify(error, null, 1)
+
   toast.error(
     <>
       <div>{title}</div>
-      <small>
-        {error instanceof Error
-          ? error.message
-          : JSON.stringify(error, null, 1)}
-      </small>
+      <small>{truncate(message, { length: 300, separator: /,? +/ })}</small>
       {error instanceof ClientError ? (
         <pre className="text-small text-muted">
           {JSON.stringify(error.details, null, 1)}
