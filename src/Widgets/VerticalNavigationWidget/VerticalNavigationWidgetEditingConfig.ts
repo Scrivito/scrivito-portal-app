cd6fac16e0ddf34f7f23fa9f43bee8c75ed778bb
David Császár <dcsaszar@users.noreply.github.com>
2024-09-13 15:08:38 +0200

    Introduce nav depth setting for vertical nav

--- src/Widgets/VerticalNavigationWidget/VerticalNavigationWidgetEditingConfig.ts
+++ src/Widgets/VerticalNavigationWidget/VerticalNavigationWidgetEditingConfig.ts
@@ -5,4 +5,17 @@ import Thumbnail from './thumbnail.svg'
 provideEditingConfig(VerticalNavigationWidget, {
   title: 'Vertical Navigation',
   thumbnail: Thumbnail,
+  attributes: {
+    navigationDepth: {
+      title: 'Navigation depth',
+      description: 'Default: Top-level only',
+      values: [
+        { value: '0', title: 'Top-level only' },
+        { value: '1', title: 'Two levels' },
+        { value: '2', title: 'Three levels' },
+      ],
+    },
+  },
+  properties: ['navigationDepth'],
+  initialContent: { navigationDepth: '0' },
 })
