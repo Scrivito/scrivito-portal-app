3999373b8e24fd11074d8d49db048041e60e998b
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-17 11:34:28 +0200

    DataFormUploadWidget: Make use of attribute from datalocator

--- src/Widgets/DataFormUploadWidget/DataFormUploadWidgetClass.ts
+++ src/Widgets/DataFormUploadWidget/DataFormUploadWidgetClass.ts
@@ -3,6 +3,7 @@ import { provideWidgetClass } from 'scrivito'
 export const DataFormUploadWidget = provideWidgetClass('DataFormUploadWidget', {
   attributes: {
     attributeName: 'string',
+    data: 'datalocator',
     helpText: 'html',
     label: 'string',
     multiple: 'boolean',
