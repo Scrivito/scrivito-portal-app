07da56a96a75ee5a5c2707296c32cb4357707a60
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-17 11:34:29 +0200

    Added (hacky) migrateAttributeNameToData
    For data attachments and icon widgets
    
    Usage: In a working copy in the JS console execute `await migrateAttributeNameToData()`.

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -2,6 +2,7 @@ import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets'
 import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
 })
+import './migrateAttributeNameToData'
 
 initNeoletterFormWidgets()
 export {}
