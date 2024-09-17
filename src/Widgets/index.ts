53a28ab802a66632a15fdd6fb7d8e5ebade1e721
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-17 11:34:30 +0200

    Revert "Added (hacky) migrateAttributeNameToData"
    This reverts commit 5ef348a126fc2edc4b237bc1ee70a0a0ee1ebf94.

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -2,7 +2,6 @@ import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets'
 import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
 })
-import './migrateAttributeNameToData'
 
 initNeoletterFormWidgets()
 export {}
