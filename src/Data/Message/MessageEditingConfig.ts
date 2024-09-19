477281fa8246198156fbc455d649cd8cdfdbbb22
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-19 14:03:01 +0200

    MessageDataClass: Provide attribute title via schema

--- src/Data/Message/MessageEditingConfig.ts
+++ src/Data/Message/MessageEditingConfig.ts
@@ -4,11 +4,8 @@ import { Message } from './MessageDataClass'
 provideEditingConfig(Message, {
   title: 'Message',
   attributes: {
-    _id: { title: 'Message ID' },
-    subjectId: { title: 'Subject ID' },
-    text: { title: 'Text' },
-    createdBy: { title: 'Created by (ID)' },
-    createdAt: { title: 'Created at' },
     attachments: { title: 'Attachments' },
+    createdBy: { title: 'Created by (ID)' },
+    subjectId: { title: 'Subject ID' },
   },
 })
