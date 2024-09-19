9075e212be3b05001bf3453c7e2ed53df06bdaf2
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-19 14:03:02 +0200

    UserDataClass: Provide attribute title via schema

--- src/Data/User/UserEditingConfig.ts
+++ src/Data/User/UserEditingConfig.ts
@@ -4,14 +4,7 @@ import { User } from './UserDataClass'
 provideEditingConfig(User, {
   title: 'User',
   attributes: {
-    _id: { title: 'User ID' },
-    name: { title: 'Name (given and family name)' },
-    email: { title: 'Email' },
-    position: { title: 'Position' },
     image: { title: 'Image' },
     staff: { title: 'Staff?' },
-    salutation: { title: 'Salutation' },
-    givenName: { title: 'Given name' },
-    familyName: { title: 'Family name' },
   },
 })
