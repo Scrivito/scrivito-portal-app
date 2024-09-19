47dc87b702f678e39cc5e0660b8ecdd0df23171f
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-19 11:26:25 +0200

    DocumentDataClass: Provide attribute title via schema

--- src/Data/Document/DocumentEditingConfig.ts
+++ src/Data/Document/DocumentEditingConfig.ts
@@ -4,15 +4,6 @@ import { Document } from './DocumentDataClass'
 provideEditingConfig(Document, {
   title: 'Document',
   attributes: {
-    _id: { title: 'Document ID' },
-    title: { title: 'Title' },
     body: { title: 'Body' },
-    number: { title: 'Number' },
-    type: { title: 'Type' },
-    createdAt: { title: 'Created at' },
-    language: { title: 'Language' },
-    format: { title: 'Format' },
-    size: { title: 'Size' },
-    version: { title: 'Version' },
   },
 })
