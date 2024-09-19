a1e2bda4577ea2cad7dbace7a9f8896fb7129942
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-19 13:45:36 +0200

    GdprDataClass: Provide attribute title via schema

--- src/Data/Gdpr/GdprDataClass.ts
+++ src/Data/Gdpr/GdprDataClass.ts
@@ -1,10 +1,19 @@
-import { DataClassAttributes } from '../types'
+import { currentLanguage, load } from 'scrivito'
+import { DataClassSchema } from '../types'
 import { localStorageGdprDataClass } from './LocalStorage/localStorageGdprDataClass'
 import { pisaGdprDataClass } from './Pisa/pisaGdprDataClass'
 
-const attributes: DataClassAttributes = {
-  description: 'string',
-  name: 'string',
+async function attributes(): Promise<DataClassSchema> {
+  const lang = await load(currentLanguage)
+
+  return {
+    _id: ['string', { title: 'ID' }],
+    description: [
+      'string',
+      { title: lang === 'de' ? 'Beschreibung' : 'Description' },
+    ],
+    name: ['string', { title: 'Name' }],
+  }
 }
 
 export const Gdpr = import.meta.env.ENABLE_PISA
