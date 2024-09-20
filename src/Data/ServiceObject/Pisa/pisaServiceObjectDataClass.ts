fb22af973a71a2a9eb5ecadf2a3daa17fa827336
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-20 13:40:12 +0200

    ServiceObjectData: Fetch schema from Pisa

--- src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
+++ src/Data/ServiceObject/Pisa/pisaServiceObjectDataClass.ts
@@ -1,10 +1,10 @@
 import { provideDataClass } from 'scrivito'
 import { pisaConfig } from '../../pisaClient'
-import { DataClassAttributes } from '../../types'
+import { filterSchema } from '../../filterSchema'
 
-export function pisaServiceObjectDataClass(attributes: DataClassAttributes) {
+export function pisaServiceObjectDataClass() {
   return provideDataClass('ServiceObject', {
     restApi: pisaConfig('service-object'),
-    attributes,
+    attributes: () => filterSchema('service-object'),
   })
 }
