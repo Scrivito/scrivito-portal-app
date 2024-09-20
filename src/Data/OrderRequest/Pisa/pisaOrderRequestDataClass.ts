75a00d33fc6cdbd9ffd299509c1ea86d624c3972
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-20 13:40:20 +0200

    OrderRequestData: Fetch schema from Pisa

--- src/Data/OrderRequest/Pisa/pisaOrderRequestDataClass.ts
+++ src/Data/OrderRequest/Pisa/pisaOrderRequestDataClass.ts
@@ -1,10 +1,10 @@
 import { provideDataClass } from 'scrivito'
 import { pisaConfig } from '../../pisaClient'
-import { DataClassAttributes } from '../../types'
+import { filterSchema } from '../../filterSchema'
 
-export function pisaOrderRequestDataClass(attributes: DataClassAttributes) {
+export function pisaOrderRequestDataClass() {
   return provideDataClass('OrderRequest', {
     restApi: pisaConfig('order-request'),
-    attributes,
+    attributes: () => filterSchema('order-request'),
   })
 }
