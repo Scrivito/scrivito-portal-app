d18fcee8348df8799906865b52be75e44b484708
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-23 09:36:03 +0200

    filterSchema: No longer "correct" enum values

--- src/Data/filterSchema.ts
+++ src/Data/filterSchema.ts
@@ -4,7 +4,6 @@ import { DataClassAttributes } from './types'
 /**
  * Fetches the schema for `subPath`.
  * Filters out boolean and reference attributes, since they are currently not fully supported.
- * It also converts enum values, if in a wrong format.
  **/
 export async function filterSchema(subPath: string) {
   const client = await pisaClient(subPath)
@@ -13,40 +12,8 @@ export async function filterSchema(subPath: string) {
   }
 
   return Object.fromEntries(
-    Object.entries(schema.attributes)
-      .filter(
-        ([_, attribute]) => !['boolean', 'reference'].includes(attribute[0]),
-      )
-      .map(([key, value]) => [
-        key,
-        value[0] === 'enum'
-          ? ['enum', correctWrongEnumValues(value[1])]
-          : value,
-      ]),
-  )
-}
-
-type LocalizedEnumValueConfig = {
-  value: string
-  title: string
-}
-
-type EnumValueConfig = string | LocalizedEnumValueConfig
-
-function correctWrongEnumValues({
-  values,
-  title,
-}: {
-  values: EnumValueConfig[]
-  title?: string
-}): {
-  values: LocalizedEnumValueConfig[]
-  title?: string
-} {
-  return {
-    values: values.map((value) =>
-      typeof value === 'string' ? { value, title: value } : value,
+    Object.entries(schema.attributes).filter(
+      ([_, attribute]) => !['boolean', 'reference'].includes(attribute[0]),
     ),
-    title,
-  }
+  )
 }
