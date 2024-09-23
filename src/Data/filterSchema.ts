a71142241c21c7adfd4c404112d983ab7648b4b1
Alexander Pepper <alexander.pepper@justrelate.com>
2024-09-23 09:40:56 +0200

    filterSchema: Added return type

--- src/Data/filterSchema.ts
+++ src/Data/filterSchema.ts
@@ -1,11 +1,13 @@
 import { pisaClient } from './pisaClient'
-import { DataClassAttributes } from './types'
+import { DataClassAttributes, DataClassSchema } from './types'
 
 /**
  * Fetches the schema for `subPath`.
  * Filters out boolean and reference attributes, since they are currently not fully supported.
  **/
-export async function filterSchema(subPath: string) {
+export async function filterSchema(
+  subPath: string,
+): Promise<DataClassSchema> {
   const client = await pisaClient(subPath)
   const schema = (await client.get('schema')) as {
     attributes: DataClassAttributes
