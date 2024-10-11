3265d2dd724966cd0de568bdb36e19bdde7e5ce6
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-11 13:28:33 +0200

    Order: Added reference attributes

--- src/Data/Order/Pisa/pisaOrderDataClass.ts
+++ src/Data/Order/Pisa/pisaOrderDataClass.ts
@@ -1,10 +1,6 @@
 import { provideDataClass } from 'scrivito'
 import { pisaConfig } from '../../pisaClient'
-import { fetchAndFilterAttributes } from '../../fetchAndFilterAttributes'
 
 export function pisaOrderDataClass() {
-  return provideDataClass('Order', {
-    restApi: pisaConfig('order'),
-    attributes: () => fetchAndFilterAttributes('order'),
-  })
+  return provideDataClass('Order', { restApi: pisaConfig('order') })
 }
