50545f2f9282c5a726276381e67de4faaa5bae9c
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-11 13:28:27 +0200

    ContractDocument: Added reference attributes

--- src/Data/ContractDocument/Pisa/pisaContractDocumentDataClass.ts
+++ src/Data/ContractDocument/Pisa/pisaContractDocumentDataClass.ts
@@ -1,10 +1,8 @@
 import { provideDataClass } from 'scrivito'
 import { pisaConfig } from '../../pisaClient'
-import { fetchAndFilterAttributes } from '../../fetchAndFilterAttributes'
 
 export function pisaContractDocumentDataClass() {
   return provideDataClass('ContractDocument', {
     restApi: pisaConfig('contract-document'),
-    attributes: () => fetchAndFilterAttributes('contract-document'),
   })
 }
