99b73f0996f63631656b9572f1485f6259518f33
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-11 13:28:34 +0200

    OrderDocument: Added reference attributes

--- src/Data/OrderDocument/OrderDocumentEditingConfig.ts
+++ src/Data/OrderDocument/OrderDocumentEditingConfig.ts
@@ -3,8 +3,4 @@ import { OrderDocument } from './OrderDocumentDataClass'
 
 provideEditingConfig(OrderDocument, {
   title: 'Order document',
-  attributes: {
-    orderId: { title: 'Order ID' },
-    documentId: { title: 'Document ID' },
-  },
 })
