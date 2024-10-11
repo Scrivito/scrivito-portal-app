62bf5c7f93c5fa43e8539f8dc8f910f11d0bda28
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-11 13:28:38 +0200

    ServiceObject: Added reference attributes

--- src/Data/ServiceObject/ServiceObjectEditingConfig.ts
+++ src/Data/ServiceObject/ServiceObjectEditingConfig.ts
@@ -4,8 +4,6 @@ import { ServiceObject } from './ServiceObjectDataClass'
 provideEditingConfig(ServiceObject, {
   title: 'Service object',
   attributes: {
-    parentId: { title: 'Parent service object ID' },
     picture: { title: 'Picture' },
-    responsibleAgent: { title: 'Responsible agent ID' },
   },
 })
