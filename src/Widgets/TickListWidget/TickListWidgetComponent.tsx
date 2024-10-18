b87dd80aaa4c1d3fb7f6a25f6c6e96d5440f5fdb
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-18 11:57:22 +0200

    TickListIconContext: Add string default

--- src/Widgets/TickListWidget/TickListWidgetComponent.tsx
+++ src/Widgets/TickListWidget/TickListWidgetComponent.tsx
@@ -3,7 +3,7 @@ import { ContentTag, provideComponent } from 'scrivito'
 import { TickListWidget } from './TickListWidgetClass'
 import './TickListWidget.scss'
 
-export const TickListIconContext = createContext<string | undefined>(undefined)
+export const TickListIconContext = createContext('')
 
 provideComponent(TickListWidget, ({ widget }) => (
   <TickListIconContext.Provider value={widget.get('icon') || 'bi-check'}>
