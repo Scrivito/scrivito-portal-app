bae8ba7a793ee6d48df47334a078e52b68e421ec
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-28 15:04:33 +0100

    DataIconWidget: Remove obsolete "fallbackIcon" description

--- src/Widgets/DataIconWidget/DataIconWidgetEditingConfig.tsx
+++ src/Widgets/DataIconWidget/DataIconWidgetEditingConfig.tsx
@@ -21,11 +21,6 @@ provideEditingConfig(DataIconWidget, {
       title: 'Size',
       description: 'Default: bi-2x',
     },
-    fallbackIcon: {
-      title: 'Fallback icon',
-      description:
-        'This icon is used, if no condition matches. Default: "bi-question-octagon". The full list of names can be found at https://icons.getbootstrap.com/',
-    },
     data: {
       restrictDataTo: ['itemAttribute'],
     },
