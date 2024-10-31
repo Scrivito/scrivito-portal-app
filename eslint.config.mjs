ef9e089b225434d10cf509ae74583a99f11e1bb4
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-31 14:14:12 +0100

    npx @eslint/migrate-config .eslintrc.json
    Followed the instructions:
    > npm install @eslint/compat @eslint/js @eslint/eslintrc -D
    
    Manually removed .eslintrc.json. Manually applied prettier.

---
+++ eslint.config.mjs
@@ -0,0 +1,86 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import jsxA11Y from 'eslint-plugin-jsx-a11y'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:@arabasta/require-useeffect-dependency-array/recommended-legacy',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:jsx-a11y/recommended',
+    ),
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      'jsx-a11y': fixupPluginRules(jsxA11Y),
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+
+      'jsx-a11y': {
+        components: {
+          ImageTag: 'img',
+        },
+      },
+    },
+
+    rules: {
+      curly: ['error', 'multi-line'],
+
+      quotes: [
+        'warn',
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false,
+        },
+      ],
+
+      'react/jsx-curly-brace-presence': ['warn'],
+      'react/prop-types': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        1,
+        {
+          args: 'all',
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    files: ['src/config/windowScrivito.ts'],
+
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 0,
+    },
+  },
+]
