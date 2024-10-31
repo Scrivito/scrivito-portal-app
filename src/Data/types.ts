46ee36b929589cccd57bb9e80afa2f1eda9b644e
Alexander Pepper <alexander.pepper@justrelate.com>
2024-10-31 11:00:38 +0100

    types: Use "UnPromise" helper

--- src/Data/types.ts
+++ src/Data/types.ts
@@ -1,5 +1,7 @@
 import type { provideDataClass } from 'scrivito'
 
+type UnPromise<T> = T extends Promise<infer U> ? U : never
+
 export interface RawItem {
   _id: string
   [key: string]: unknown
@@ -11,18 +13,13 @@ export interface DataIndexResponse {
   continuation?: string
 }
 
-export type DataConnection = Parameters<
-  typeof provideDataClass
->[1]['connection'] extends Partial<infer U> | Promise<Partial<infer U>>
-  ? Partial<U>
-  : never
+type ProvideDataClassParams = Parameters<typeof provideDataClass>[1]
 
-type ProvideDataClassFunction = typeof provideDataClass
-type Params = Parameters<ProvideDataClassFunction>[1]
-export type DataClassAttributes = Readonly<Params['attributes']>
+export type DataConnection = UnPromise<ProvideDataClassParams['connection']>
 
-type ExtractDataClassSchema<T> = T extends Promise<infer U> ? U : never
-export type DataClassSchema = ExtractDataClassSchema<DataClassAttributes>
+export type DataClassAttributes = Readonly<ProvideDataClassParams['attributes']>
+
+export type DataClassSchema = UnPromise<DataClassAttributes>
 
 // TODO: Remove when #11321 is resolved
 type ExtractReadonlyDataClassSchema<T> =
