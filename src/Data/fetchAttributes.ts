28f10dd89ca29ec58bde5f90cf88ec0f504a24c9
Alexander Pepper <alexander.pepper@justrelate.com>
2024-11-14 12:45:54 +0100

    fetchAttributes: Add option to ignore specific attributes

--- src/Data/fetchAttributes.ts
+++ src/Data/fetchAttributes.ts
@@ -1,14 +1,22 @@
 import { pisaClient } from './pisaClient'
 import { DataClassSchema } from './types'
 
-/** Fetches the schema for `subPath`. */
+/**
+ * Fetches the schema for `subPath`.
+ * Allows optionally to `ignoreAttributes`.
+ * */
 export async function fetchAttributes(
   subPath: string,
+  ignoreAttributes: string[] = [],
 ): Promise<DataClassSchema> {
   const client = await pisaClient(subPath)
   const schema = (await client.get('schema')) as {
     attributes: DataClassSchema
   }
 
-  return schema.attributes
+  return Object.fromEntries(
+    Object.entries(schema.attributes).filter(
+      ([attributeName]) => !ignoreAttributes.includes(attributeName),
+    ),
+  )
 }
