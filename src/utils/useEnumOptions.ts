975acad47b5b1d7397039f3f0fbf4724eb3c39bf
Alexander Pepper <alexander.pepper@justrelate.com>
2024-11-29 11:42:04 +0100

    Simplify useEnumOptions
    It was fixed with `1.46.0-rc1`

--- src/utils/useEnumOptions.ts
+++ src/utils/useEnumOptions.ts
@@ -1,28 +1,7 @@
 import { useAttributeDefinition } from 'scrivito'
 
-// TODO: Remove/trim, once #11238 is resolved
 export function useEnumOptions(): Array<{ title: string; value: string }> {
-  const attributeDefinition = useAttributeDefinition()
+  const definition = useAttributeDefinition()
 
-  if (attributeDefinition[0] !== 'enum') return []
-
-  return attributeDefinition[1].values.map((enumValue) => {
-    if (isLocalizedEnumValueConfig(enumValue)) return enumValue
-
-    return {
-      title: enumValue,
-      value: enumValue,
-    }
-  })
-}
-
-function isLocalizedEnumValueConfig(
-  value: unknown,
-): value is { title: string; value: string } {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    'title' in value &&
-    'value' in value
-  )
+  return definition[0] === 'enum' ? definition[1].values : []
 }
