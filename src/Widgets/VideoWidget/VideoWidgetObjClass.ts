b9a02235936359b997e54ffa126ffcc398ca9b1c
Alexander Pepper <alexander.pepper@justrelate.com>
2024-12-02 08:51:21 +0100

    VideoWidget: Option to hide download button
    This option is currently only supported by chromium-based browsers (Chrome, Edge, Brave etc.). Firefox and Safari do _not_ support this option, since they both don't offer a download button at all (see https://github.com/whatwg/html/pull/6715)

--- src/Widgets/VideoWidget/VideoWidgetObjClass.ts
+++ src/Widgets/VideoWidget/VideoWidgetObjClass.ts
@@ -4,6 +4,7 @@ export const VideoWidget = provideWidgetClass('VideoWidget', {
   attributes: {
     source: ['reference', { only: ['Video'] }],
     poster: ['reference', { only: ['Image'] }],
+    noDownload: 'boolean',
     aspectRatio: [
       'enum',
       { values: ['21to9', '16to9', '4to3', '1to1', '3to4', '9to16'] },
