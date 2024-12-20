b3501f232fb4b4ead42ada2787f8376f59dbdf99
Alexander Pepper <alexander.pepper@justrelate.com>
2024-12-20 11:58:33 +0100

    localStorageGlobalResultDataClass: Implement basic search

--- src/Data/GlobalResult/LocalStorage/localStorageGlobalResultDataClass.ts
+++ src/Data/GlobalResult/LocalStorage/localStorageGlobalResultDataClass.ts
@@ -1,5 +1,30 @@
-import { DataClass } from 'scrivito'
+import { DataClass, provideDataClass } from 'scrivito'
+import { searchLocalStorageDataClasses } from '../../provideLocalStorageDataClass'
 
 export function localStorageGlobalResultDataClass(): DataClass {
-  throw new Error('Not yet implemented!')
+  return provideDataClass('GlobalResult', {
+    attributes: { title: 'string' },
+    connection: {
+      async index(params) {
+        const search = params.search()
+        if (!search) return { results: [], count: 0 }
+
+        const entities = [
+          'Ticket',
+          'Event',
+          'Quote',
+          'Order',
+          'ServiceObject',
+          'Document',
+          'Contract',
+        ]
+
+        const results = searchLocalStorageDataClasses(search, [
+          'CartItem',
+        ]).filter(({ entity }) => entities.includes(entity))
+
+        return { results, count: results.length }
+      },
+    },
+  })
 }
