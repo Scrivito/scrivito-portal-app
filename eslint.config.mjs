ebb015082292b4624d1e53743a3ca8b88d9ea9ca
David Császár <dcsaszar@users.noreply.github.com>
2025-01-13 14:42:15 +0100

    Lint with type information
    https://typescript-eslint.io/getting-started/typed-linting/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,6 +37,10 @@ export default [
 
     languageOptions: {
       parser: tsParser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
 
     settings: {
