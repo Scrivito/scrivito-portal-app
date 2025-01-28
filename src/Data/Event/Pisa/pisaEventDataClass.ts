847512e860ada0eecf031a4c387507e04700edbd
Alexander Pepper <alexander.pepper@justrelate.com>
2025-01-28 12:10:59 +0100

    Unified Event data class

--- src/Data/Event/Pisa/pisaEventDataClass.ts
+++
@@ -1,6 +0,0 @@
-import { provideDataClass } from 'scrivito'
-import { pisaConfig } from '../../pisaClient'
-
-export function pisaEventDataClass() {
-  return provideDataClass('Event', { restApi: pisaConfig('event') })
-}
--- src/Data/Event/eventParamsFallback.ts
+++ src/Data/Event/eventParamsFallback.ts
@@ -1,5 +1,5 @@
 import { currentLanguage, DataAttributeDefinitions, load } from 'scrivito'
-import { provideLocalStorageDataClass } from '../../provideLocalStorageDataClass'
+import { localStorageDataConnection } from '../localStorageDataConnection'
 import spiderImage from './FakeBinaries/Schulung_Spider_30.png'
 import roadshowImage from './FakeBinaries/TYNACOON_Roadshow.jpg'
 
@@ -101,40 +101,42 @@ async function attributes(): Promise<DataAttributeDefinitions> {
   }
 }
 
-export function localStorageEventDataClass() {
-  return provideLocalStorageDataClass('Event', {
+export function eventParamsFallback() {
+  return {
     attributes,
-    initialContent: [
-      {
-        _id: 'DD6585A543FE48018F497D3631332C6F',
-        keyword: 'Schulung: Wirkungsprinzipien Typ Spider 30',
-        number: 'P-11-007950',
-        beginsAt: '2024-04-29T13:30:00Z',
-        endsAt: '2024-04-29T14:30:00Z',
-        location: 'Schulungszentrum 1',
-        responsibleAgent: '0BD5DC0F9DA442C5946FA7ECAF870D7B',
-        status: 'PSA_PRO_EVT_REG_OPN',
-        logo: { url: spiderImage },
-        language: 'GER',
-        attendanceFee: 0.0,
-      },
-      {
-        _id: '4100600152A8FFCBE040007F01002CE3',
-        keyword: 'TYNACOON - Roadshow',
-        number: 'P-07.006325',
-        beginsAt: '2024-05-06T12:19:00Z',
-        endsAt: '2024-05-06T13:19:00Z',
-        location: 'Nürnberg',
-        organizer: 'K & R Messe GmbH & Co.KG (Karlsruhe)',
-        responsibleAgent: 'F203EF15869B46509B2BE0AB12D480D2',
-        status: 'PSA_PRO_EVT_REG_OPN',
-        url: 'http://www.tynacoon-roadshow.de',
-        logo: { url: roadshowImage },
-        language: 'GER',
-        attendanceFee: 0.0,
-        description:
-          'Pilotveranstaltung: Roadshow mit einem Show-Truck. Auf dem Truck sind Maschinen und Geräte installiert und können von den Besuchern teilweise im Betrieb begutachtet werden. Der Show-Truck fährt eine Woche die aktuellen Interessenten ab.',
-      },
-    ],
-  })
+    connection: localStorageDataConnection('Event', {
+      initialContent: [
+        {
+          _id: 'DD6585A543FE48018F497D3631332C6F',
+          keyword: 'Schulung: Wirkungsprinzipien Typ Spider 30',
+          number: 'P-11-007950',
+          beginsAt: '2024-04-29T13:30:00Z',
+          endsAt: '2024-04-29T14:30:00Z',
+          location: 'Schulungszentrum 1',
+          responsibleAgent: '0BD5DC0F9DA442C5946FA7ECAF870D7B',
+          status: 'PSA_PRO_EVT_REG_OPN',
+          logo: { url: spiderImage },
+          language: 'GER',
+          attendanceFee: 0.0,
+        },
+        {
+          _id: '4100600152A8FFCBE040007F01002CE3',
+          keyword: 'TYNACOON - Roadshow',
+          number: 'P-07.006325',
+          beginsAt: '2024-05-06T12:19:00Z',
+          endsAt: '2024-05-06T13:19:00Z',
+          location: 'Nürnberg',
+          organizer: 'K & R Messe GmbH & Co.KG (Karlsruhe)',
+          responsibleAgent: 'F203EF15869B46509B2BE0AB12D480D2',
+          status: 'PSA_PRO_EVT_REG_OPN',
+          url: 'http://www.tynacoon-roadshow.de',
+          logo: { url: roadshowImage },
+          language: 'GER',
+          attendanceFee: 0.0,
+          description:
+            'Pilotveranstaltung: Roadshow mit einem Show-Truck. Auf dem Truck sind Maschinen und Geräte installiert und können von den Besuchern teilweise im Betrieb begutachtet werden. Der Show-Truck fährt eine Woche die aktuellen Interessenten ab.',
+        },
+      ],
+    }),
+  }
 }
