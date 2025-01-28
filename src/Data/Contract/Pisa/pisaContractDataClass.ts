36951786fd707b788cb9c40d1269de688d976c62
Alexander Pepper <alexander.pepper@justrelate.com>
2025-01-28 12:10:56 +0100

    Unified Contract data class

--- src/Data/Contract/Pisa/pisaContractDataClass.ts
+++
@@ -1,6 +0,0 @@
-import { provideDataClass } from 'scrivito'
-import { pisaConfig } from '../../pisaClient'
-
-export function pisaContractDataClass() {
-  return provideDataClass('Contract', { restApi: pisaConfig('contract') })
-}
--- src/Data/Contract/contractParamsFallback.ts
+++ src/Data/Contract/contractParamsFallback.ts
@@ -1,5 +1,5 @@
 import { currentLanguage, DataAttributeDefinitions, load } from 'scrivito'
-import { provideLocalStorageDataClass } from '../../provideLocalStorageDataClass'
+import { localStorageDataConnection } from '../localStorageDataConnection'
 
 async function attributes(): Promise<DataAttributeDefinitions> {
   const lang = await load(currentLanguage)
@@ -221,25 +221,27 @@ async function attributes(): Promise<DataAttributeDefinitions> {
   }
 }
 
-export function localStorageContractDataClass() {
-  return provideLocalStorageDataClass('Contract', {
+export function contractParamsFallback() {
+  return {
     attributes,
-    initialContent: [
-      {
-        _id: '1E3039CD3CACF294E040A8C00F0177E3',
-        keyword: 'Baugruppenwartung bei Maier Schmiertechnik GmbH',
-        number: 'P-06.003736',
-        type: 'PSA_CTR_TYP_20',
-        category: 'PSA_CTR_KND_70',
-        partner: 'Maier Schmiertechnik GmbH (München)',
-        agent: '052601BEBCEC39C8E040A8C00D0107AC',
-        status: 'PSA_PRO_CTR_INI',
-        startAt: '2023-11-21T08:40:38Z',
-        endAt: '2035-11-20T22:59:59Z',
-        minimumTerm: 12,
-        minimumTermUnit: 'PSA_CTR_CYC_YEA',
-        totalPrice: 0,
-      },
-    ],
-  })
+    connection: localStorageDataConnection('Contract', {
+      initialContent: [
+        {
+          _id: '1E3039CD3CACF294E040A8C00F0177E3',
+          keyword: 'Baugruppenwartung bei Maier Schmiertechnik GmbH',
+          number: 'P-06.003736',
+          type: 'PSA_CTR_TYP_20',
+          category: 'PSA_CTR_KND_70',
+          partner: 'Maier Schmiertechnik GmbH (München)',
+          agent: '052601BEBCEC39C8E040A8C00D0107AC',
+          status: 'PSA_PRO_CTR_INI',
+          startAt: '2023-11-21T08:40:38Z',
+          endAt: '2035-11-20T22:59:59Z',
+          minimumTerm: 12,
+          minimumTermUnit: 'PSA_CTR_CYC_YEA',
+          totalPrice: 0,
+        },
+      ],
+    }),
+  }
 }
