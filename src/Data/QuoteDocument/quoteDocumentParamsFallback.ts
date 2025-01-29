a4fe9531d8a00d3d36bd678d8d88b110bb862db3
Alexander Pepper <alexander.pepper@justrelate.com>
2025-01-29 15:27:15 +0100

    Dynamic title for QuoteDocument

--- src/Data/QuoteDocument/quoteDocumentParamsFallback.ts
+++ src/Data/QuoteDocument/quoteDocumentParamsFallback.ts
@@ -27,6 +27,10 @@ async function attributes(): Promise<DataAttributeDefinitions> {
 export function quoteDocumentParamsFallback() {
   return {
     attributes,
+    title: async () =>
+      (await load(currentLanguage)) === 'de'
+        ? 'Angebotsdokument'
+        : 'Quote document',
     connection: localStorageDataConnection('QuoteDocument', {
       initialContent: [
         {
