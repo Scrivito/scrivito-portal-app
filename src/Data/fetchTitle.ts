19a8c0b9cf01ef4f5f3c8d323b08085ae758aef6
Alexander Pepper <alexander.pepper@justrelate.com>
2025-01-29 15:26:57 +0100

    Dynamic title for CallbackRequest

---
+++ src/Data/fetchTitle.ts
@@ -0,0 +1,17 @@
+import { pisaClient } from './pisaClient'
+
+/**
+ * Fetches the schema for `subPath`.
+ * */
+export async function fetchTitle(subPath: string): Promise<string | undefined> {
+  const client = await pisaClient(subPath)
+  if (!client) {
+    throw new Error('Please configure a pisaUrl on the default homepage.')
+  }
+
+  const schema = (await client.get('schema')) as {
+    title?: string
+  }
+
+  return schema.title
+}
