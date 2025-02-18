3b115e0c6cea883fe54bb6f2827fb829109af67d
Alexander Pepper <alexander.pepper@justrelate.com>
2025-02-18 13:00:03 +0100

    Combine fetchAttributes and fetchTitle into fetchSchema
    This only makes one schema request per data-class, not two requests (as previously).

--- src/Data/fetchSchema.ts
+++ src/Data/fetchSchema.ts
@@ -1,17 +1,17 @@
 import { DataAttributeDefinitions } from 'scrivito'
 import { pisaClient } from './pisaClient'
 
-export async function fetchAttributes(
-  subPath: string,
-): Promise<DataAttributeDefinitions> {
+export async function fetchSchema(subPath: string): Promise<{
+  attributes: DataAttributeDefinitions
+  title?: string
+}> {
   const client = await pisaClient(subPath)
   if (!client) {
     throw new Error('Please configure a pisaUrl on the default homepage.')
   }
 
-  const { attributes } = (await client.get('schema')) as {
+  return (await client.get('schema')) as {
     attributes: DataAttributeDefinitions
+    title?: string
   }
-
-  return attributes
 }
