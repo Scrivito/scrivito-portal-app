c4594c918022ad072c801e3f34d8ce18083b5179
Alexander Pepper <alexander.pepper@justrelate.com>
2025-03-07 12:23:02 +0100

    Homepage: Allow to ignore homepage layout

--- src/Objs/Homepage/HomepageLayoutComponent.tsx
+++ src/Objs/Homepage/HomepageLayoutComponent.tsx
@@ -1,7 +1,37 @@
-import { provideLayoutComponent } from 'scrivito'
-import { Homepage } from './HomepageObjClass'
+import {
+  connect,
+  CurrentPage,
+  currentPage,
+  Obj,
+  provideLayoutComponent,
+} from 'scrivito'
+import { Homepage, HomepageInstance } from './HomepageObjClass'
 import { DefaultPageLayoutComponent } from '../defaultPageLayoutComponent'
+import { Loading } from '../../Components/Loading'
 
-provideLayoutComponent(Homepage, ({ page }) => {
-  return <DefaultPageLayoutComponent page={page} />
-})
+provideLayoutComponent(Homepage, ({ page }) => <HomepageLayout page={page} />)
+
+// TODO: Remove workaround for issue #11635 once it is fixed.
+const HomepageLayout = connect(
+  function HomepageLayout({ page }: { page: HomepageInstance }) {
+    if (ignoreHomepageLayout()) return <CurrentPage />
+
+    return <DefaultPageLayoutComponent page={page} />
+  },
+  { loading: Loading },
+)
+
+function ignoreHomepageLayout(): boolean {
+  const firstSubpagePath = currentPage()
+    ?.path()
+    ?.split('/')
+    .slice(0, 2)
+    .join('/')
+  if (!firstSubpagePath) return false
+  if (firstSubpagePath === '/') return false
+
+  const firstSubpage = Obj.getByPath(firstSubpagePath)
+  if (!firstSubpage) return false
+
+  return firstSubpage.get('layoutIgnoreHomepageLayout') == true
+}
