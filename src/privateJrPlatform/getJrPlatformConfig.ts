14fa791d49cb8917654950ff985a8a968d383538
Alexander Pepper <alexander.pepper@justrelate.com>
2025-03-18 11:13:45 +0100

    JR Platform: Provide initial content

--- src/privateJrPlatform/getJrPlatformConfig.ts
+++ src/privateJrPlatform/getJrPlatformConfig.ts
@@ -1,6 +1,8 @@
 export function getJrPlatformConfig() {
   return {
     unstable: {
+      initialContentDumpUrl:
+        'https://v5-content.scrivito-portal-app.pages.dev/index.json',
       trustedUiOrigins: [
         'http://localhost:8090',
         'https://*.scrivito-ui.pages.dev',
