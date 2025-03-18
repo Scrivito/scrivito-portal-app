3122c8ee09cc09383b0db5e41e7ed7eef8a46bf1
Alexander Pepper <alexander.pepper@justrelate.com>
2025-03-18 11:13:43 +0100

    getJrPlatformApp: Check if content matches the expected format.

---
+++ src/privateJrPlatform/getJrPlatformApp.ts
@@ -0,0 +1,40 @@
+import { load, Obj } from 'scrivito'
+import { App } from '../App'
+import { WrongContentFormat } from './Components/WrongContentFormat'
+
+export async function getJrPlatformApp() {
+  return (await isValidContentFormat()) ? App : WrongContentFormat
+}
+
+const CONTENT_FORMAT = 'portal-app:5'
+const KNOWN_CONTENT_FORMATS: Record<string, string> = {
+  'portal-app:5': 'https://scrivito-portal-app.pages.dev',
+
+  /* Once v6 is released, the list of KNOWN_CONTENT_FORMATS looks like this: */
+  // 'portal-app:5': 'https://v5.scrivito-portal-app.pages.dev',
+  // 'portal-app:6': 'https://scrivito-portal-app.pages.dev',
+}
+
+/** Redirects away, if content format is not valid. */
+async function isValidContentFormat(): Promise<boolean> {
+  const root = await load(() => Obj.root())
+  if (!root) return true
+
+  const siteContentFormat = root.get('contentFormat')
+  if (siteContentFormat === CONTENT_FORMAT) return true
+
+  if (typeof siteContentFormat !== 'string') return false
+  if (!siteContentFormat) return false
+
+  const redirectOrigin = KNOWN_CONTENT_FORMATS[siteContentFormat]
+  if (!redirectOrigin) return false
+
+  const url = location.href.replace(location.origin, redirectOrigin)
+  location.replace(url)
+
+  return never() // location.replace will trigger a redirect
+}
+
+function never() {
+  return new Promise<never>(() => {})
+}
