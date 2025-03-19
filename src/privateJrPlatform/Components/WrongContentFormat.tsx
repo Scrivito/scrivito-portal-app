0ac03eddd0ad76d038a66cc93e2c336689a364e3
Alexander Pepper <alexander.pepper@justrelate.com>
2025-03-19 10:42:44 +0100

    WrongContentFormat: Improve error message

--- src/privateJrPlatform/Components/WrongContentFormat.tsx
+++ src/privateJrPlatform/Components/WrongContentFormat.tsx
@@ -4,6 +4,8 @@ import errorBackground from '../../assets/images/error-background.jpg'
 import spaceman from '../../assets/images/spaceman.svg'
 
 export const WrongContentFormat = connect(function WrongContentFormat() {
+  const contentFormat = ensureString(Obj.root()?.get('contentFormat'))
+
   return (
     <main id="main">
       <section className="bg-danger vh-100 py-5">
@@ -20,8 +22,9 @@ export const WrongContentFormat = connect(function WrongContentFormat() {
             <div className="col-sm-6">
               <h1 className="display-1">Error</h1>
               <h3 className="h3">
-                Content format “{ensureString(Obj.root()?.get('contentFormat'))}
-                ” not supported.
+                {contentFormat
+                  ? `Content format “${contentFormat}” is unsupported.`
+                  : 'Missing content format.'}
               </h3>
             </div>
           </div>
