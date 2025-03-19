a8430a13a586e31c049d820dde3c6a7426236a7f
Alexander Pepper <alexander.pepper@justrelate.com>
2025-03-19 12:02:58 +0100

    LanguageSwitch: Added alignment option

--- src/Widgets/LanguageSwitchWidget/LanguageSwitchWidgetEditingConfig.ts
+++ src/Widgets/LanguageSwitchWidget/LanguageSwitchWidgetEditingConfig.ts
@@ -3,4 +3,19 @@ import { LanguageSwitchWidget } from './LanguageSwitchWidgetClass'
 
 provideEditingConfig(LanguageSwitchWidget, {
   title: 'Language Switch',
+  attributes: {
+    alignment: {
+      title: 'Alignment',
+      description: 'Default: Left',
+      values: [
+        { value: 'left', title: 'Left' },
+        { value: 'center', title: 'Center' },
+        { value: 'right', title: 'Right' },
+      ],
+    },
+  },
+  properties: ['alignment'],
+  initialContent: {
+    alignment: 'left',
+  },
 })
