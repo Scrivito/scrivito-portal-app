865399db6443ce2f029cc657886bc5c718e89fe0
Alexander Pepper <alexander.pepper@justrelate.com>
2025-03-19 13:28:52 +0100

    getJrPlatformInstanceId: Support hostname fallback

--- src/privateJrPlatform/multiTenancy.ts
+++ src/privateJrPlatform/multiTenancy.ts
@@ -1,8 +1,18 @@
 const location = typeof window !== 'undefined' ? window.location : undefined
 
+const INSTANCE_ALIAS: Partial<Record<string, string>> = {
+  'tynacoon.com': '13b78a0a81072f996f5010bb59b48957',
+  'www.tynacoon.com': '13b78a0a81072f996f5010bb59b48957',
+  'my.justrelate.com': '6d226c03f32a8ea4e8ae8e5cbe6c6e2c',
+  'my-beta.justrelate.com': '6d226c03f32a8ea4e8ae8e5cbe6c6e2c',
+}
+
 export function getJrPlatformInstanceId(): string {
   const scrivitoInstance = import.meta.env.SCRIVITO_TENANT
   if (scrivitoInstance) return scrivitoInstance
+
+  const hostnameInstance = instanceFromHostname()
+  if (hostnameInstance) return hostnameInstance
 
   if (!location) throw new Error('Could not determine instance!')
 
@@ -34,5 +44,9 @@ export function jrPlatformRedirectToSiteUrl(siteUrl: string) {
 }
 
 function isMultitenancyEnabled(): boolean {
-  return !import.meta.env.SCRIVITO_TENANT
+  return !import.meta.env.SCRIVITO_TENANT && !instanceFromHostname()
+}
+
+function instanceFromHostname() {
+  return (location && INSTANCE_ALIAS[location.hostname]) ?? null
 }
