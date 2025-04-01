58d1c5348a2dec6b3ed6bc5405d32d45d49d0cf7
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-04-01 10:33:44 +0200

    Add QST Builder

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -1,7 +1,13 @@
 import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets'
+import { initPisaQuestionnaireWidgets } from 'scrivito-pisasales-questionnaire-builder'
+
 import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
 })
 
 initNeoletterFormWidgets()
-export {}
+
+// Replace "https://your-pisa-api-url" with the actual PisaSales REST API URL
+initPisaQuestionnaireWidgets({ pisaUrl: "https://your-pisa-api-url" })
+
+export { }
