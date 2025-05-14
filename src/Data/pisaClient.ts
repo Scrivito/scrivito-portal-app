30a011a4f55ecb1a01b2f976feaf01ba709c7f2c
Alexander Pepper <alexander.pepper@justrelate.com>
2025-05-14 14:33:32 +0200

    Extract `pisaBaseUrl` function
    This is closes to the upcoming "Common URL path for all PisaSales REST APIs"

--- src/Data/pisaClient.ts
+++ src/Data/pisaClient.ts
@@ -1,15 +1,33 @@
-import { Obj, createRestApiClient, currentLanguage, load } from 'scrivito'
+import {
+  Obj,
+  createRestApiClient,
+  currentLanguage,
+  currentUser,
+  load,
+} from 'scrivito'
 import { isHomepage } from '../Objs/Homepage/HomepageObjClass'
 
-export async function pisaUrl(): Promise<string | null> {
+async function pisaBaseUrl(): Promise<string | null> {
   if (import.meta.env.FORCE_LOCAL_STORAGE) return null
+  if (!(await load(() => currentUser()))) return null
 
+  // TODO: Read out `pisa_sales_api_url` from AMS instance configuration once available
   const defaultRoot = await load(() =>
     Obj.onAllSites().get(import.meta.env.SCRIVITO_ROOT_OBJ_ID),
   )
   if (!isHomepage(defaultRoot)) return never()
 
-  return defaultRoot.get('pisaUrl') || null
+  const pisaUrl = defaultRoot.get('pisaUrl')
+  if (!pisaUrl) return null
+
+  return pisaUrl.replace(/\/portal$/, '')
+}
+
+export async function pisaUrl(): Promise<string | null> {
+  const baseUrl = await pisaBaseUrl()
+  if (!baseUrl) return null
+
+  return `${baseUrl}/portal`
 }
 
 export async function pisaClient(subPath: string) {
