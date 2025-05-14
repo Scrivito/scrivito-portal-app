2f97142a0f1fd98eedfe93c5c8c9b4f5be4b5085
Alexander Pepper <alexander.pepper@justrelate.com>
2025-05-14 11:16:08 +0200

    Add basic version of honeybadger
    it is only included in the build, if a import.meta.env.HONEYBADGER_API_KEY is given.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
+import { configureHoneybadger } from './configureHoneybadger'
 import { configureHistory } from './history'
 import { configureObjClassForContentType } from './objClassForContentType'
 import { configureScrivito } from './scrivito'
@@ -10,5 +11,6 @@ export function configure() {
   configureObjClassForContentType()
   configureScrivitoContentBrowser()
   configureHistory()
+  configureHoneybadger()
   configureWindowScrivito()
 }
