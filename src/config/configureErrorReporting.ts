81e72a15521685633e82beb77bc074971b8be396
Alexander Pepper <alexander.pepper@justrelate.com>
2025-05-20 11:05:27 +0200

    Enrich Honeybadger context

--- src/config/configureErrorReporting.ts
+++ src/config/configureErrorReporting.ts
@@ -1,10 +1,27 @@
+import {
+  currentUser,
+  getInstanceId,
+  isEditorLoggedIn,
+  load,
+  Obj,
+} from 'scrivito'
+
 export async function configureErrorReporting() {
   if (!import.meta.env.HONEYBADGER_API_KEY) return
 
   const { default: Honeybadger } = await import('@honeybadger-io/js')
 
-  Honeybadger.configure({
+  const honeybadger = Honeybadger.configure({
     apiKey: import.meta.env.HONEYBADGER_API_KEY,
     revision: import.meta.env.HONEYBADGER_REVISION,
+  })
+
+  await load(() => Obj.onAllSites().all().count()) // TODO: Remove workaround for issue #11895
+  const user = await load(() => currentUser())
+  honeybadger.setContext({
+    instance_id: getInstanceId(),
+    is_editor: isEditorLoggedIn(),
+    // ⚠️ Do not include plain text e-mail addresses in the context. ⚠️
+    user_id: user?.id(),
   })
 }
