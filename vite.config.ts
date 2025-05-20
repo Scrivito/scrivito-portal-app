bbbd08d6c2d3d10db79bb313de45dd84fc6fd168
Alexander Pepper <alexander.pepper@justrelate.com>
2025-05-20 11:21:37 +0200

    Upload sourcemaps to Honeybadger during build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import dns from 'dns'
 import fs from 'fs'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import honeybadgerRollupPlugin from '@honeybadger-io/rollup-plugin'
 import { resolve } from 'path'
 import { productionHeaders, developmentHeaders } from './headers.config'
 
@@ -33,6 +34,15 @@ export default defineConfig(({ mode }) => {
           main: resolve(__dirname, 'index.html'),
           _scrivito_extensions: resolve(__dirname, '_scrivito_extensions.html'),
         },
+        plugins: [
+          HONEYBADGER_API_KEY
+            ? honeybadgerRollupPlugin({
+                apiKey: HONEYBADGER_API_KEY,
+                assetsUrl: 'https://*',
+                revision: HONEYBADGER_REVISION,
+              })
+            : {},
+        ],
         output: {
           manualChunks: {
             react: [
@@ -45,6 +55,7 @@ export default defineConfig(({ mode }) => {
           },
         },
       },
+      sourcemap: !!HONEYBADGER_API_KEY,
     },
     define: {
       'import.meta.env.SCRIVITO_ORIGIN': JSON.stringify(scrivitoOrigin(env)),
