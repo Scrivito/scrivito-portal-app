c7f149030bd0697f17ce2a0e0ba0f80db3461d87
Alexander Pepper <alexander.pepper@justrelate.com>
2025-06-06 13:18:48 +0200

    Added jwtPisaSalesApiConfig and use it

--- src/Data/Gdpr/GdprDataClass.ts
+++ src/Data/Gdpr/GdprDataClass.ts
@@ -1,23 +1,20 @@
-import { isUserLoggedIn, provideDataClass } from 'scrivito'
+import { isUserLoggedIn, load, Obj, provideDataClass } from 'scrivito'
 import { pisaConfig } from '../pisaClient'
-import { getTokenAuthorization } from '../getTokenAuthorization'
+import { jwtPisaSalesApiConfig } from '../jwtPisaSalesApiConfig'
 
 export const Gdpr = provideDataClass(
   'Gdpr',
   (async () => {
+    await load(() => Obj.onAllSites().all().count()) // TODO: Remove workaround for issue #11895 or #11924
+    if (!isUserLoggedIn()) {
+      const jwtRestApi = await jwtPisaSalesApiConfig({ subPath: 'portal/gdpr' })
+
+      if (jwtRestApi) return { restApi: jwtRestApi }
+    }
+
     const restApi = await pisaConfig('gdpr')
     if (!restApi) {
       return (await import('./gdprParamsFallback')).gdprParamsFallback()
-    }
-
-    if (!isUserLoggedIn()) {
-      const tokenAuthorization = getTokenAuthorization()
-      if (tokenAuthorization) {
-        const { url, headers: baseHeaders } = restApi
-        const headers = { ...baseHeaders, Authorization: tokenAuthorization }
-
-        return { restApi: { url, headers } }
-      }
     }
 
     return { restApi }
