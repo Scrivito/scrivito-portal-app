e80b21b80927214261080b45a04fe88b144e4265
Alexander Pepper <alexander.pepper@justrelate.com>
2025-07-03 11:11:19 +0200

    SmkArtwork: Sort attributes alphabetically

--- src/Data/SmkArtwork/SmkArtworkDataClass.ts
+++ src/Data/SmkArtwork/SmkArtworkDataClass.ts
@@ -7,22 +7,21 @@ import {
 export const SmkArtwork = provideDataClass('SmkArtwork', {
   title: 'Statens Museum for Kunst - Artwork',
   attributes: {
+    acquisition_date: ['date', { title: 'Acquisition date' }],
     creator_date_of_birth: ['date', { title: 'Artist’s date of birth' }],
     creator_date_of_death: ['date', { title: 'Artist’s date of death' }],
     creator: ['string', { title: 'Artist' }],
-    label: ['string', { title: 'Label' }],
-    production_date: ['date', { title: 'Creation date' }],
-    title: ['string', { title: 'Title' }],
-
-    acquisition_date: ['date', { title: 'Acquisition date' }],
     current_location_name: ['string', { title: 'Current location' }],
     frontend_url: ['string', { title: 'URL' }],
     geo_location: ['string', { title: 'Geo location' }],
     has_image: ['boolean', { title: 'Has image?' }],
     image_native: ['string', { title: 'Image URL (high resolution)' }],
     image_thumbnail: ['string', { title: 'Image URL (thumbnail)' }],
+    label: ['string', { title: 'Label' }],
     on_display: ['boolean', { title: 'On display?' }],
+    production_date: ['date', { title: 'Creation date' }],
     public_domain: ['boolean', { title: 'Public domain?' }],
+    title: ['string', { title: 'Title' }],
   },
   connection: {
     async index(params) {
