c227c3fd1407ce65f3c855ca7faff45e72f6555b
Alexander Pepper <alexander.pepper@justrelate.com>
2025-07-04 09:16:14 +0200

    Skip to content: Provide translations for other languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { CurrentPage } from 'scrivito'
+import { currentLanguage, CurrentPage } from 'scrivito'
 import { HelmetProvider, HelmetServerState } from 'react-helmet-async'
 
 import { CurrentPageMetadata } from './Components/CurrentPageMetadata'
@@ -20,7 +20,7 @@ export function App({
       <ErrorBoundary>
         <div ref={appWrapperRef} id="app-wrapper">
           <a href="#main" className="btn skip-to-content">
-            Skip to Content
+            {localizeSkipToContent()}
           </a>
           <DesignAdjustments>
             <SinglePageSite>
@@ -34,4 +34,17 @@ export function App({
       </ErrorBoundary>
     </HelmetProvider>
   )
+}
+
+function localizeSkipToContent(): string {
+  switch (currentLanguage()) {
+    case 'de':
+      return 'Zum Inhalt springen'
+    case 'fr':
+      return 'Aller au contenu'
+    case 'pl':
+      return 'Przejdź do treści'
+    default:
+      return 'Skip to Content'
+  }
 }
