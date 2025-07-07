31bd2c2cb8e8ea790b7401c61d20e1740e37de30
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-07-07 14:27:31 +0200

    Add package and use new function

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -1,6 +1,6 @@
 import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets'
 import { initPisaQuestionnaireWidgets } from 'scrivito-pisasales-questionnaire-builder'
-import { pisaUrl } from '../Data/pisaClient'
+import { pisaSalesApiUrl } from '../Data/pisaClient'
 
 import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
@@ -8,6 +8,6 @@ import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
 
 initNeoletterFormWidgets()
 
-pisaUrl().then(pisaUrl => initPisaQuestionnaireWidgets({ pisaUrl: pisaUrl || '' }))
+pisaSalesApiUrl().then(pisaUrl => initPisaQuestionnaireWidgets({ pisaUrl: pisaUrl || '' }))
 
 export {}
