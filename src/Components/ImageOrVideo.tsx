5911c4d6116c9a44a1a95ad4400efd9ad6d9173e
Alexander Pepper <alexander.pepper@justrelate.com>
2025-07-28 13:48:28 +0200

    ImageOrVideo: Pause autoplay video on click

--- src/Components/ImageOrVideo.tsx
+++ src/Components/ImageOrVideo.tsx
@@ -1,16 +1,51 @@
-import { connect, Widget, InPlaceEditingOff, ImageTag } from 'scrivito'
+import { useImperativeHandle, useRef, useState } from 'react'
+import {
+  connect,
+  Widget,
+  InPlaceEditingOff,
+  ImageTag,
+  currentLanguage,
+} from 'scrivito'
+import './ImageOrVideo.scss'
+
+export interface TogglePlayPauseRef {
+  togglePlayPause: (e: React.MouseEvent) => void
+}
 
 export const ImageOrVideo = connect(function ImageOrVideo<T extends string>({
   attribute,
   className,
+  togglePlayPauseRef,
   widget,
 }: {
   attribute: T
   className?: string
+  togglePlayPauseRef: React.Ref<TogglePlayPauseRef>
   widget: Widget<{
     [K in T]: 'reference'
   }>
 }) {
+  const [isPaused, setIsPaused] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const handleVideoClick = (e: React.MouseEvent) => {
+    const video = videoRef.current
+    if (!video) return
+
+    if (isPaused) {
+      video.play()
+      setIsPaused(false)
+    } else {
+      video.pause()
+      setIsPaused(true)
+    }
+    e.stopPropagation()
+  }
+
+  useImperativeHandle(togglePlayPauseRef, () => ({
+    togglePlayPause: handleVideoClick,
+  }))
+
   const background = widget.get(attribute)
   if (!background) return null
 
@@ -19,15 +54,28 @@ export const ImageOrVideo = connect(function ImageOrVideo<T extends string>({
 
   if (background.contentType().startsWith('video/')) {
     return (
-      <video
-        autoPlay
-        className={classNames.join(' ')}
-        key={background.contentUrl()}
-        loop
-        muted
-        playsInline
-        src={background.contentUrl()}
-      />
+      <>
+        <video
+          ref={videoRef}
+          autoPlay
+          className={classNames.join(' ')}
+          key={background.contentUrl()}
+          loop
+          muted
+          playsInline
+          src={background.contentUrl()}
+          onClick={handleVideoClick}
+        />
+        {isPaused && (
+          <button
+            onClick={handleVideoClick}
+            className="image-or-video-play-button"
+            aria-label={localizePlayVideoLabel()}
+          >
+            <i className="bi bi-play-fill text-white bi-5x" />
+          </button>
+        )}
+      </>
     )
   }
 
@@ -42,3 +90,16 @@ export const ImageOrVideo = connect(function ImageOrVideo<T extends string>({
     </InPlaceEditingOff>
   )
 })
+
+function localizePlayVideoLabel(): string {
+  switch (currentLanguage()) {
+    case 'de':
+      return 'Video abspielen'
+    case 'fr':
+      return 'Lire la vidéo'
+    case 'pl':
+      return 'Odtwórz wideo'
+    default:
+      return 'Play video'
+  }
+}
