e2c993981962c08880af25630560f68c4a65d247
Alexander Pepper <alexander.pepper@justrelate.com>
2025-09-04 12:48:53 +0200

    blobToBase64: Now ignores blob.type
    + truncated error messages using "truncate".

--- src/utils/blobToBinary.ts
+++ src/utils/blobToBinary.ts
@@ -1,5 +1,6 @@
 import { pisaSalesApiUrl } from '../Data/pisaClient'
 import { pseudoRandom32CharHex } from './pseudoRandom32CharHex'
+import { truncate } from 'lodash-es'
 
 export interface DataBinaryUpload {
   dataBase64: string
@@ -14,9 +15,10 @@ export interface DataBinaryUpload {
 export async function blobToBinary(
   blob: Blob | File,
 ): Promise<DataBinaryUpload> {
-  const { dataBase64, contentType } = await blobToBase64(blob)
-  const filename = blob instanceof File ? blob.name : 'unknown-name'
-  const binary = { dataBase64, filename }
+  const binary = {
+    dataBase64: await blobToBase64(blob),
+    filename: blob instanceof File ? blob.name : 'unknown-name',
+  }
 
   const connectedToPisa = (await pisaSalesApiUrl()) !== null
 
@@ -25,14 +27,12 @@ export async function blobToBinary(
     : {
         ...binary,
         contentLength: blob.size,
-        contentType,
+        contentType: blob.type,
         _id: pseudoRandom32CharHex(),
       }
 }
 
-async function blobToBase64(
-  blob: Blob,
-): Promise<{ dataBase64: string; contentType: string }> {
+async function blobToBase64(blob: Blob): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
 
@@ -49,38 +49,18 @@ async function blobToBase64(
         return
       }
 
-      const genericPrefix = 'data:application/octet-stream;base64,'
-      if (dataUrl.startsWith(genericPrefix)) {
-        resolve({
-          dataBase64: dataUrl.substring(genericPrefix.length),
-          contentType: 'application/octet-stream',
-        })
-        return
-      }
-
-      if (!blob.type) {
+      const parts = dataUrl.split(';base64,')
+      const [_firstPart, secondPart, ...rest] = parts
+      if (!secondPart || rest.length > 0) {
         reject(
           new Error(
-            `FileReader result does not start with expected prefix 'data:application/octet-stream;base64,' and blob.type is empty: ${dataUrl.substring(0, 100)}${dataUrl.length > 100 ? '...' : ''}`,
+            `FileReader result does not contain the delimiter “;base64,”: ${truncate(dataUrl, { length: 50 })}`,
           ),
         )
         return
       }
 
-      const dataPrefix = `data:${blob.type};base64,`
-      if (!dataUrl.startsWith(dataPrefix)) {
-        reject(
-          new Error(
-            `FileReader result does not start with expected prefix 'data:application/octet-stream;base64,' or '${dataPrefix}': ${dataUrl.substring(0, 100)}${dataUrl.length > 100 ? '...' : ''}`,
-          ),
-        )
-        return
-      }
-
-      resolve({
-        dataBase64: dataUrl.substring(dataPrefix.length),
-        contentType: blob.type,
-      })
+      resolve(secondPart)
     }
 
     reader.readAsDataURL(blob)
