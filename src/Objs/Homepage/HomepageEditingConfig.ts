8d8ea677322d00129729bc6fc151eb2d41c4ea3e
Alexander Pepper <alexander.pepper@justrelate.com>
2025-09-09 11:57:23 +0200

    jwt: Base on SCRIVITO_DEFAULT_CONTENT_ID not SCRIVITO_ROOT_OBJ_ID

--- src/Objs/Homepage/HomepageEditingConfig.ts
+++ src/Objs/Homepage/HomepageEditingConfig.ts
@@ -13,6 +13,7 @@ import { TopNavigationWidget } from '../../Widgets/TopNavigationWidget/TopNaviga
 import { SectionWidget } from '../../Widgets/SectionWidget/SectionWidgetClass'
 import { HeadlineWidget } from '../../Widgets/HeadlineWidget/HeadlineWidgetClass'
 import { TextWidget } from '../../Widgets/TextWidget/TextWidgetClass'
+import { jwtPisaSalesConfigSite } from '../../Data/jwtPisaSalesApiConfig'
 
 provideEditingConfig(Homepage, {
   title: 'Homepage',
@@ -91,7 +92,7 @@ provideEditingConfig(Homepage, {
       properties: [
         'contentTitle',
         'baseUrl',
-        site.id() === import.meta.env.SCRIVITO_ROOT_OBJ_ID
+        site.id() === jwtPisaSalesConfigSite()?.id()
           ? 'jwtPisaSalesApiUrl'
           : null,
         'siteLogoDark',
