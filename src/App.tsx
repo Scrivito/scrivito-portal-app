b4b40cbc01017ecbfc25c6928a3cdf5b7722e622
Markus Hartig <markus.hartig@justrelate.com>
2025-10-23 15:44:04 +0200

    Add portal bot

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { Toasts } from './Components/Toasts'
 import { DesignAdjustments } from './Components/DesignAdjustments'
 import { SinglePageSite } from './Components/SinglePageSite'
 
+import { PortalBot } from './portal_bot'
+
 export function App({
   appWrapperRef,
   onServerState,
@@ -29,6 +31,7 @@ export function App({
           </DesignAdjustments>
           <Toasts />
         </div>
+        <PortalBot />
       </ErrorBoundary>
     </HelmetProvider>
   )
