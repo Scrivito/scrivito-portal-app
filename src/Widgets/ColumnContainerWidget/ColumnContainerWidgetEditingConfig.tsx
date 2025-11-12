73c9e660f523eaf4d6227383bf500a57ae106ac8
Alexander Pepper <alexander.pepper@justrelate.com>
2025-11-12 14:41:34 +0100

    Extract AlignmentEditor from ColumnsEditorTab.

--- src/Widgets/ColumnContainerWidget/ColumnContainerWidgetEditingConfig.tsx
+++ src/Widgets/ColumnContainerWidget/ColumnContainerWidgetEditingConfig.tsx
@@ -1,19 +1,48 @@
-import { provideEditingConfig } from 'scrivito'
+import {
+  canEdit,
+  isComparisonActive,
+  provideEditingConfig,
+  uiContext,
+  Widget,
+} from 'scrivito'
 import { ColumnContainerWidget } from './ColumnContainerWidgetClass'
 import { ColumnsEditorTab } from './ColumnsEditorTab'
 import { ColumnWidget } from '../ColumnWidget/ColumnWidgetClass'
+import { AlignmentEditor } from '../../Components/ScrivitoExtensions/AlignmentEditor'
 import Thumbnail from './thumbnail.svg'
 
 provideEditingConfig(ColumnContainerWidget, {
   title: 'Columns',
   thumbnail: Thumbnail,
+  attributes: {
+    alignment: {
+      title: 'Alignment',
+      description: 'Default: Start',
+    },
+  },
+  properties: [
+    [
+      'alignment',
+      {
+        component: ({ widget }: { widget: Widget }) => (
+          <AlignmentEditor
+            attributeValue={widget.get('alignment')}
+            readOnly={!canEdit(widget.obj()) || isComparisonActive()}
+            theme={(uiContext() || { theme: null }).theme}
+            updateAttributeValue={(value) =>
+              widget.update({ alignment: value })
+            }
+          />
+        ),
+      },
+    ],
+  ],
   propertiesGroups: [
     {
       title: 'Columns layout',
       key: 'columns-layout-group',
       component: ColumnsEditorTab,
       properties: [
-        'alignment',
         'columns',
         'disableGutters',
         'disableResponsiveAdaption',
