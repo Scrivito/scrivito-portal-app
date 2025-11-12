2dd8393145dbf881408a6471ff5edcd10be07917
Alexander Pepper <alexander.pepper@justrelate.com>
2025-11-12 14:41:28 +0100

    (Data)ImageWidget: No longer use `ImageDimensionsEditor`

--- src/Widgets/DataImageWidget/DataImageWidgetEditingConfig.tsx
+++ src/Widgets/DataImageWidget/DataImageWidgetEditingConfig.tsx
@@ -1,7 +1,14 @@
-import { provideEditingConfig } from 'scrivito'
+import {
+  canEdit,
+  isComparisonActive,
+  provideEditingConfig,
+  uiContext,
+  Widget,
+} from 'scrivito'
 import { DataImageWidget } from './DataImageWidgetClass'
 import Thumbnail from './thumbnail.svg'
-import { ImageDimensionsEditor } from '../../Components/ScrivitoExtensions/ImageDimensionsEditor'
+import { DimensionPicker } from '../../Components/ScrivitoExtensions/DimensionPicker'
+import { ObjectFitEditor } from '../../Components/ScrivitoExtensions/ObjectFitEditor'
 
 provideEditingConfig(DataImageWidget, {
   title: 'Data Image',
@@ -16,23 +23,84 @@ provideEditingConfig(DataImageWidget, {
         { value: 'right', title: 'Right' },
       ],
     },
+    height: {
+      title: 'Height',
+    },
     link: {
       title: 'Link (optional)',
       description: 'The page to open after clicking the image.',
     },
+    objectFit: {
+      title: 'Object fit',
+      description: 'Default: Contain',
+    },
     roundCorners: {
       title: 'Round corners?',
+    },
+    width: {
+      title: 'Width',
     },
     data: {
       restrictDataTo: ['itemAttribute'],
     },
   },
   properties: ['alignment', 'link', 'roundCorners'],
-  propertiesGroups: [
+  propertiesGroups: (widget) => [
     {
       title: 'Dimensions',
-      properties: ['height', 'objectFit', 'width'],
-      component: ImageDimensionsEditor,
+      properties: [
+        [
+          'width',
+          {
+            component: ({ widget }: { widget: Widget }) => (
+              <DimensionPicker
+                attributeValue={widget.get('width')}
+                readOnly={!canEdit(widget.obj()) || isComparisonActive()}
+                theme={(uiContext() || { theme: null }).theme}
+                units={['px', '%']}
+                updateAttributeValue={(value) =>
+                  widget.update({ width: value })
+                }
+              />
+            ),
+          },
+        ],
+        [
+          'height',
+          {
+            component: ({ widget }: { widget: Widget }) => (
+              <DimensionPicker
+                attributeValue={widget.get('height')}
+                readOnly={!canEdit(widget.obj()) || isComparisonActive()}
+                theme={(uiContext() || { theme: null }).theme}
+                units={['px']}
+                updateAttributeValue={(value) =>
+                  widget.update({ height: value })
+                }
+              />
+            ),
+          },
+        ],
+        ...(widget.get('width')
+          ? ([
+              [
+                'objectFit',
+                {
+                  component: ({ widget }: { widget: Widget }) => (
+                    <ObjectFitEditor
+                      attributeValue={widget.get('objectFit')}
+                      readOnly={!canEdit(widget.obj()) || isComparisonActive()}
+                      theme={(uiContext() || { theme: null }).theme}
+                      updateAttributeValue={(value) =>
+                        widget.update({ objectFit: value })
+                      }
+                    />
+                  ),
+                },
+              ],
+            ] as const)
+          : []),
+      ],
       key: 'dimensions-group',
     },
   ],
