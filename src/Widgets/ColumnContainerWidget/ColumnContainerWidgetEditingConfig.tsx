20497e715017baf064d280edf5a02280a5e12d13
Alexander Pepper <alexander.pepper@justrelate.com>
2025-11-12 14:41:36 +0100

    Extracted InvertedBooleanEditor

--- src/Widgets/ColumnContainerWidget/ColumnContainerWidgetEditingConfig.tsx
+++ src/Widgets/ColumnContainerWidget/ColumnContainerWidgetEditingConfig.tsx
@@ -10,6 +10,7 @@ import { ColumnsEditorTab } from './ColumnsEditorTab'
 import { ColumnWidget } from '../ColumnWidget/ColumnWidgetClass'
 import { AlignmentEditor } from '../../Components/ScrivitoExtensions/AlignmentEditor'
 import Thumbnail from './thumbnail.svg'
+import { InvertedBooleanEditor } from '../../Components/ScrivitoExtensions/InvertedBooleanEditor'
 
 provideEditingConfig(ColumnContainerWidget, {
   title: 'Columns',
@@ -26,6 +27,13 @@ provideEditingConfig(ColumnContainerWidget, {
         { value: 'flex', title: 'Flex' },
       ],
       description: 'Default: Grid',
+    },
+    disableGutters: {
+      title: 'Show gutters?',
+      description: 'Gutters are the spaces between columns in a layout.',
+    },
+    disableResponsiveAdaption: {
+      title: 'Responsive adaption?',
     },
   },
   properties: [
@@ -45,13 +53,43 @@ provideEditingConfig(ColumnContainerWidget, {
       },
     ],
     'layoutMode',
+    [
+      'disableResponsiveAdaption',
+      {
+        component: ({ widget }: { widget: Widget }) => (
+          <InvertedBooleanEditor
+            attributeValue={widget.get('disableResponsiveAdaption') === true}
+            readOnly={!canEdit(widget.obj()) || isComparisonActive()}
+            theme={(uiContext() || { theme: null }).theme}
+            updateAttributeValue={(value) =>
+              widget.update({ disableResponsiveAdaption: value })
+            }
+          />
+        ),
+      },
+    ],
+    [
+      'disableGutters',
+      {
+        component: ({ widget }: { widget: Widget }) => (
+          <InvertedBooleanEditor
+            attributeValue={widget.get('disableGutters') === true}
+            readOnly={!canEdit(widget.obj()) || isComparisonActive()}
+            theme={(uiContext() || { theme: null }).theme}
+            updateAttributeValue={(value) =>
+              widget.update({ disableGutters: value })
+            }
+          />
+        ),
+      },
+    ],
   ],
   propertiesGroups: [
     {
       title: 'Columns layout',
       key: 'columns-layout-group',
       component: ColumnsEditorTab,
-      properties: ['columns', 'disableGutters', 'disableResponsiveAdaption'],
+      properties: ['columns'],
     },
   ],
   initialContent: {
