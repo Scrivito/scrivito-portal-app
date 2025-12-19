153a8b6410ac67f8b9d8a77457dd5004159cae3b
Alexander Pepper <alexander.pepper@justrelate.com>
2025-12-19 09:27:20 +0100

    Move configurePisaSalesQuestionnaireWidgets from "Widgets/index" to "config/index"
    This way it can be ensured, that it is called _after_ configure has be executed.

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -1,12 +1,9 @@
 import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets'
-import { configurePisaSalesQuestionnaireWidgets } from './configurePisaSalesQuestionnaireWidgets'
 
 import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
 })
 
 initNeoletterFormWidgets()
-
-configurePisaSalesQuestionnaireWidgets()
 
 export {}
