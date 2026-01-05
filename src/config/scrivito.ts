bc24556cf678325bcd1d3375231d71f796a0cf12
Alexander Pepper <alexander.pepper@justrelate.com>
2026-01-05 11:10:06 +0100

    Scrivito.configure: Use modern "instanceId" flag.

--- src/config/scrivito.ts
+++ src/config/scrivito.ts
@@ -4,8 +4,8 @@ import { getJrPlatformConfig } from '../privateJrPlatform/getJrPlatformConfig'
 import { getJrPlatformInstanceId } from '../privateJrPlatform/multiTenancy'
 
 export function configureScrivito(options?: { priority?: 'background' }) {
-  const tenant = scrivitoTenantId()
-  if (!tenant) throw new Error('No Scrivito tenant ID found!')
+  const instanceId = scrivitoInstanceId()
+  if (!instanceId) throw new Error('No Scrivito instance ID found!')
 
   configure({
     activateDataIntegration: true,
@@ -13,17 +13,17 @@ export function configureScrivito(options?: { priority?: 'background' }) {
     autoConvertAttributes: true,
     baseUrlForSite,
     contentTagsForEmptyAttributes: false,
-    extensionsUrl: `/_scrivito_extensions.html?tenantId=${tenant}`,
+    extensionsUrl: `/_scrivito_extensions.html?tenantId=${instanceId}`,
+    instanceId,
     optimizedWidgetLoading: true,
     siteForUrl,
     strictSearchOperators: true,
-    tenant,
     ...(import.meta.env.PRIVATE_JR_PLATFORM ? getJrPlatformConfig() : {}),
     ...options,
   })
 }
 
-function scrivitoTenantId(): string | null {
+function scrivitoInstanceId(): string | null {
   if (import.meta.env.PRIVATE_JR_PLATFORM) return getJrPlatformInstanceId()
 
   return import.meta.env.SCRIVITO_INSTANCE_ID
