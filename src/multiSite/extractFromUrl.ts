f2357b248ef2850646704b565d571c05edec0b73
Alexander Pepper <alexander.pepper@justrelate.com>
2026-03-18 14:50:46 +0100

    Support full BCP 47.
    As described on https://developer.mozilla.org/en-US/docs/Glossary/BCP_47_language_tag. Previously the following languages where not supported:
    * `bdz`
    * `es-013`
    * `ja-kana`
    * `ru-cyrl-by`

--- src/multiSite/extractFromUrl.ts
+++ src/multiSite/extractFromUrl.ts
@@ -8,7 +8,7 @@ export function extractFromUrl(url: string): {
 } {
   return (
     new RegExp(
-      `^${instanceBaseUrl()}(?<defaultLocation>(/(?<contentId>[0-9a-z]{16}))?(/(?<language>[a-z]{2}(-[A-Z]{2})?))?(?<location>([?/].*)|$))`,
+      `^${instanceBaseUrl()}(?<defaultLocation>(/(?<contentId>[0-9a-z]{16}))?(/(?<language>[a-z]{2,3}(-[A-Z][a-z]{3})?(-([A-Z]{2}|[0-9]{3}))?))?(?<location>([?/].*)|$))`,
     ).exec(url)?.groups || {}
   )
 }
