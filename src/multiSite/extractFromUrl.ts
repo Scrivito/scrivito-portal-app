ce813084a75806d0ddfe2192081e6caeb3fc2561
Alexander Pepper <alexander.pepper@justrelate.com>
2026-03-20 11:26:26 +0100

    extractFromUrl: Rename location to pagePath
    Avoids confusion with the browser's window.location.

--- src/multiSite/extractFromUrl.ts
+++ src/multiSite/extractFromUrl.ts
@@ -4,7 +4,7 @@ export function extractFromUrl(url: string): {
   contentId?: string
   defaultLocation?: string
   language?: string
-  location?: string
+  pagePath?: string
 } {
   const contentId = '[0-9a-z]{16}'
 
@@ -16,12 +16,12 @@ export function extractFromUrl(url: string): {
     '(-[A-Z][a-z]{3})?' + // optional script code (e.g. 'Cyrl' or 'Latn')
     '(-([A-Z]{2}|[0-9]{3}))?' // optional region code (e.g. 'US' or '013')
 
-  const location = '([?/].*)|$'
+  const pagePath = '([?/].*)|$'
 
   const defaultLocation =
     `(/(?<contentId>${contentId}))?` +
     `(/(?<language>${language}))?` +
-    `(?<location>${location})`
+    `(?<pagePath>${pagePath})`
 
   return (
     new RegExp(
