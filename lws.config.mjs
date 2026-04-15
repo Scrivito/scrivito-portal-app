e1b446dd202a8f2a46abdd123ef75fa318ca1116
Alexander Pepper <alexander.pepper@justrelate.com>
2026-04-15 10:20:05 +0200

    prerender-preview: Improved fallback
    E.g. http://localhost:8080/71ec0827969c2bd8/en now falls back to file 71ec0827969c2bd8/en.html

--- lws.config.mjs
+++ lws.config.mjs
@@ -1,7 +1,7 @@
 export default {
   directory: 'dist-ssr',
   port: 8080,
-  rewrite: [{ from: '(/..)(/.*|$)', to: '$&.html' }],
+  rewrite: [{ from: '(/[^.]+)', to: '$1.html' }],
   spa: 'index.html',
   spaAssetTestFs: true,
   staticMaxage: 31536000,
