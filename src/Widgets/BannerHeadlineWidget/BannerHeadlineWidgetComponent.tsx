39d8ba8e1d99a5da0697c4e31911cb1f8521c51e
Alexander Pepper <alexander.pepper@justrelate.com>
2026-05-18 14:47:08 +0200

    PageTitle and BannerHeadline widgets also use newer background
    They also now support "transparent" for completeness sake

--- src/Widgets/BannerHeadlineWidget/BannerHeadlineWidgetComponent.tsx
+++ src/Widgets/BannerHeadlineWidget/BannerHeadlineWidgetComponent.tsx
@@ -12,7 +12,7 @@ provideComponent(BannerHeadlineWidget, ({ widget }) => {
         content={widget}
         attribute="headline"
         tag="span"
-        className={`bg-${backgroundColor}`}
+        className={backgroundClassName(backgroundColor)}
       />
     </WidgetTag>
   )
@@ -29,4 +29,33 @@ function tag(level: string | null, style: string): string {
   if (style === 'display-6') return 'h6'
 
   return style
+}
+
+function backgroundClassName(backgroundColor: string): string {
+  switch (backgroundColor) {
+    case 'primary':
+      return 'bg-portal-primary text-on-portal-primary'
+    case 'secondary':
+      return 'bg-portal-secondary text-on-portal-secondary'
+    case 'white':
+      return 'bg-portal-white text-on-portal-white'
+    case 'light-grey':
+      return 'bg-portal-light-grey text-on-portal-light-grey'
+    case 'middle-grey':
+      return 'bg-portal-middle-grey text-on-portal-middle-grey'
+    case 'dark-grey':
+      return 'bg-portal-dark-grey text-on-portal-dark-grey'
+    case 'success':
+      return 'bg-portal-success text-on-portal-success'
+    case 'info':
+      return 'bg-portal-info text-on-portal-info'
+    case 'warning':
+      return 'bg-portal-warning text-on-portal-warning'
+    case 'danger':
+      return 'bg-portal-danger text-on-portal-danger'
+    case 'transparent':
+      return ''
+    default:
+      throw new Error(`Unknown backgroundColor: ${backgroundColor}`)
+  }
 }
