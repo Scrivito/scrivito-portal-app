ff5806b297dd959f269d1369cfea31e4bdf9b156
patrykboch <patrykboch@gmail.com>
2026-05-18 15:21:06 +0200

    TEST

--- src/privateJrPlatform/getJrPlatformConfig.ts
+++ src/privateJrPlatform/getJrPlatformConfig.ts
@@ -2,7 +2,7 @@ export function getJrPlatformConfig() {
   return {
     unstable: {
       initialContentDumpUrl:
-        'https://v6-content.scrivito-portal-app.pages.dev/index.json',
+        'https://v6-content-v2.scrivito-portal-app.pages.dev/index.json',
       trustedUiOrigins: [
         'http://localhost:8090',
         'https://*.scrivito-ui.pages.dev',
