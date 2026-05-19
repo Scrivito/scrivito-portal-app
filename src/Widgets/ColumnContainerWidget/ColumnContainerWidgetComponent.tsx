541aa4805cbf740ac0f3249464e7230206d2a017
Alexander Pepper <alexander.pepper@justrelate.com>
2026-05-19 10:36:20 +0200

    ColumnContainerWidget: Convert from bootstrap to tailwind

--- src/Widgets/ColumnContainerWidget/ColumnContainerWidgetComponent.tsx
+++ src/Widgets/ColumnContainerWidget/ColumnContainerWidgetComponent.tsx
@@ -2,7 +2,6 @@ import { provideComponent, ContentTag, connect } from 'scrivito'
 import { ColumnContainerWidget } from './ColumnContainerWidgetClass'
 import { ColumnWidgetInstance } from '../ColumnWidget/ColumnWidgetClass'
 import { normalizeColSizes } from './normalizeColSizes'
-import './ColumnContainerWidget.scss'
 
 provideComponent(ColumnContainerWidget, ({ widget }) => {
   const columns = widget.get('columns')
@@ -15,16 +14,18 @@ provideComponent(ColumnContainerWidget, ({ widget }) => {
   const normalizedColSizes = isFlex
     ? columns.map(() => 1)
     : normalizeColSizes(columns as ColumnWidgetInstance[])
-  const classNames = [`align-items-${alignment}`]
+
+  const classNames = [alignmentClass(alignment)]
 
   if (isFlex) {
-    classNames.push(
-      'column-container-widget--flex-wrapper',
-      isResponsive ? 'd-md-flex' : 'd-flex',
-    )
+    if (isResponsive) {
+      classNames.push('md:flex', 'md:-mx-2')
+    } else {
+      classNames.push('flex', '-mx-2')
+    }
   } else {
-    classNames.push('row')
-    if (widget.get('disableGutters')) classNames.push('g-0')
+    classNames.push('grid', 'grid-cols-12')
+    if (!widget.get('disableGutters')) classNames.push('gap-x-6')
   }
 
   return (
@@ -61,16 +62,20 @@ const Column = connect(function Column({
   const classNames = []
 
   if (isFlex) {
-    classNames.push(
-      ...(isResponsive ? ['my-md-0', 'my-2', 'mx-md-2'] : ['mx-2']),
-    )
-    if (columnWidget.get('flexGrow')) classNames.push('flex-grow-1')
+    if (isResponsive) {
+      classNames.push('my-2', 'md:my-0', 'md:mx-2')
+    } else {
+      classNames.push('mx-2')
+    }
+    if (columnWidget.get('flexGrow')) classNames.push('grow')
     if (isStretch && columnWidget.get('content').length < 2) {
-      classNames.push(isResponsive ? 'd-md-flex' : 'd-flex')
+      classNames.push(isResponsive ? 'md:flex' : 'flex')
     }
   } else {
     classNames.push(
-      isResponsive ? `col-md-${normalizedColSize}` : `col-${normalizedColSize}`,
+      isResponsive
+        ? responsiveColSpanClass(normalizedColSize)
+        : colSpanClass(normalizedColSize),
     )
   }
 
@@ -83,3 +88,40 @@ const Column = connect(function Column({
     />
   )
 })
+
+function alignmentClass(alignment: string): string {
+  if (alignment === 'center') return 'items-center'
+  if (alignment === 'end') return 'items-end'
+  if (alignment === 'stretch') return 'items-stretch'
+  return 'items-start'
+}
+
+function colSpanClass(size: number): string {
+  if (size === 1) return 'col-span-1'
+  if (size === 2) return 'col-span-2'
+  if (size === 3) return 'col-span-3'
+  if (size === 4) return 'col-span-4'
+  if (size === 5) return 'col-span-5'
+  if (size === 6) return 'col-span-6'
+  if (size === 7) return 'col-span-7'
+  if (size === 8) return 'col-span-8'
+  if (size === 9) return 'col-span-9'
+  if (size === 10) return 'col-span-10'
+  if (size === 11) return 'col-span-11'
+  return 'col-span-12'
+}
+
+function responsiveColSpanClass(size: number): string {
+  if (size === 1) return 'col-span-12 md:col-span-1'
+  if (size === 2) return 'col-span-12 md:col-span-2'
+  if (size === 3) return 'col-span-12 md:col-span-3'
+  if (size === 4) return 'col-span-12 md:col-span-4'
+  if (size === 5) return 'col-span-12 md:col-span-5'
+  if (size === 6) return 'col-span-12 md:col-span-6'
+  if (size === 7) return 'col-span-12 md:col-span-7'
+  if (size === 8) return 'col-span-12 md:col-span-8'
+  if (size === 9) return 'col-span-12 md:col-span-9'
+  if (size === 10) return 'col-span-12 md:col-span-10'
+  if (size === 11) return 'col-span-12 md:col-span-11'
+  return 'col-span-12 md:col-span-12'
+}
