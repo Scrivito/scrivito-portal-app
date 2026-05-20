96af5f6222119e4840d1a8556de3734d7eb847a0
Alexander Pepper <alexander.pepper@justrelate.com>
2026-05-20 22:28:37 +0200

    CardWidget: Convert to Tailwind

--- src/Widgets/CardWidget/CardWidgetComponent.tsx
+++ src/Widgets/CardWidget/CardWidgetComponent.tsx
@@ -16,20 +16,30 @@ import { useRef } from 'react'
 
 provideComponent(CardWidget, ({ widget }) => {
   const cardBodyClassNames: string[] = ['card-body']
-  const padding = widget.get('padding')
-  cardBodyClassNames.push(padding ? padding : 'p-4')
+  const bootstrapPadding = widget.get('padding') || 'p-4'
+  cardBodyClassNames.push(tailwindPaddingFor(bootstrapPadding))
   const togglePlayPauseRef = useRef<TogglePlayPauseRef>(null)
 
   const image = widget.get('image')
 
   const cardClassNames: string[] = ['card']
 
-  cardClassNames.push(widget.get('margin') ?? 'mb-4')
+  const bootstrapMarginBottom = widget.get('margin') ?? 'mb-4'
+  cardClassNames.push(tailwindMarginBottomFor(bootstrapMarginBottom))
 
   const backgroundColor = widget.get('backgroundColor') || 'white'
   cardClassNames.push(backgroundClassName(backgroundColor))
+  if (backgroundColor === 'transparent') cardClassNames.push('shadow-none')
 
-  if (widget.get('cardExtended')) cardClassNames.push('card-extended')
+  if (widget.get('cardExtended')) {
+    cardClassNames.push(
+      'overflow-visible',
+      "after:rounded-portal after:pointer-events-none after:absolute after:top-0 after:bottom-0 after:-z-[1] after:block after:bg-inherit after:content-['']",
+      '[*:first-child_&]:after:right-0 [*:first-child_&]:after:left-[-2600px]',
+      '[*:last-child_&]:after:right-[-2600px] [*:last-child_&]:after:left-0',
+      '[*:only-child_&]:after:right-0 [*:only-child_&]:after:left-0',
+    )
+  }
 
   return (
     <WidgetTag
@@ -100,6 +110,44 @@ function backgroundClassName(backgroundColor: string): string {
       return ''
     default:
       throw new Error(`Unknown backgroundColor: ${backgroundColor}`)
+  }
+}
+
+function tailwindPaddingFor(bootstrapPadding: string): string {
+  switch (bootstrapPadding) {
+    case 'p-0':
+      return 'p-0'
+    case 'p-1':
+      return 'p-1'
+    case 'p-2':
+      return 'p-2'
+    case 'p-3':
+      return 'p-4'
+    case 'p-4':
+      return 'p-6'
+    case 'p-5':
+      return 'p-12'
+    default:
+      throw new Error(`Unknown bootstrap padding: ${bootstrapPadding}`)
+  }
+}
+
+function tailwindMarginBottomFor(bootstrapMarginBottom: string): string {
+  switch (bootstrapMarginBottom) {
+    case 'mb-0':
+      return 'mb-0'
+    case 'mb-1':
+      return 'mb-1'
+    case 'mb-2':
+      return 'mb-2'
+    case 'mb-3':
+      return 'mb-4'
+    case 'mb-4':
+      return 'mb-6'
+    case 'mb-5':
+      return 'mb-12'
+    default:
+      throw new Error(`Unknown bootstrap margin: ${bootstrapMarginBottom}`)
   }
 }
 
