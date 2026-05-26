4163a0452f619a09ed1a0845291ace1536814012
Alexander Pepper <alexander.pepper@justrelate.com>
2026-05-26 14:36:34 +0200

    Tailwind: Extracted backgroundClassName to utils
    Also converted to tailwind best practice (see https://tailwindcss.com/docs/detecting-classes-in-source-files#dynamic-class-names)

--- src/Widgets/BannerHeadlineWidget/BannerHeadlineWidgetComponent.tsx
+++ src/Widgets/BannerHeadlineWidget/BannerHeadlineWidgetComponent.tsx
@@ -1,5 +1,6 @@
 import { ContentTag, provideComponent, WidgetTag } from 'scrivito'
 import { BannerHeadlineWidget } from './BannerHeadlineWidgetClass'
+import { backgroundClassName } from '../../utils/backgroundClassName'
 
 provideComponent(BannerHeadlineWidget, ({ widget }) => {
   const style = widget.get('style') || 'h2'
@@ -29,33 +30,4 @@ function tag(level: string | null, style: string): string {
   if (style === 'display-6') return 'h6'
 
   return style
-}
-
-function backgroundClassName(backgroundColor: string): string {
-  switch (backgroundColor) {
-    case 'primary':
-      return 'bg-portal-primary text-on-portal-primary'
-    case 'secondary':
-      return 'bg-portal-secondary text-on-portal-secondary'
-    case 'white':
-      return 'bg-portal-white text-on-portal-white'
-    case 'light-grey':
-      return 'bg-portal-light-grey text-on-portal-light-grey'
-    case 'middle-grey':
-      return 'bg-portal-middle-grey text-on-portal-middle-grey'
-    case 'dark-grey':
-      return 'bg-portal-dark-grey text-on-portal-dark-grey'
-    case 'success':
-      return 'bg-portal-success text-on-portal-success'
-    case 'info':
-      return 'bg-portal-info text-on-portal-info'
-    case 'warning':
-      return 'bg-portal-warning text-on-portal-warning'
-    case 'danger':
-      return 'bg-portal-danger text-on-portal-danger'
-    case 'transparent':
-      return ''
-    default:
-      throw new Error(`Unknown backgroundColor: ${backgroundColor}`)
-  }
 }
