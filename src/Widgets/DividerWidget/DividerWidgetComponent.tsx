feb66798620f24e74a11abc06485e0b05e52da8e
Alexander Pepper <alexander.pepper@justrelate.com>
2026-05-27 11:02:11 +0200

    DividerWidget: Don't hard-code old bootstrap colors
    This changes `#dee2e6` to `#e5e5e5`. The other alternative (`border-gray-200`) would result in `#ebe6e7`, but seems to be a bitter color change to the original.

--- src/Widgets/DividerWidget/DividerWidgetComponent.tsx
+++ src/Widgets/DividerWidget/DividerWidgetComponent.tsx
@@ -2,5 +2,5 @@ import { provideComponent } from 'scrivito'
 import { DividerWidget } from './DividerWidgetClass'
 
 provideComponent(DividerWidget, () => {
-  return <div className="border-t border-[#dee2e6]"></div>
+  return <div className="border-t border-neutral-200"></div>
 })
