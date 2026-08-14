c7829f0f98afcd97af4eeea0b7c68a70e13d626e
David Császár <dcsaszar@users.noreply.github.com>
2026-08-14 15:53:40 +0200

    Enable etracker for all JustRelate platform instances

--- src/privateJrPlatform/jrPlatformConfigureEtracker.ts
+++ src/privateJrPlatform/jrPlatformConfigureEtracker.ts
@@ -1,5 +1,4 @@
-import { isEditorLoggedIn } from 'scrivito'
-import { getJrPlatformInstanceId, instanceFromHostname } from './multiTenancy'
+import { getInstanceId, isEditorLoggedIn } from 'scrivito'
 
 declare global {
   interface Window {
@@ -11,17 +10,6 @@ export function jrPlatformConfigureEtracker() {
   if (typeof window === 'undefined') return
   if (isEditorLoggedIn()) return
 
-  const jrPlatformInstanceId = getJrPlatformInstanceId()
-  if (!jrPlatformInstanceId) return
-
-  const hostnameInstance = instanceFromHostname()
-  if (
-    hostnameInstance &&
-    hostnameInstance !== '13b78a0a81072f996f5010bb59b48957' // allow tynacoon.com for now
-  ) {
-    return
-  }
-
   // Copyright (c) 2000-2026 etracker GmbH. All rights reserved.
   // No reproduction, publication or modification allowed without permission.
   // etracker code 6.0
@@ -31,7 +19,7 @@ export function jrPlatformConfigureEtracker() {
   script.id = '_etLoader'
   script.type = 'text/javascript'
   script.setAttribute('data-block-cookies', 'true')
-  script.setAttribute('data-instance-id', jrPlatformInstanceId)
+  script.setAttribute('data-instance-id', getInstanceId())
   script.setAttribute('data-page-changed-detection', 'url')
   script.src = 'https://code.etracker.com/code/e.js'
   script.async = true
