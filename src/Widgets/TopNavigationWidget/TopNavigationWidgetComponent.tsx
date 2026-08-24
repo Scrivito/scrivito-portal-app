ac08cbfdeae4e28ca6b7d701d55ceef28a4aa57d
David Császár <dcsaszar@users.noreply.github.com>
2026-08-24 15:34:40 +0200

    Implement top nav grey background in Tailwind

--- src/Widgets/TopNavigationWidget/TopNavigationWidgetComponent.tsx
+++ src/Widgets/TopNavigationWidget/TopNavigationWidgetComponent.tsx
@@ -6,15 +6,21 @@ import { Brand } from './SubComponents/Brand'
 import { MainNavigation } from './SubComponents/MainNavigation'
 import { MetaNavigation } from './SubComponents/MetaNavigation'
 import { isHomepage } from '../../Objs/Homepage/HomepageObjClass'
-import './TopNavigationWidget.scss'
 
 provideComponent(TopNavigationWidget, ({ widget }) => {
   const root = Obj.root()
   if (!isHomepage(root)) return null
 
-  const classNames = ['top-navigation-widget']
+  const classNames = ['z-[3]']
 
-  if (widget.get('slimDesign')) classNames.push('slim-nav')
+  if (widget.get('slimDesign')) {
+    classNames.push('slim-nav')
+  } else {
+    // two color top navbar
+    classNames.push(
+      "after:pointer-events-none after:absolute after:inset-x-0 after:top-0 after:-z-[1] after:block after:h-[38px] after:bg-portal-light-grey after:content-['']",
+    )
+  }
 
   return (
     <WidgetTag tag="section" className={classNames.join(' ')}>
