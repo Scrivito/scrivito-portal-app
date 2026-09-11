6cfd4a0e80e443459215d6d80555478099694fa3
Alexander Pepper <alexander.pepper@justrelate.com>
2026-09-11 11:45:05 +0200

    Converting error components to tailwind

--- src/Components/ErrorBoundary.tsx
+++ src/Components/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import errorBackground from '../assets/images/error-background.jpg'
 import spaceman from '../assets/images/spaceman.svg'
+import { backgroundClassName } from '../utils/theme/backgroundClassName'
 
 interface ErrorBoundaryProps {
   children: React.ReactNode
@@ -28,7 +29,9 @@ export class ErrorBoundary extends Component<
     if (!this.state.hasError) return this.props.children
 
     return (
-      <section className="bg-danger vh-100 py-5">
+      <section
+        className={`${backgroundClassName('danger')} flex min-h-screen items-center py-12`}
+      >
         <div className="img-zoom-container">
           <img
             src={errorBackground}
@@ -36,18 +39,18 @@ export class ErrorBoundary extends Component<
             className="img-background img-zoom"
           />
         </div>
-        <div className="container h-100">
-          <div className="row align-items-center h-100">
-            <div className="col-sm-6">
+        <div className="container h-full">
+          <div className="flex h-full flex-wrap items-center">
+            <div className="w-full sm:w-1/2">
               <img src={spaceman} alt="spaceman" />
             </div>
-            <div className="col-sm-6">
+            <div className="w-full sm:w-1/2">
               <h1 className="display-1">Error</h1>
-              <h3 className="h3">Something went wrong.</h3>
+              <h3 className="text-2xl font-semibold">Something went wrong.</h3>
               <div>
                 <button
                   type="button"
-                  className="btn btn-light"
+                  className="btn-portal border-white bg-white text-gray-800 hover:bg-gray-100"
                   onClick={window.location.reload}
                 >
                   Please try again
--- src/privateJrPlatform/Components/JrPlatformWrongContentFormat.tsx
+++ src/privateJrPlatform/Components/JrPlatformWrongContentFormat.tsx
@@ -2,6 +2,7 @@ import { connect, Obj } from 'scrivito'
 import { ensureString } from '../../utils/ensureString'
 import errorBackground from '../../assets/images/error-background.jpg'
 import spaceman from '../../assets/images/spaceman.svg'
+import { backgroundClassName } from '../../utils/theme/backgroundClassName'
 
 export const JrPlatformWrongContentFormat = connect(
   function JrPlatformWrongContentFormat() {
@@ -9,7 +10,9 @@ export const JrPlatformWrongContentFormat = connect(
 
     return (
       <main id="main">
-        <section className="bg-danger vh-100 py-5">
+        <section
+          className={`${backgroundClassName('danger')} flex min-h-screen items-center py-12`}
+        >
           <div className="img-zoom-container">
             <img
               src={errorBackground}
@@ -17,14 +20,14 @@ export const JrPlatformWrongContentFormat = connect(
               className="img-background img-zoom"
             />
           </div>
-          <div className="container h-100">
-            <div className="row align-items-center h-100">
-              <div className="col-sm-6">
+          <div className="container h-full">
+            <div className="flex h-full flex-wrap items-center">
+              <div className="w-full sm:w-1/2">
                 <img src={spaceman} alt="spaceman" />
               </div>
-              <div className="col-sm-6">
+              <div className="w-full sm:w-1/2">
                 <h1 className="display-1">Error</h1>
-                <h3 className="h3">
+                <h3 className="text-2xl font-semibold">
                   {contentFormat
                     ? `Content format “${contentFormat}” is unsupported.`
                     : 'Missing content format.'}
