9232912e1aa70ad229ddce0ba04b33001016acaf
Alexander Pepper <alexander.pepper@justrelate.com>
2026-09-11 11:44:53 +0200

    DataLoadMoreButton: Convert to Tailwind

--- src/Widgets/DataLoadMoreButtonWidget/DataLoadMoreButtonWidgetComponent.tsx
+++ src/Widgets/DataLoadMoreButtonWidget/DataLoadMoreButtonWidgetComponent.tsx
@@ -7,37 +7,39 @@ import {
 import { DataLoadMoreButtonWidget } from './DataLoadMoreButtonWidgetClass'
 import { useContext } from 'react'
 import { DataBatchContext } from '../../Components/DataBatchContext'
-import { buttonSizeClassName } from '../../utils/buttonSizeClassName'
-import { alignmentClassNameWithBlock } from '../../utils/alignmentClassName'
+import { buttonColorClassName } from '../../utils/theme/buttonColorClassName'
 
 provideComponent(DataLoadMoreButtonWidget, ({ widget }) => {
   const { hasMore, loadMore } = useContext(DataBatchContext)
 
-  const classNames = ['btn', 'mt-4']
-
-  const buttonSize = buttonSizeClassName(widget.get('buttonSize'))
-  if (buttonSize) classNames.push(buttonSize)
+  const classNames = ['btn-portal', 'mt-6']
+  const widgetTagClassNames: string[] = []
 
   const buttonColor = widget.get('buttonColor') || 'btn-outline-primary'
-  if (buttonColor) classNames.push(buttonColor)
+  classNames.push(buttonColorClassName(buttonColor))
 
-  const alignmentClassName = alignmentClassNameWithBlock(
-    widget.get('alignment'),
-  )
+  const buttonSize = widget.get('buttonSize') || 'medium'
+  if (buttonSize === 'small') classNames.push('px-2', 'py-1', 'text-sm')
+  if (buttonSize === 'large') classNames.push('px-4', 'py-2', 'text-lg')
+
+  const alignment = widget.get('alignment')
+  if (alignment === 'block') classNames.push('w-full')
+  if (alignment === 'center') widgetTagClassNames.push('text-center')
+  if (alignment === 'right') widgetTagClassNames.push('text-right')
 
   if (!hasMore()) {
     if (!isInPlaceEditingActive() && !isComparisonActive()) return null
 
     classNames.push('opacity-60')
     return (
-      <WidgetTag className={alignmentClassName}>
+      <WidgetTag className={widgetTagClassNames.join(' ')}>
         <button className={classNames.join(' ')}>{widget.get('title')}</button>
       </WidgetTag>
     )
   }
 
   return (
-    <WidgetTag className={alignmentClassName}>
+    <WidgetTag className={widgetTagClassNames.join(' ')}>
       <button className={classNames.join(' ')} onClick={loadMore}>
         {widget.get('title')}
       </button>
